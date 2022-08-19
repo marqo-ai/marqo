7a7674778398310a4d5000ff21786dde6f1248b6
Jesse Clark <jesse@s2search.io>
2022-08-19 13:22:45 +1000

    bump default gpu processes to 4

--- src/marqo/neural_search/configs.py
+++ src/marqo/neural_search/configs.py
@@ -23,7 +23,7 @@ def get_default_neural_index_settings():
     }
 
 def get_max_processes():
-    return {'max_processes_cpu' :6, 'max_processes_gpu': 2}
+    return {'max_processes_cpu' :6, 'max_processes_gpu': 4}
 
 def get_threads_per_process():
     total_cpu = max(1, mp.cpu_count() - 2)
