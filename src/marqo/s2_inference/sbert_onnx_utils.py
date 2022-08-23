839edd3be225c05c17068b7d8508a149b65cc606
Jesse Clark <jesse@s2search.io>
2022-08-24 09:06:32 +1000

    update model cache import

--- src/marqo/s2_inference/sbert_onnx_utils.py
+++ src/marqo/s2_inference/sbert_onnx_utils.py
@@ -10,25 +10,9 @@ from pathlib import Path
 
 from marqo.s2_inference.types import *
 from marqo.s2_inference.logger import get_logger
+from marqo.s2_inference.configs import ModelCache
+
 logger = get_logger(__name__)
-
-class ModelCache:
-
-    onnx_cache_path = os.environ.get('ONNX_SAVE_PATH', './cache/models_onnx/')
-    torch_cache_path = os.getenv('SENTENCE_TRANSFORMERS_HOME', './cache/models/')
-
-class BaseTransformerModels:
-
-    names = ('albert-base-v1', 'albert-base-v2', 'albert-large-v1', 'albert-large-v2', 'albert-xlarge-v1', 'albert-xlarge-v2', 'albert-xxlarge-v1', 'albert-xxlarge-v2', 'bert-base-cased-finetuned-mrpc', 'bert-base-cased', 'bert-base-chinese', 'bert-base-german-cased', 'bert-base-german-dbmdz-cased', 'bert-base-german-dbmdz-uncased', 'bert-base-multilingual-cased', 'bert-base-multilingual-uncased', 'bert-base-uncased', 'bert-large-cased-whole-word-masking-finetuned-squad', 'bert-large-cased-whole-word-masking', 'bert-large-cased', 'bert-large-uncased-whole-word-masking-finetuned-squad', 'bert-large-uncased-whole-word-masking', 'bert-large-uncased', 'camembert-base', 'ctrl', 'distilbert-base-cased-distilled-squad', 'distilbert-base-cased', 'distilbert-base-german-cased', 'distilbert-base-multilingual-cased', 'distilbert-base-uncased-distilled-squad',
-                                    'distilbert-base-uncased-finetuned-sst-2-english', 'distilbert-base-uncased', 'distilgpt2', 'distilroberta-base', 'gpt2-large', 'gpt2-medium', 'gpt2-xl', 'gpt2', 'openai-gpt', 'roberta-base-openai-detector', 'roberta-base', 'roberta-large-mnli', 'roberta-large-openai-detector', 'roberta-large', 't5-11b', 't5-3b', 't5-base', 't5-large', 't5-small', 'transfo-xl-wt103', 'xlm-clm-ende-1024', 'xlm-clm-enfr-1024', 'xlm-mlm-100-1280', 'xlm-mlm-17-1280', 'xlm-mlm-en-2048', 'xlm-mlm-ende-1024', 'xlm-mlm-enfr-1024', 'xlm-mlm-enro-1024', 'xlm-mlm-tlm-xnli15-1024', 'xlm-mlm-xnli15-1024', 'xlm-roberta-base', 'xlm-roberta-large-finetuned-conll02-dutch', 'xlm-roberta-large-finetuned-conll02-spanish', 'xlm-roberta-large-finetuned-conll03-english', 'xlm-roberta-large-finetuned-conll03-german', 'xlm-roberta-large', 'xlnet-base-cased', 'xlnet-large-cased')
-
-class Invalid:
-
-    paths = (None, '', ' ', "", " ", "''", '""')
-
-class Ignore:
-
-    files = ('flax_model.msgpack', 'rust_model.ot', 'tf_model.h5')
 
 
 
@@ -98,7 +82,7 @@ class SBERT_ONNX(object):
         """determine where the model should run based on specified device
         """
         self.onnxproviders = onnxruntime.get_available_providers()
-        logger.info(f"device:{self.device}")
+        logger.info(f"device:{self.device} and available providers {self.onnxproviders}")
         if self.device == 'cpu':
             self.fast_onnxprovider = 'CPUExecutionProvider'
         else:
