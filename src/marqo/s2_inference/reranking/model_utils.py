e2100c2b2a945c2985b749acd167174162c1c8b1
Jesse Clark <jesse@s2search.io>
2022-08-24 09:05:58 +1000

    add dummy model for tests, add onnx converter, map device to int for pipelines, add option for onnx in loading

--- src/marqo/s2_inference/reranking/model_utils.py
+++ src/marqo/s2_inference/reranking/model_utils.py
@@ -1,19 +1,111 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import numpy as np
+import os
+
+from transformers import (
+    AutoTokenizer, 
+    AutoModelForSequenceClassification,
+    OwlViTProcessor, 
+    OwlViTForObjectDetection,
+    pipeline,
+)
+
+from optimum.onnxruntime import ORTModelForSequenceClassification
 from sentence_transformers import CrossEncoder
 import torch
-from transformers import OwlViTProcessor, OwlViTForObjectDetection
 
 from marqo.s2_inference.types import *
 from marqo.s2_inference.s2_inference import available_models
 from marqo.s2_inference.s2_inference import _create_model_cache_key
+from marqo.s2_inference.configs import ModelCache
 
 from marqo.s2_inference.logger import get_logger
 logger = get_logger(__name__)
 
 
+def _verify_model_inputs(list_of_lists):
+    return all(isinstance(x, (list, tuple)) for x in list_of_lists)
+
+def convert_device_id_to_int(device: str = 'cpu'):
+
+    if device[:4] not in ['cpu', 'cuda']:
+        raise ValueError(f"expected one of cpu or cuda or cuda:# but received {device}")
+
+    if device == 'cpu':
+        return -1
+    
+    if device == 'cuda':
+        return 0
+
+    if device.startswith('cuda:'):
+        # check if it is id'd by number
+        if device[-1].isnumeric():
+            return int(device.replace('cuda:', ''))
+    
+    raise TypeError(f"unexpected device {device}")
+
+class DummyModel:
+    # used for mocking the model
+    def __init__(self, *args, **kwargs) -> None:
+        pass
+
+    def predict(self, inputs):
+
+        return np.random.rand(len(inputs))
+
+class HFClassificationOnnx:
+    # https://huggingface.co/docs/optimum/main/en/onnxruntime/modeling_ort
+
+    def __init__(self, model_name: str, device: str = 'cpu', load_from_cache: bool = True):
+
+        self.model_name = model_name
+        self.save_path = None
+        self.device_string = device
+        self.device = convert_device_id_to_int(device)
+
+        # TODO load local version
+        self.model = ORTModelForSequenceClassification.from_pretrained(self.model_name, from_transformers=True)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        self.onnx_classifier = pipeline("text-classification", model=self.model, 
+                                        tokenizer=self.tokenizer, device=self.device)
+
+
+    def _get_save_name(self):
+
+        self.save_path = os.path.join(ModelCache.onnx_cache_path, self.model_name + '_onnx')
+        self.model_save_name = self.save_path
+        self.tokenizer_save_name = self.save_path
+
+    def save(self):
+
+        logger.info(f"saving model to {self.model_save_name}")
+        if self.save_path is None:
+            self._get_save_name()
+
+        self.model.save_pretrained(self.model_save_name)
+        self.tokenizer.save_pretrained(self.tokenizer_save_name)
+
+    @staticmethod
+    def _prepare_inputs(inputs: List[List[str]]):
+        if not _verify_model_inputs(inputs):
+            raise RuntimeError(f"expected list of lists, received {type(inputs)} of {type(inputs[0])}")
+
+        return [{'text':pair[0], 'text_pair':pair[1]} for pair in inputs]
+
+    @staticmethod
+    def _parepare_outputs(outputs):
+        return np.array([pred['score'] for pred in outputs])
+
+    def predict(self, inputs):
+        self.inputs = self._prepare_inputs(inputs)
+        self.predictions = self.onnx_classifier(self.inputs)
+        self.outputs = self._parepare_outputs(self.predictions)
+
+        return self.outputs
+
+
 def load_sbert_cross_encoder_model(model_name: str, device: str = 'cpu', max_length: int = 512) -> Any:
     """    
-    
+    https://huggingface.co/cross-encoder/ms-marco-TinyBERT-L-2
     scores = model.predict([('Query', 'Paragraph1'), ('Query', 'Paragraph2') , ('Query', 'Paragraph3')])
 
     Args:
@@ -28,7 +120,13 @@ def load_sbert_cross_encoder_model(model_name: str, device: str = 'cpu', max_len
         model = available_models[model_cache_key] 
     else:
         logger.info(f"loading {model_name} on device {device} and adding to cache...")
-        model = CrossEncoder(model_name, max_length=max_length)
+        if model_name == '_testing':
+            model = DummyModel()
+            logger.warning('using the test model - << TESTING PURPOSES ONLY >>')
+        elif model_name.startswith('onnx/'):
+            model = HFClassificationOnnx(model_name.replace('onnx/', ''), device=device)
+        else:
+            model = CrossEncoder(model_name, max_length=max_length, device=device)
         available_models[model_cache_key] = model
 
     return {'model':model}
