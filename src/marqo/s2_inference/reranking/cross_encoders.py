da1c6ed668343e7ffb6d0ef87e1d414aa2607034
Jesse Clark <jesse@s2search.io>
2022-08-24 09:03:06 +1000

    update imports

--- src/marqo/s2_inference/reranking/cross_encoders.py
+++ src/marqo/s2_inference/reranking/cross_encoders.py
@@ -1,4 +1,4 @@
-from functools import lru_cache
+from functools import lru_cache, partial
 import requests
 import validators
 import time
@@ -6,8 +6,10 @@ import uuid
 from collections import defaultdict
 
 import pandas as pd
+pd.options.mode.chained_assignment = None
 from PIL import Image
 import numpy as np
+
 
 from marqo.s2_inference.types import *
 from marqo.s2_inference.reranking.model_utils import (
@@ -16,20 +18,24 @@ from marqo.s2_inference.reranking.model_utils import (
     _process_owl_inputs,
     _predict_owl,
     process_owl_results,
-    sort_owl_boxes_scores
+    sort_owl_boxes_scores,
+    _verify_model_inputs
 )
+from marqo.s2_inference.reranking.enums import Columns, ResultsFields
+from marqo.s2_inference.reranking.configs import get_default_text_processing_parameters
+from marqo.processing import text as text_processor
 
 from marqo.s2_inference.logger import get_logger
 logger = get_logger(__name__)
 
 # TODO move this to an enum
-HIGHLIGHTS_FIELD = '_highlights'
+HIGHLIGHTS_FIELD = ResultsFields.highlights
 
 
 
 def _get_ids_from_results(results):
 
-    return [doc['_id'] for doc in results['hits']]
+    return [doc[ResultsFields.id] for doc in results[ResultsFields.hits]]
 
 def get_results_by_doc_id(results):
 
@@ -42,20 +48,20 @@ class FormattedResults:
 
     # output should be a dataframe with all fields required 
 
-    def __init__(self, results, highlights_field=HIGHLIGHTS_FIELD):
+    def __init__(self, results, highlights_field=ResultsFields.highlights):
 
         self.results = results
         self.highlights_field = highlights_field
-
+        
         # check ids exist and if not create some
-        self._fill_doc_ids(results)
+        self._fill_doc_ids(self.results)
 
         self.results_to_df()
 
         self._get_searchable_columns()
 
     def results_to_df(self):
-        self.results_df = pd.DataFrame(self.results['hits'])
+        self.results_df = pd.DataFrame(self.results[ResultsFields.hits])
 
         def _get_highlights(content):
             if content == []:
@@ -65,7 +71,7 @@ class FormattedResults:
                 if len(_key) == 0:
                     return None
                 if len(_key) > 1:
-                    logger.warning(f"found more than 1 highlight. found {_key}. using the first only for re-ranking...")
+                    logger.warning(f"found more than 1 highlight. found {_key}. keeping the first only...")
                 return content[_key[0]]
 
         if self.highlights_field in self.results_df:
@@ -77,12 +83,12 @@ class FormattedResults:
         # check if an id exists, otherwise create a temporary one for easier identification
         # during re-ranking
         
-        for result in results['hits']:
-            if '_id' not in result:
+        for result in results[ResultsFields.hits]:
+            if ResultsFields.id not in result:
                 doc_id = str(uuid.uuid4())
-                result['_rerank_id'] = doc_id 
+                result[ResultsFields.reranked_id] = doc_id 
             else:
-                result['_rerank_id'] = result['_id']
+                result[ResultsFields.reranked_id] = result[ResultsFields.id]
 
     def _get_searchable_columns(self):
         self.searchable_fields = [field for field in self.results_df.columns.tolist() if not field.startswith('_')]
@@ -94,89 +100,40 @@ class FormattedResults:
         # the first two will go to the model while the latter will be used to report the results
         inputs = []
         for field in searchable_fields:
-            _inputs_df = results_df[[field, '_rerank_id', '_score']]
-            _inputs_df['field_name'] = field
+            _inputs_df = results_df[[field, ResultsFields.reranked_id, ResultsFields.original_score]]
+            _inputs_df[Columns.field_name] = field
 
             # this is the case if we have some documents that do not have all the fields, 
-            # the empty fields go to na when converted to pandas
+            # the empty fields go to nan when converted to pandas
             _inputs_df = _inputs_df[_inputs_df[field].notna()] 
 
             if len(_inputs_df) > 0:
                 
-                _inputs_df['query'] = query
+                _inputs_df[Columns.query] = query
 
                 # we keep it in case we want to do hybrid search
-                if '_score' not in _inputs_df:
-                    _inputs_df['_score'] = 1.0
+                if ResultsFields.original_score not in _inputs_df:
+                    _inputs_df[ResultsFields.original_score] = 1.0
 
-                fields_to_keep = ['query', field, '_rerank_id', 'field_name', '_score']
+                fields_to_keep = [Columns.query, field, ResultsFields.reranked_id, Columns.field_name, ResultsFields.original_score]
 
                 inputs += _inputs_df[fields_to_keep].values.tolist()
             
-        inputs_df = pd.DataFrame(inputs, columns=['query', 'field_content', '_rerank_id', 'original_field_name',  '_score'])
+        inputs_df = pd.DataFrame(inputs, columns=[Columns.query, Columns.field_content, ResultsFields.reranked_id, Columns.original_field_name,  ResultsFields.original_score])
         
         return inputs_df
 
-
-    # def get_results_mapped_by_id(self):
-
-    #     for result in self.results['hits']:
-    #         doc_id = result['_rerank_id']
-    #         self.results_mapped_by_id[doc_id].append(result)
-
-    # def get_attributes(self):
-    #     # get the attributes of the documents
-    #     for result in self.results['hits']:
-    #         self.attributes = 1
-
-
-    # def get_highlighted_content(self):
-    #     # used when we have a non-empty highlights field
-    #     if len(self.results['hits']) == 0:
-    #         self.highlighted_content = None
-    #         return 
-
-    #     if self.highlights_field not in self.results['hits'][0]:
-    #         raise KeyError(f'could not find {self.highlights_field} field in results')
-
-    #     # we need to return if highlights are empty. we also assume that if the first record has no highlights, 
-    #     # then none do
-    #     if len(self.results['hits'][0][self.highlights_field]) == 0:
-    #         self.highlighted_content = None
-    #         return 
-
-    #     highlighted_keys = list(self.results['hits'][0][self.highlights_field].keys())
-    #     # should only be 1 highlight at the moment
-    #     if len(highlighted_keys) != 1:
-    #         raise RuntimeError(f"too many highlights, expected 1 but received {len(highlighted_keys)}")
-    #     self.highlighted_field = highlighted_keys[0]
-
-    #     # now get the dat ready for the re-ranker
-    #     self.highlighted_content = [data[self.highlighted_field] for data in self.results['hits']]
-    #     self.doc_ids = [data['_id'] for data in self.results['hits']]
-
-    #     # we want to output the content so it can be passed to a reranker
-    #     self.rerank_over_highlights_inputs = pd.DataFrame()
-
-    # # def get_all_content(self):
-    # #     # used to get content, and ids for passing to the model
-
-    # def _process(self):
 class ReRanker:
 
-    def __init__(self):
+    def __init__(self, ):
         pass 
-
 
     def load_model(self):
         pass
 
-    
     def format_results(self, results, query=None):
         self.results = results
-        self.formatted_results = FormattedResults(results)
-        
-
+        self.formatted_results = FormattedResults(self.results)
 
     def rerank(self, query, results):
         pass
@@ -184,24 +141,49 @@ class ReRanker:
 # TODO add generic HF models
 class ReRankerText(ReRanker):
 
-    def __init__(self, model_name: str, device: str = 'cpu', max_length: int = 512, num_highlights=1):
+    def __init__(self, model_name: str, device: str = 'cpu', max_length: int = 512, num_highlights=1, 
+                        split_params=get_default_text_processing_parameters()):
         super().__init__()
         self.model_name = model_name
         self.device = device
         self.max_length = max_length
         self.num_highlights = num_highlights
         self.formatted_results = None
-
+        self.split_params = split_params
         self.model = None
+
+        self.split_length = None
+        self.split_overlap = None
+        self.split_method = None
+
+        if self.split_params is not None and isinstance(self.split_params, (dict, defaultdict)):
+            self._extract_text_processing_parameters()
+
+    def _extract_text_processing_parameters(self):
+        self.split_length = self.split_params['split_length']
+        self.split_overlap = self.split_params['split_overlap']
+        self.split_method = self.split_params['split_method']
 
     def load_model(self):
 
-        self.model = load_sbert_cross_encoder_model(self.model_name, self.max_length)['model']
+        self.model = load_sbert_cross_encoder_model(model_name=self.model_name, 
+                            device=self.device, max_length=self.max_length)['model']
 
     @staticmethod
-    def _prepare_inputs(inputs_df, query_column='query', content_column='field_content'):
+    def _prepare_inputs(inputs_df, query_column=Columns.query, content_column=Columns.field_content):
         # TODO add in chunking option
         return inputs_df[[query_column, content_column]].values.tolist()
+
+    def explode_nested_content_field(self, inputs_df):
+        # used to allow chunking on text in the same way the inexing does it
+        _func = partial(text_processor.split_text, split_length=self.split_length, split_overlap=self.split_overlap, split_by=self.split_method)
+        inputs_df = inputs_df.merge(inputs_df[Columns.field_content].apply(_func).explode(), left_index=True, right_index=True)
+        inputs_df[Columns.field_content_original] = inputs_df[Columns.field_content + '_x']
+        inputs_df[Columns.field_content] = inputs_df[Columns.field_content + '_y']
+        del inputs_df[Columns.field_content + '_x']
+        del inputs_df[Columns.field_content + '_y']
+        
+        return inputs_df
 
     def rerank(self, query, results, searchable_attributes=None):
 
@@ -211,7 +193,7 @@ class ReRankerText(ReRanker):
         if not isinstance(results, (dict, defaultdict)):
             raise TypeError(f"expected a dict or defaultdict, received {type(results)}")
 
-        if len(results['hits']) == 0:
+        if len(results[ResultsFields.hits]) == 0:
             logger.warning("empty results for re-ranking. returning doing nothing...")
             return 
 
@@ -228,44 +210,55 @@ class ReRankerText(ReRanker):
 
         # now loop through each combination of query and searchable fields - could also just smash everything together here as well
         self.inputs_df = self.formatted_results.format_for_model(self.formatted_results.results_df, self.searchable_attributes, query=query)
+        
+        if self.split_params is not None:
+            _n = len(self.inputs_df)
+            self.inputs_df = self.explode_nested_content_field(self.inputs_df)
+            logger.info(f"chunking field content, went from length {_n} to {len(self.inputs_df)}")
+        
         # TODO chunk the text and send to model
         self.model_inputs = self._prepare_inputs(self.inputs_df)
 
+        if not _verify_model_inputs(self.model_inputs):
+            raise RuntimeError(f"incorrect model inputs, expected list of lists but recevied {type(self.model_inputs)} and {type(self.model_inputs[0])}")
+
         self.scores = self.model.predict(self.model_inputs)
 
-        self.inputs_df['_score_rerank'] = self.scores
-        self.inputs_df['_score_multiply'] = np.clip(self.inputs_df['_score'], 1e-3, np.inf)*np.clip(self.inputs_df['_score_rerank'], 1e-3, np.inf)
-        self.inputs_df['_score_add'] = self.inputs_df['_score'] + self.inputs_df['_score_rerank']
+        self.inputs_df[ResultsFields.reranker_score] = self.scores
+        self.inputs_df[ResultsFields.hybrid_score_multiply] = np.clip(self.inputs_df[ResultsFields.original_score], 1e-3, np.inf)*np.clip(self.inputs_df[ResultsFields.reranker_score], 1e-3, np.inf)
+        self.inputs_df[ResultsFields.hybrid_score_add] = self.inputs_df[ResultsFields.original_score] + self.inputs_df[ResultsFields.reranker_score]
 
         self.get_reranked_results()
 
-    def get_reranked_results(self, score_column='_score_rerank'):
+    def get_reranked_results(self, score_column=ResultsFields.reranker_score):
         
         reranked_top = []
-        for i,group in self.inputs_df.groupby('_rerank_id'):
+        for i,group in self.inputs_df.groupby(ResultsFields.reranked_id):
             group = group.sort_values(score_column, ascending=False).head(self.num_highlights)
             reranked_top.append(group)
-        reranked_top = pd.concat(reranked_top).set_index('_rerank_id')
+        reranked_top = pd.concat(reranked_top).set_index(ResultsFields.reranked_id)
         
         # find out which document is the highest, then do the field as a highlight
-        for result in self.results['hits']:
-            rerank_id = result['_rerank_id']
+        for result in self.results[ResultsFields.hits]:
+            rerank_id = result[ResultsFields.reranked_id]
             if self.num_highlights == 1:
                 _score = reranked_top.loc[rerank_id][score_column]
             else:
                 _score = reranked_top.loc[rerank_id][score_column].values.tolist()
                 
-            result['_score_rerank'] = _score
+            result[ResultsFields.reranker_score] = _score
             _df = reranked_top.loc[rerank_id]
-            #import ipdb;ipdb.set_trace()
+             
             if self.num_highlights == 1:
-                _content = [{_df['original_field_name']:_df['field_content']}]
+                _content = {_df[Columns.original_field_name]:_df[Columns.field_content]}
             else:
-                _content = [{row['original_field_name']:row['field_content']} for _,row in _df.iterrows()]
+                _content = [{row[Columns.original_field_name]:row[Columns.field_content]} for _,row in _df.iterrows()]
 
-            result['_highlights_rerank'] = _content
+            result[ResultsFields.highlights_reranked] = _content
 
-        self.results['hits'] = sorted(self.results['hits'], key=lambda x:x['_score_rerank'], reverse=True)
+        self.results[ResultsFields.hits] = sorted(self.results[ResultsFields.hits], key=lambda x:x[ResultsFields.reranker_score], reverse=True)
+
+
 
 class ReRankerOwl(ReRanker):
 # we might need the index config to get the processing params
@@ -297,7 +290,7 @@ class ReRankerOwl(ReRanker):
 
     def rerank(self, query, results):
         t_start = time.time()
-        self.content, self.ids = self.get_data(results, highlights_field=HIGHLIGHTS_FIELD)
+        self.content, self.ids = self.get_data(results, highlights_field=ResultsFields.highlights)
         t_content = time.time()
         self.images = self.load_images(self.content, self.image_size)
         t_image_loaded = time.time()
@@ -321,7 +314,7 @@ class ReRankerOwl(ReRanker):
         scores = torch.cat(_s)
         self.identifier = _i
         images = _im
-        #import ipdb;ipdb.set_trace()
+         
         self.boxes, self.scores, self.identifier = sort_owl_boxes_scores(boxes, scores, self.identifier)
         t_sort = time.time()
 
@@ -331,18 +324,11 @@ class ReRankerOwl(ReRanker):
         # TODO prepatch if we want 
         # TODO take top n from each one
         results['reranked'] = {}
-        results['reranked']['hits'] = []
-        results['reranked']['hits'] = [{'boxes':self.boxes, 'scores':self.scores, 'identifier':self.identifier}]
+        results['reranked'][ResultsFields.hits] = []
+        results['reranked'][ResultsFields.hits] = [{'boxes':self.boxes, 'scores':self.scores, 'identifier':self.identifier}]
         results['reranked']['processTime'] = timings
 
         return results
-
-def rerank_search_results(search_result, query, model_name, device, 
-                searchable_attributes=None, num_highlights=1):
-
-    # TODO add in routing and move this into another file for interfacing
-    reranker = ReRankerText(model_name, device=device, num_highlights=num_highlights)
-    reranker.rerank(query=query, results=search_result, searchable_attributes=searchable_attributes)
 
 
 
@@ -354,7 +340,7 @@ def _load_image(filename, size=None):
         im = Image.open(requests.get(filename, stream=True).raw)
     else:
         im = Image.open(filename)    
-    #import ipdb;ipdb.set_trace()
+     
     if size is None:
         size = im.size
     #im.draft('RGB', size)
