fe56f96e9577e27b4f9d4eedcd6ddd7293c00cb7
Pandu Oliver Kerr <pandu@s2search.io>
2022-09-16 10:37:23 +1000

    fix: checking if TARGETPLATFORM is defined

--- scripts/install_onnx_gpu_for_amd.sh
+++ scripts/install_onnx_gpu_for_amd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This script is meant to be run at buildtime. This is because onnxruntime-gpu
 # doesn't install on arm64
-if [[ "$TARGETPLATFORM" ]]; then
-  echo environment var TARGETPLATFORM is undefined
+if ! [[ "$TARGETPLATFORM" ]]; then
+  echo ERROR environment var TARGETPLATFORM is undefined
   exit 1
 fi
 
