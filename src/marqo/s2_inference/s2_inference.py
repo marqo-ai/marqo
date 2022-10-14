c68a3d494698b378296454a61e9cec8d9af6e5a7
aryanagarwal9 <59826369+aryanagarwal9@users.noreply.github.com>
2022-10-14 04:12:50 +0100

    add model_property validation

--- src/marqo/s2_inference/s2_inference.py
+++ src/marqo/s2_inference/s2_inference.py
@@ -13,12 +13,13 @@ logger = get_logger(__name__)
 available_models = dict()
 MODEL_PROPERTIES = load_model_properties()
 
-def vectorise(model_name: Union[str, dict], content: Union[str, List[str]], device: str = get_default_device(),
+
+def vectorise(model_name: str, content: Union[str, List[str]], model_properties: dict = None, device: str = get_default_device(),
                                     normalize_embeddings: bool = get_default_normalization(), **kwargs) -> List[List[float]]:
     """vectorizes the content by model name
 
     Args:
-        model (str | dict): _description_
+        model_name (str) : _description_
         content (_type_): _description_
 
     Returns:
@@ -28,14 +29,9 @@ def vectorise(model_name: Union[str, dict], content: Union[str, List[str]], devi
         VectoriseError: if the content can't be vectorised, for some reason.
     """
 
-    model = model_name
-    model_name = _get_model_name(model)
-
     model_cache_key = _create_model_cache_key(model_name, device)
 
-    _update_model_properties(model_name, model)
-
-    _update_available_models(model_cache_key, model_name, device, normalize_embeddings)
+    _update_available_models(model_cache_key, model_name, model_properties, device, normalize_embeddings)
 
     try:
         vectorised = available_models[model_cache_key].encode(content, normalize=normalize_embeddings, **kwargs)
@@ -43,16 +39,6 @@ def vectorise(model_name: Union[str, dict], content: Union[str, List[str]], devi
         raise VectoriseError from e
 
     return _convert_vectorized_output(vectorised)
-
-
-def _get_model_name(model: Union[str, dict]) -> str:
-    """returns the name of the model
-    """
-    if isinstance(model, str):
-        return model
-    elif isinstance(model, dict):
-        _check_model_dict(model)
-        return model['name']
 
 
 def _create_model_cache_key(model_name: str, device: str) -> Tuple[str, str]:
@@ -70,45 +56,40 @@ def _create_model_cache_key(model_name: str, device: str) -> Tuple[str, str]:
     return model_cache_key
 
 
-def _check_model_dict(model: dict) ->  bool:
-    """checks model dict to see if all required keys are present
-    """
-    required_keys = ["name", "dimensions"]
-    for key in required_keys:
-        if key not in model:
-            raise InvalidModelSettingsError(f'model {key} not in settings')
-    else:
-        return True
-
-
-def _update_model_dict(model: dict) -> dict:
-    """updates model dict with default values if optional keys are missing
-    """
-    optional_keys_values = [("type", "sbert"), ("tokens", 128)]
-    for key, value in optional_keys_values:
-        if key not in model:
-            model[key] = value
-
-    return model
-
-def _update_model_properties(model_name: str, model: Union[str, dict], all_model_properties = MODEL_PROPERTIES) -> None:
-    """updates MODEL_PROPERTIES if model details are specified by user
-    """
-    if isinstance(model, dict):
-        _model = _update_model_dict(model)
-        model_properties = {model_name: _model}
-        all_model_properties['models'].update(model_properties)
-
-
-def _update_available_models(model_cache_key: str, model_name: str, device: str, normalize_embeddings: bool) -> None:
+def _update_available_models(model_cache_key: Tuple[str, str], model_name: str, model_properties: dict, device: str, normalize_embeddings: bool) -> None:
     """loads the model if it is not already loaded
     """
     if model_cache_key not in available_models:
         try:
-            available_models[model_cache_key] = _load_model(model_name, device=device)
+            validated_model_properties = _validate_model_properties(model_name, model_properties)
+            available_models[model_cache_key] = _load_model(validated_model_properties['name'], validated_model_properties, device=device)
             logger.info(f'loaded {model_name} on device {device} with normalization={normalize_embeddings}')
         except:
             raise ModelLoadError(f"Unable to load model={model_name} on device={device} with normalization={normalize_embeddings}")
+
+
+def _validate_model_properties(model_name: str, model_properties: dict) -> dict:
+    """validate model_properties, if not given then return model_registry properties
+    """
+    if model_properties is not None:
+        """checks model dict to see if all required keys are present
+        """
+        required_keys = ["name", "dimensions"]
+        for key in required_keys:
+            if key not in model_properties:
+                raise InvalidModelSettingsError(f'model {key} not in settings')
+
+        """updates model dict with default values if optional keys are missing
+        """
+        optional_keys_values = [("type", "sbert"), ("tokens", get_default_seq_length())]
+        for key, value in optional_keys_values:
+            if key not in model_properties:
+                model_properties[key] = value
+
+    else:
+        model_properties = MODEL_PROPERTIES['models'][model_name]
+
+    return model_properties
 
 
 def clear_loaded_models() -> None:
@@ -117,7 +98,7 @@ def clear_loaded_models() -> None:
     available_models.clear()
 
 
-def get_model_properties(model_name: str) -> dict:
+def get_model_properties_from_registry(model_name: str) -> dict:
     """ Returns a dict describing properties of a model.
 
     These properties will be used by the tensor_search application to set up
@@ -236,7 +217,7 @@ def _convert_vectorized_output(output: Union[FloatTensor, ndarray, List[List[flo
     raise TypeError(f"unable to convert input of type {type(output)} to a list of lists of floats")
 
 
-def _get_model_loader(model_name: str) -> Any:
+def _get_model_loader(model_name: str, model_properties: dict) -> Any:
     """ Returns a dict describing properties of a model.
 
     These properties will be used by the tensor_search application to set up
@@ -250,10 +231,8 @@ def _get_model_loader(model_name: str) -> Any:
         dict: a dictionary describing properties of the model.
     """
 
-    if model_name not in MODEL_PROPERTIES['models']:
-        raise KeyError(f"model_name={model_name} not in allowed model names")
 
-    model_type = MODEL_PROPERTIES['models'][model_name]['type']
+    model_type = model_properties['type']
 
     if model_type not in MODEL_PROPERTIES['loaders']:
         raise KeyError(f"model_name={model_name} for model_type={model_type} not in allowed model types")
@@ -261,7 +240,7 @@ def _get_model_loader(model_name: str) -> Any:
     return MODEL_PROPERTIES['loaders'][model_type]
 
 
-def _load_model(model_name: str, device: str = get_default_device()) -> Any:
+def _load_model(model_name: str, model_properties: dict, device: str = get_default_device()) -> Any:
     """_summary_
 
     Args:
@@ -272,13 +251,11 @@ def _load_model(model_name: str, device: str = get_default_device()) -> Any:
         Any: _description_
     """
 
-    model_properties = get_model_properties(model_name)
-
-    loader = _get_model_loader(model_name)
+    loader = _get_model_loader(model_name, model_properties)
 
     max_sequence_length = model_properties.get('tokens', get_default_seq_length())
 
-    model = loader(model_properties['name'], device=device, embedding_dim=model_properties['dimensions'],
+    model = loader(model_name, device=device, embedding_dim=model_properties['dimensions'],
                     max_seq_length=max_sequence_length)
 
     model.load()
