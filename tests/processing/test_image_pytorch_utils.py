e3987d94458d7ffc2165581c554299bbcf7034ac
Jesse Clark <jesse@s2search.io>
2022-11-15 13:04:54 +1100

    pytorch utils test

---
+++ tests/processing/test_image_pytorch_utils.py
@@ -0,0 +1,31 @@
+import unittest
+import tempfile
+import os
+
+import numpy as np
+from PIL import Image
+ 
+
+from marqo.s2_inference.types import List, Dict, ImageType
+
+from marqo.s2_inference.processing.pytorch_utils import (
+    load_pytorch,
+    get_default_rcnn_params
+)
+
+
+class TestImageUtils(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.device = 'cpu'
+    
+    def test_get_rcnn_params(self):
+
+        params = get_default_rcnn_params()
+        assert isinstance(params, Dict)
+
+        assert len(params) >= 1
+
+    def test_load_pytorch(self):
+
+        model, preprocess = load_pytorch(model_name='frcnn', device=self.device)
