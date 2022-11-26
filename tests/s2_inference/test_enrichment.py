d72f221ddbe1add7e765affec1621b9808336441
Jesse Clark <jesse@s2search.io>
2022-11-26 15:30:36 +1100

    update tests

--- tests/s2_inference/test_enrichment.py
+++ tests/s2_inference/test_enrichment.py
@@ -1,12 +1,55 @@
 import unittest
 import os
-from marqo.s2_inference.enrichment.vqa_utils import _process_kwargs_vqa
-
+from marqo.s2_inference.enrichment.vqa_utils import (
+    _process_kwargs_vqa,
+    _check_output_type,
+    convert_to_bool,
+    get_allowed_vqa_tasks,
+    VQA
+)
 class TestEncoding(unittest.TestCase):
 
     def setUp(self) -> None:
 
         pass
+
+    def test_check_vqa_output_type(self):
+        
+        outputs = [
+            [],
+            [1],
+            ['a'],
+            [0],
+            [.1],
+            [1,'a',.1],
+            ['a', 'asasas']
+        ]
+        for output in outputs:
+            assert _check_output_type(output)
+
+        outputs = [
+            'a',
+            1,
+            None, 
+            (1)
+        ]
+        for output in outputs:
+            assert not _check_output_type(output)
+
+    def test_convert_to_bool(self):
+        
+        answers = ['yes', True, 'True', 'true', 1, '1', 'y', 't']
+        for answer in answers:
+            assert convert_to_bool(answer)
+
+        answers = ['no', False, 'False', 'false', 0, '0', 'n', 'f']
+        for answer in answers:
+            assert not convert_to_bool(answer)
+
+    def test_allowed_vqa_tasks(self):
+        allowed_tasks = get_allowed_vqa_tasks()
+        assert "attribute-extraction" in allowed_tasks 
+        assert "question-answer" in allowed_tasks 
 
     def test_process_kwargs_vqa(self):
 
@@ -79,3 +122,31 @@ class TestEncoding(unittest.TestCase):
         assert image_query_pairs == expected_output
 
         # TODO add tests for erros
+
+    def test_vqa_model(self):
+        
+        model = VQA()
+        model.load()
+        
+        task = "attribute-extraction"
+        # output from _parse_kwargs in enrichment.py
+        parsed_kwargs = {
+                "attributes": ["hippo, water, car, truck"],
+                "image_field": "https://raw.githubusercontent.com/marqo-ai/marqo-api-tests/mainline/assets/ai_hippo_realistic.png",
+            }
+
+        model.predict(task, [], **parsed_kwargs)
+
+        assert model.answers == [True, True, False, False]
+
+        task = "question-answer"
+
+        # output from _parse_kwargs in enrichment.py
+        parsed_kwargs = {
+                "query": "how many hippos are in the picture?",
+                "image_field": "https://raw.githubusercontent.com/marqo-ai/marqo-api-tests/mainline/assets/ai_hippo_realistic.png",
+            }
+
+        model.predict(task, [], **parsed_kwargs)
+
+        assert model.answers == ['one']
