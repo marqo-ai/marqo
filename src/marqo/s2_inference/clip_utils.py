87a87c9148e1ef6f04bd8f891663f9d86d31e78c
Li Wan <lwan3@student.unimelb.edu.au>
2022-11-28 14:58:47 +1100

    load device to self.device in clip

--- src/marqo/s2_inference/clip_utils.py
+++ src/marqo/s2_inference/clip_utils.py
@@ -163,20 +163,19 @@ class CLIP:
         self.model = self.model.to(self.device)
         self.tokenizer = clip.tokenize
         self.model.eval()
-    
+
     def _convert_output(self, output):
-        print(output.dtype)
         if self.device == 'cpu':
             start = timer()
             output = output.numpy()
             end = timer()
-            logger.info(f"It takes {(end - start):.3f}s to convert the output to ndarray from cpu")
+            logger.info(f"It takes {(end - start):.3f}s to convert the output with {output.dtype} to ndarray from cpu")
             return output
         elif self.device.startswith('cuda'):
             start = timer()
             output = output.cpu().numpy()
             end = timer()
-            logger.info(f"It takes {(end - start):.3f}s to convert the output to ndarray from cuda")
+            logger.info(f"It takes {(end - start):.3f}s to convert the output with {output.dtype} to ndarray from cuda")
             return output
 
     @staticmethod
