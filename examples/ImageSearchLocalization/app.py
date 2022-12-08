f644d43b621aec9e5143930386d717ff402845c3
jnc <jessenclark@gmail.com>
2022-12-08 15:49:31 +1100

    clean up

--- examples/ImageSearchLocalization/app.py
+++ examples/ImageSearchLocalization/app.py
@@ -132,19 +132,15 @@ def main():
 
     image_file = s3.file_uploader("Upload Images", type=["png", "jpg", "jpeg"])
 
-    # search with images and text?
     if submit1:
         st.text("searching using '{}'...".format(sentence))
         res = client.index(index_name).search("{}".format(sentence), searchable_attributes=[image_location], reranker=reranker, device=device)
 
-        # get the image 
         if len(res[hits]) == 0:
             st.text(f"No results found for {sentence}")
         else:
-            # get the image 
             images = [i[image_location].replace(docker_image_server_prefix, local_image_location) for i in res['hits']]
-            # render text
-            render_images(images) #, boxes=boxes)
+            render_images(images) 
  
     if submit2:       
         st.text("searching using '{}'...".format(sentence))        
