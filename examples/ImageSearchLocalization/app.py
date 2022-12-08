debc93ab774d87c159068040c153a3abfefd4b15
jnc <jessenclark@gmail.com>
2022-12-08 15:18:56 +1100

    clean up and rename

--- examples/ImageSearchLocalization/app.py
+++ examples/ImageSearchLocalization/app.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageDraw
 import streamlit as st
 from tempfile import NamedTemporaryFile
 import validators
@@ -48,7 +48,7 @@ def render_images(images, captions=None, boxes=None):
     if captions is None:
         st.image(images, use_column_width=False, width=230)
     else:
-        st.image(images, caption=captions, use_column_width=False)#, width=230)
+        st.image(images, caption=captions, use_column_width=False)
 
 
 def load_image(image_file):
@@ -64,23 +64,38 @@ def load_image(image_file):
     return img
 
 def draw_box(img, box):
-    import math
-    from PIL import Image, ImageDraw
-        
-    # create rectangle image
+    """draws a rectangle box on a PIL image
+
+    Args:
+        img (_type_): _description_
+        box (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     img1 = ImageDraw.Draw(img)  
     img1.rectangle(box, outline  = 'orange', width = 8)
     return img
 
 def main():
 
-    data_dir = "/home/jesse/code/stable-diffusion/scripts/coco/"
+    #########################################
+    # SETUP SOME PARAMTERS
+    # this is for temporary storage of an image
+    temp_image_dir = os.getcwd() + '/'
+    # the field name for the image
     image_location = 'image_location'
+    # some enums
     highlights = '_highlights'
+    hits = 'hits'
+
+    # specify the device
     device = 'cuda'
     
-    docker_prefix = 'http://host.docker.internal:8222/'
-    locale_location = '/home/jesse/code/stable-diffusion/scripts/coco/'
+    docker_image_server_prefix = 'http://host.docker.internal:8222/'
+    local_image_location = os.getcwd() + '/images/'
+
+    #########################################
 
     st.title("image search with localisation")
 
@@ -88,90 +103,59 @@ def main():
 
     s1,s2,s3,s4,s5 = st.columns(5)
 
-    options = ['None',
-                   'yolox',
-                   'dino/v2',
-                   'dino/v1',
-                   'frcnn'
-                   ]
-    option = s4.radio('Select three known variables:',
-                  options)
+    options = ['None', 'yolox', 'dino/v2']
 
-    print(option)
+    option = s4.radio('Select the indexing method', options)
 
     if option == options[0]:
-
         index_name = 'visual-search'
-
     elif option == options[1]:
-
         index_name = 'visual-search-yolox'
-    
     elif option == options[2]:
-
         index_name = 'visual-search-dino-v2'
+    else:
+        raise ValueError(f"unexpected option for {option}")
 
-    elif option == options[3]:
-
-        index_name = 'visual-search-dino-v1'
-
-    elif option == options[4]:
-
-        index_name = 'visual-search-frcnn'
-
-    print(index_name)
-    option_reranker = s5.radio('Select three known variables:',
-                  ['None',
-                   "google/owlvit-base-patch32",
-                   "google/owlvit-base-patch16",
-                   "google/owlvit-large-patch14"
-                   ])
+    option_reranker = s5.radio('Select the reranker',
+                  ['None', "google/owlvit-base-patch32"])
 
     if option_reranker == 'None':
         reranker = None
     else:
         reranker = option_reranker
-    print(option_reranker)
+
     form1 = s1.form(key='my-form1')
     submit1 = form1.form_submit_button('Search with tensor...')
 
     form2 = s2.form(key='my-form2')
     submit2 = form2.form_submit_button('Search with tensor localisation...')
 
-    image_file = s3.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+    image_file = s3.file_uploader("Upload Images", type=["png", "jpg", "jpeg"])
 
     # search with images and text?
     if submit1:
         st.text("searching using '{}'...".format(sentence))
-        
         res = client.index(index_name).search("{}".format(sentence), searchable_attributes=[image_location], reranker=reranker, device=device)
-        # print(res)
-        print(res, len(res['hits'][0]))
+
         # get the image 
-        if len(res['hits']) == 0:
+        if len(res[hits]) == 0:
             st.text(f"No results found for {sentence}")
         else:
             # get the image 
-            images = [i[image_location].replace(docker_prefix, locale_location) for i in res['hits']]
-            #boxes = [i[highlights][image_location] for i in res['hits']]
+            images = [i[image_location].replace(docker_image_server_prefix, local_image_location) for i in res['hits']]
             # render text
             render_images(images) #, boxes=boxes)
  
-    if submit2:
-       
-        st.text("searching using '{}'...".format(sentence))
-        
+    if submit2:       
+        st.text("searching using '{}'...".format(sentence))        
         res = client.index(index_name).search("{}".format(sentence), searchable_attributes=[image_location], reranker=reranker, device=device)
-        # print(res)
-        print(res, len(res['hits'][0]))
         # get the image 
-        if len(res['hits']) == 0:
+        if len(res[hits]) == 0:
             st.text(f"No results found for {sentence}")
         else:
             # get the image 
-            images = [i[image_location].replace(docker_prefix, locale_location) for i in res['hits']]
- 
-            boxes = [i['_highlights'][image_location] for i in res['hits']]
+            images = [i[image_location].replace(docker_image_server_prefix, local_image_location) for i in res['hits']]
+            boxes = [i[highlights][image_location] for i in res[hits]]
 
             # render text
             render_images(images, boxes=boxes)
@@ -182,8 +166,8 @@ def main():
         temp_file = NamedTemporaryFile(delete=False)
         if image_file:
             temp_file.write(image_file.getvalue())
-            #st.write(load_image(temp_file.name))
-        save_name = f'{data_dir}temp.png'
+           
+        save_name = f'{temp_image_dir}temp.png'
         image = load_image(temp_file.name)
         image.save(save_name)
         
@@ -193,14 +177,14 @@ def main():
 
         res = client.index(index_name).search("{}".format(query), searchable_attributes=[image_location], 
                                          limit=9, device=device)
-        print(res, len(res['hits']))
+
         # get the image 
         if len(res['hits']) == 0:
             st.text(f"No results found for {sentence}")
         else:
-            images = [i[image_location].replace(docker_prefix, locale_location) for i in res['hits']]
+            images = [i[image_location].replace(docker_image_server_prefix, local_image_location) for i in res['hits']]
 
-            boxes = [i['_highlights'][image_location] for i in res['hits']]
+            boxes = [i[highlights][image_location] for i in res[hits]]
 
             # render text
             render_images(images, boxes=boxes)
