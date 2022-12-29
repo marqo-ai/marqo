9aa15a952b72f08d9035411485b565a56faee423
Li Wan <lwan3@student.unimelb.edu.au>
2022-12-30 09:07:40 +1100

    reduce a model for testing stability

--- tests/tensor_search/test_model_cache_management.py
+++ tests/tensor_search/test_model_cache_management.py
@@ -35,9 +35,9 @@ class TestModelCacheManagement(MarqoTestCase):
 
         # We loaded 6 models (3 in cuda, 3 in cpu) as initial setup
         if self.CUDA_FLAG:
-            assert len(available_models) >= 6
+            assert len(available_models) >= 4
         else:
-            assert len(available_models) >= 3
+            assert len(available_models) >= 2
 
     def tearDown(self) -> None:
         clear_loaded_models()
