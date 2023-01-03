e453e7738222ea426d7c6692dc11f1c75548ca85
Li Wan <lwan3@student.unimelb.edu.au>
2023-01-03 17:16:41 +1100

    onnx/open_clip/ViT-B-32/openai

--- src/marqo/s2_inference/model_registry.py
+++ src/marqo/s2_inference/model_registry.py
@@ -627,6 +627,38 @@ def _get_onnx_clip_properties() -> Dict:
                 "image_std": None,
             },
 
+        "onnx32/open_clip/ViT-B-32/openai":
+            {
+                "name": "onnx32/open_clip/ViT-B-32/openai",
+                "dimensions": 512,
+                "type": "clip_onnx",
+                "note": "the onnx float32 version of open_clip ViT-B-32/openai",
+                "repo_id": "Marqo/onnx-open_clip-ViT-B-32",
+                "visual_file": "onnx32-open_clip-ViT-B-32-openai-visual.onnx",
+                "textual_file": "onnx32-open_clip-ViT-B-32-openai-textual.onnx",
+                "token": None,
+                "resolution": 224,
+                "pretrained": "openai",
+                "image_mean": None,
+                "image_std": None,
+            },
+
+        "onnx16/open_clip/ViT-B-32/openai":
+            {
+                "name": "onnx16/open_clip/ViT-B-32/openai",
+                "dimensions": 512,
+                "type": "clip_onnx",
+                "note": "the onnx float16 version of open_clip ViT-B-32/openai",
+                "repo_id": "Marqo/onnx-open_clip-ViT-B-32",
+                "visual_file": "onnx16-open_clip-ViT-B-32-openai-visual.onnx",
+                "textual_file": "onnx16-open_clip-ViT-B-32-openai-textual.onnx",
+                "token": None,
+                "resolution": 224,
+                "pretrained": "openai",
+                "image_mean": None,
+                "image_std": None,
+            },
+
 
 
 
