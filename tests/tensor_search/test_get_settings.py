6905000f651ad28729e5183c8711583409ee7800
aryanagarwal9 <59826369+aryanagarwal9@users.noreply.github.com>
2023-01-05 14:10:56 +0530

    Update test_get_settings.py

--- tests/tensor_search/test_get_settings.py
+++ tests/tensor_search/test_get_settings.py
@@ -6,8 +6,8 @@ from marqo.errors import IndexNotFoundError
 class TestGetSettings(MarqoTestCase):
 
     def setUp(self) -> None:
-        self.generic_header = {"Content-type": "application/json"}
-        self.index_name = "my-test-index-1"
+        self.generic_header = {'Content-type': 'application/json'}
+        self.index_name = 'my-test-index-1'
         try:
             tensor_search.delete_index(config=self.config, index_name=self.index_name)
         except IndexNotFoundError as s:
@@ -17,91 +17,48 @@ class TestGetSettings(MarqoTestCase):
         self.assertRaises(IndexNotFoundError, backend.get_index_info, config=self.config, index_name=self.index_name)
 
     def test_default_settings(self):
-        tensor_search.create_vector_index(config=self.config, index_name=self.index_name)
-
-        test_index_info = list(backend.get_index_info(config=self.config, index_name=self.index_name))
-        index_info = ['hf/all_datasets_v4_MiniLM-L6',
-                      {'__chunks': {'type': 'nested', 'properties': {'__field_content': {'type': 'text'},
-                                                                     '__field_name': {
-                                                                         'type': 'keyword'}}}},
-                      {'index_defaults': {'treat_urls_and_pointers_as_images': False,
+        """default fields should be returned if index is created with default settings
+            sample structure of output: {'index_defaults': {'treat_urls_and_pointers_as_images': False,
                                           'text_preprocessing': {'split_method': 'sentence', 'split_length': 2,
                                                                  'split_overlap': 0},
                                           'model': 'hf/all_datasets_v4_MiniLM-L6', 'normalize_embeddings': True,
-                                          'image_preprocessing': {'patch_method': None}}, 'number_of_shards': 5}]
+                                          'image_preprocessing': {'patch_method': None}}, 'number_of_shards': 5}
+        """
+        tensor_search.create_vector_index(config=self.config, index_name=self.index_name)
 
-        self.assertEqual(index_info, test_index_info)
+        index_info = backend.get_index_info(config=self.config, index_name=self.index_name)
+        index_settings = index_info.index_settings
+        fields = {'treat_urls_and_pointers_as_images', 'text_preprocessing', 'model', 'normalize_embeddings',
+                  'image_preprocessing'}
+
+        self.assertIn('index_defaults', index_settings)
+        self.assertIn('number_of_shards', index_settings)
+        self.assertTrue(fields.issubset(set(index_settings['index_defaults'])))
 
     def test_custom_settings(self):
-        model_properties = {"name": "sentence-transformers/multi-qa-MiniLM-L6-cos-v1",
-                            "dimensions": 384,
-                            "tokens": 128,
-                            "type": "sbert"}
+        """adding custom settings to the index should be reflected in the returned output
+        """
+        model_properties = {'name': 'sentence-transformers/multi-qa-MiniLM-L6-cos-v1',
+                            'dimensions': 384,
+                            'tokens': 128,
+                            'type': 'sbert'}
 
         index_settings = {
-            "index_defaults": {
-                "treat_urls_and_pointers_as_images": False,
-                "model": "test-model",
-                "model_properties": model_properties,
-                "normalize_embeddings": True,
+            'index_defaults': {
+                'treat_urls_and_pointers_as_images': False,
+                'model': 'test-model',
+                'model_properties': model_properties,
+                'normalize_embeddings': True,
             }
         }
 
         tensor_search.create_vector_index(config=self.config, index_name=self.index_name, index_settings=index_settings)
 
-        test_index_info = list(backend.get_index_info(config=self.config, index_name=self.index_name))
-        index_info = ['test-model', {'__chunks': {'type': 'nested', 'properties': {'__field_content': {'type': 'text'},
-                                                                                  '__field_name': {
-                                                                                      'type': 'keyword'}}}}, {
-                          'index_defaults': {'treat_urls_and_pointers_as_images': False,
-                                             'text_preprocessing': {'split_method': 'sentence', 'split_length': 2,
-                                                                    'split_overlap': 0}, 'model_properties': {
-                                  'name': 'sentence-transformers/multi-qa-MiniLM-L6-cos-v1', 'tokens': 128,
-                                  'type': 'sbert', 'dimensions': 384}, 'model': 'test-model',
-                                             'normalize_embeddings': True,
-                                             'image_preprocessing': {'patch_method': None}}, 'number_of_shards': 5}]
+        index_info = backend.get_index_info(config=self.config, index_name=self.index_name)
+        index_settings = index_info.index_settings
+        fields = {'treat_urls_and_pointers_as_images', 'text_preprocessing', 'model', 'normalize_embeddings',
+                  'image_preprocessing', 'model_properties'}
 
-        self.assertEqual(index_info, test_index_info)
-
-    def test_settings_with_documents(self):
-        tensor_search.create_vector_index(config=self.config, index_name=self.index_name)
-        tensor_search.add_documents(config=self.config, index_name=self.index_name, docs=[
-            {
-                "Title": "test-title",
-                "Description": "test-desc",
-                "_id": "1"
-            }], auto_refresh=True)
-
-        test_index_info = list(backend.get_index_info(config=self.config, index_name=self.index_name))
-        index_info = ['hf/all_datasets_v4_MiniLM-L6', {'Description': {'type': 'text'}, 'Title': {'type': 'text'},
-                                                       '__chunks': {'type': 'nested', 'properties': {
-                                                           'Description': {'type': 'keyword', 'ignore_above': 32766},
-                                                           'Title': {'type': 'keyword', 'ignore_above': 32766},
-                                                           '__field_content': {'type': 'text'},
-                                                           '__field_name': {'type': 'keyword'},
-                                                           '__vector_Description': {'type': 'knn_vector',
-                                                                                    'dimension': 384,
-                                                                                    'method': {'engine': 'lucene',
-                                                                                               'space_type': 'cosinesimil',
-                                                                                               'name': 'hnsw',
-                                                                                               'parameters': {
-                                                                                                   'ef_construction': 128,
-                                                                                                   'm': 16}}},
-                                                           '__vector_Title': {'type': 'knn_vector', 'dimension': 384,
-                                                                              'method': {'engine': 'lucene',
-                                                                                         'space_type': 'cosinesimil',
-                                                                                         'name': 'hnsw', 'parameters': {
-                                                                                      'ef_construction': 128,
-                                                                                      'm': 16}}}}}}, {
-                          'index_defaults': {'treat_urls_and_pointers_as_images': False,
-                                             'text_preprocessing': {'split_method': 'sentence', 'split_length': 2,
-                                                                    'split_overlap': 0},
-                                             'model': 'hf/all_datasets_v4_MiniLM-L6', 'normalize_embeddings': True,
-                                             'image_preprocessing': {'patch_method': None}}, 'number_of_shards': 5}]
-
-        self.assertEqual(index_info, test_index_info)
-
-        # deleting docs should not change index settings
-        tensor_search.delete_documents(config=self.config, index_name=self.index_name, doc_ids=["1"],
-                                       auto_refresh=True)
-        self.assertEqual(index_info, test_index_info)
+        self.assertIn('index_defaults', index_settings)
+        self.assertIn('number_of_shards', index_settings)
+        self.assertTrue(fields.issubset(set(index_settings['index_defaults'])))
