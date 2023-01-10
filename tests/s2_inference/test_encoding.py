2136fa04387ace77f2ff23db390e51751186531d
Li Wan <lwan3@student.unimelb.edu.au>
2023-01-10 18:34:31 +1100

    add unit test for multilingual clip

--- tests/s2_inference/test_encoding.py
+++ tests/s2_inference/test_encoding.py
@@ -289,8 +289,8 @@ class TestEncoding(unittest.TestCase):
         image = "https://raw.githubusercontent.com/marqo-ai/marqo-clip-onnx/main/examples/coco.jpg"
         e = 0.1
         for name in names:
-            text = vectorise(model_name=name, content=texts, normalize_embeddings=True)
-            image = vectorise(model_name=name, content=image, normalize_embeddings=True)
+            text = vectorise(model_name=name, content=texts, normalize_embeddings=True, device=device)
+            image = vectorise(model_name=name, content=image, normalize_embeddings=True, device=device)
 
             text_feature = np.array(text)
             image_feature = np.array(image)
