9635bb677b08c756786fa55c3980f150c34a5df6
Pandu Oliver Kerr <pandu@s2search.io>
2023-01-16 11:03:21 +1100

    added caveat to docstring

--- src/marqo/tensor_search/add_docs.py
+++ src/marqo/tensor_search/add_docs.py
@@ -29,12 +29,23 @@ def download_images(docs: List[dict], thread_count: int) -> List[dict]:
     """Concurrently downloads images from each doc.
 
     This should be called only if treat URLs as images is True
+
+    Note: double retrieving of ungettable images:
+        Images that can't be found are ignored and not recorded. This assumes that
+        a later function will process images again, and again attempt to get these
+        ungettable images - at that point will the failure be recorded to be
+        returned to the user.
+
+        This decision was made, because the alternative assumes that we'll replace
+        the image URL with some other content - like None - that the next function
+        needs to be aware of. In other words, starting to define an interface,
+        coupling the functions together.
     """
 
     docs_per_thread = math.ceil(len(docs)/thread_count)
     copied = copy.deepcopy(docs)
     thread_allocated_docs = [copied[i: i + docs_per_thread] for i in range(docs_per_thread)[::docs_per_thread]]
-    warnings.warn("thread_allocated_docs"+ str (thread_allocated_docs))
+    warnings.warn("thread_allocated_docs" + str(thread_allocated_docs))
     threads = [threading.Thread(target=threaded_download_images, args=(thread_allocated_docs[i], ))
                for i in range(docs_per_thread)]
     for th in threads:
