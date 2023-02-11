3d25bb2dc4226cd73a933935d90f6271a511ba68
Li Wan <lwan3@student.unimelb.edu.au>
2023-02-11 20:58:30 +1100

    pass MARQO_LOG_LEVEL to uvicorn

--- run_marqo.sh
+++ run_marqo.sh
@@ -91,7 +91,7 @@ export MARQO_LOG_LEVEL=${MARQO_LOG_LEVEL:-info}
 
 # Start the tensor search web app in the background
 cd /app/src/marqo/tensor_search || exit
-uvicorn api:app --host 0.0.0.0 --port 8882 --timeout-keep-alive 75--log-level $MARQO_LOG_LEVEL &
+uvicorn api:app --host 0.0.0.0 --port 8882 --timeout-keep-alive 75 --log-level $MARQO_LOG_LEVEL &
 api_pid=$!
 wait "$api_pid"
 
