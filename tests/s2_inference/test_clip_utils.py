3e7c3499ff20b16b143ea95617d3aeb78738488a
Pandu Oliver Kerr <pandu@s2search.io>
2023-02-13 22:10:34 +1100

    RequestException is excepted, which is the super class of all requests errors

--- tests/s2_inference/test_clip_utils.py
+++ tests/s2_inference/test_clip_utils.py
@@ -1,7 +1,8 @@
 import PIL
-
 from marqo.s2_inference import clip_utils, types
 import unittest
+from unittest import mock
+import requests
 
 
 class TestEncoding(unittest.TestCase):
@@ -17,3 +18,25 @@ class TestEncoding(unittest.TestCase):
             raise AssertionError
         except PIL.UnidentifiedImageError:
             pass
+
+    def test_load_image_from_path_all_req_errors(self):
+        """Do we catch other download errors?
+        The errors tested inherit from requests.exceptions.RequestException
+        """
+        good_url = 'https://raw.githubusercontent.com/marqo-ai/marqo-api-tests/mainline/assets/ai_hippo_realistic.png'
+        # should be fine on regular timeout:
+        clip_utils.load_image_from_path(good_url)
+
+        for err in [requests.exceptions.ReadTimeout(), requests.exceptions.HTTPError]:
+            mock_get = mock.MagicMock()
+            mock_get.return_value.raiseError.side_effect = err
+            @mock.patch('requests.get', mock_get)
+            def run():
+                try:
+                    clip_utils.load_image_from_path(good_url)
+                    raise AssertionError
+                except PIL.UnidentifiedImageError:
+                    pass
+                return True
+
+            run()
