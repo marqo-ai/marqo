623824633992e39a93ed89128daf9ceec9f3ad12
Jack Eadie <jack@marqo.ai>
2023-03-02 13:36:09 +1000

    batch vectorise calls

--- src/marqo/tensor_search/models/api_models.py
+++ src/marqo/tensor_search/models/api_models.py
@@ -6,7 +6,7 @@ https://pydantic-docs.helpmanual.io/usage/types/#enums-and-choices
 import json
 import pydantic
 from pydantic import BaseModel
-from typing import Any, Union, List, Dict, Optional
+from typing import Any, Union, List, Dict, Optional, NewType
 from marqo.tensor_search.enums import SearchMethod, Device
 from marqo.tensor_search import validation
 
@@ -32,6 +32,14 @@ class SearchQuery(BaseModel):
         )
 
 
+Qidx = NewType('Qidx', int)
+JHash = NewType('JHash', int)
+
+class VectorisedJobPointer(BaseModel):
+    job_hash: JHash
+    start_idx: int
+    end_idx: int
+
 class VectorisedJobs(BaseModel):
     model_name: str
     model_properties: Dict[str, Any]
@@ -41,7 +49,27 @@ class VectorisedJobs(BaseModel):
     image_download_headers: Optional[Dict]
 
     def __hash__(self):
-        return hash(self.model_name) + hash(json.dumps(self.model_properties, sort_keys=True)) + hash(json.dumps(self.content, sort_keys=True)) + hash(self.device) + hash(self.normalize_embeddings) + hash(json.dumps(self.image_download_headers, sort_keys=True))
+        return self.groupby_key() + hash(json.dumps(self.content, sort_keys=True))
+
+    def groupby_key(self) -> JHash:
+        return VectorisedJobs.get_groupby_key(self.model_name, self.model_properties, self.device, self.normalize_embeddings, self.image_download_headers)
+
+    @staticmethod
+    def get_groupby_key(model_name: str, model_properties: Dict[str, Any], device: str,
+                        normalize_embeddings: bool, image_download_headers: Optional[Dict]) -> JHash:
+        return JHash(hash(model_name) + hash(json.dumps(model_properties, sort_keys=True))
+                     + hash(device) + hash(normalize_embeddings)
+                     + hash(json.dumps(image_download_headers, sort_keys=True)))
+
+    def add_content(self, content: List[Union[str, List[str]]]) -> VectorisedJobPointer:
+        start_idx = len(self.content)
+        self.content.extend(content)
+
+        return VectorisedJobPointer(
+            job_hash=self.groupby_key(),
+            start_idx=start_idx,
+            end_idx=len(self.content)
+        )
 
 class BulkSearchQueryEntity(SearchQuery):
     index: str
