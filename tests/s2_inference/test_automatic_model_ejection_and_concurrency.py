9b689555f9845f9ca6c3eeb815b1750eac12c3be
Li Wan <lwan3@student.unimelb.edu.au>
2023-03-24 11:39:47 +1100

    revise the threading test

--- tests/s2_inference/test_automatic_model_ejection_and_concurrency.py
+++ tests/s2_inference/test_automatic_model_ejection_and_concurrency.py
@@ -194,7 +194,7 @@ class TestAutomaticModelEject(unittest.TestCase):
 
         assert q_2.qsize() == num_of_threads
         while not q_2.empty():
-            assert type(q_2.get()) is type(ModelCacheManagementError)
+            assert type(q_2.get()) is type(ModelCacheManagementError(""))
 
     def test_concurrent_vectorise_call_cached(self):
         # To test error is thrown if multiple threads want to load the model
