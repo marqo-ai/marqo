691c012d988841279b02c203fc20e6ebc5ea102b
Pandu Oliver Kerr <pandu@s2search.io>
2023-04-14 14:59:46 +1000

    combined components together

--- src/marqo/tensor_search/delete_docs.py
+++ src/marqo/tensor_search/delete_docs.py
@@ -4,7 +4,7 @@ import json
 from marqo import errors
 from marqo._httprequests import HttpRequests
 from marqo.config import Config
-from marqo.tensor_search import validation, utils
+from marqo.tensor_search import validation, utils, enums
 
 
 # -- Marqo delete endpoint interface: --
@@ -38,7 +38,7 @@ class MqDeleteDocsRequest(NamedTuple):
     auto_refresh: bool
 
 
-def delete_documents(config: Config, del_request: MqDeleteDocsRequest):
+def delete_documents(config: Config, del_request: MqDeleteDocsRequest) -> dict:
     """entrypoint function for deleting documents"""
     if not del_request.document_ids:
         raise errors.InvalidDocumentIdError("doc_ids can't be empty!")
@@ -46,17 +46,25 @@ def delete_documents(config: Config, del_request: MqDeleteDocsRequest):
     for _id in del_request.document_ids:
         validation.validate_id(_id)
 
-    # TODO
+    if config.backend == enums.SearchDb.opensearch:
+        del_response: MqDeleteDocsResponse = delete_documents_marqo_os(config=config, deletion_instruction=del_request)
+    else:
+        raise RuntimeError(f"Config set to use unknown backend `{config.backend}`. "
+                           f"See tensor_search.enums.SearchDB for allowed backends")
+
+    return format_delete_docs_response(del_response)
+
+
 # -- Marqo-OS-specific deletion implementation: --
 
 
-def delete_documents_marqo_os(config: Config, index_name: str, doc_ids: List[str], auto_refresh: bool):
+def delete_documents_marqo_os(config: Config, deletion_instruction: MqDeleteDocsRequest) -> MqDeleteDocsResponse:
     """Deletes documents """
 
     # Prepare bulk delete request body
     bulk_request_body = ""
-    for doc_id in doc_ids:
-        bulk_request_body += json.dumps({"delete": {"_index": index_name, "_id": doc_id}}) + "\n"
+    for doc_id in deletion_instruction.document_ids:
+        bulk_request_body += json.dumps({"delete": {"_index": deletion_instruction.index_name, "_id": doc_id}}) + "\n"
 
     # Send bulk delete request
     t0 = datetime.datetime.utcnow()
@@ -65,15 +73,15 @@ def delete_documents_marqo_os(config: Config, index_name: str, doc_ids: List[str
         body=bulk_request_body,
     )
 
-    if auto_refresh:
-        refresh_response = HttpRequests(config).post(path=f"{index_name}/_refresh")
+    if deletion_instruction.auto_refresh:
+        refresh_response = HttpRequests(config).post(path=f"{deletion_instruction.index_name}/_refresh")
 
     t1 = datetime.datetime.utcnow()
     deleted_documents_count = sum(1 for item in delete_res_backend["items"] if "delete" in item and item["delete"]["status"] == 200)
 
     mq_delete_res = MqDeleteDocsResponse(
-        index_name=index_name, status_string='succeeded', document_ids=doc_ids,
+        index_name=deletion_instruction.index_name, status_string='succeeded', document_ids=deletion_instruction.document_ids,
         deleted_docments_count=deleted_documents_count, deletion_start=t0,
         deletion_end=t1
     )
-    return format_delete_docs_response(mq_delete_res)
+    return mq_delete_res
