1184ad350566792c83de5b3b6c76c07bcc1a68cd
Li Wan <lwan3@student.unimelb.edu.au>
2023-04-18 11:28:16 +1000

    modify create index

--- src/marqo/s2_inference/s2_inference.py
+++ src/marqo/s2_inference/s2_inference.py
@@ -221,6 +221,7 @@ def _validate_model_into_device(model_name:str, model_properties: dict, device: 
         return True
     else:
         model_cache_key_for_device = [key for key in list(available_models) if key.endswith(device)]
+        print(model_cache_key_for_device)
         sorted_key_for_device = sorted(model_cache_key_for_device,
                                        key=lambda x: available_models[x][
                                            AvailableModelsKey.most_recently_used_time])
