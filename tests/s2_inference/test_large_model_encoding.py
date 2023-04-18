d350c59ebfbb394fea46fca9e15e471a89cafea2
Li Wan <lwan3@student.unimelb.edu.au>
2023-04-19 00:45:56 +1000

    update device

--- tests/s2_inference/test_large_model_encoding.py
+++ tests/s2_inference/test_large_model_encoding.py
@@ -143,21 +143,17 @@ class TestLargeModelEncoding(unittest.TestCase):
         image = "https://raw.githubusercontent.com/marqo-ai/marqo-clip-onnx/main/examples/coco.jpg"
         e = 0.1
         with patch.dict(os.environ, {"MARQO_MAX_CUDA_MODEL_MEMORY": "10"}):
-            def run():
-                for name in names:
-                    text_feature = np.array(vectorise(model_name=name, content=texts, normalize_embeddings=True, device=device))
-                    image_feature = np.array(vectorise(model_name=name, content=image, normalize_embeddings=True, device=device))
+            for name in names:
+                text_feature = np.array(vectorise(model_name=name, content=texts, normalize_embeddings=True, device=device))
+                image_feature = np.array(vectorise(model_name=name, content=image, normalize_embeddings=True, device=device))
 
-                    clear_loaded_models()
-                    similarity_score = (text_feature @ image_feature.T).flatten()
+                clear_loaded_models()
+                similarity_score = (text_feature @ image_feature.T).flatten()
 
-                    assert np.abs(np.max(similarity_score) - np.min(similarity_score)) < e
+                assert np.abs(np.max(similarity_score) - np.min(similarity_score)) < e
 
-                    del similarity_score
-                    clear_loaded_models()
-                return True
-
-            assert run()
+                del similarity_score
+                clear_loaded_models()
 
 
     def test_cuda_encode_type(self):
