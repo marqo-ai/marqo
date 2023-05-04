cc914b6cc3ecd108f2302950aff64db314d6d54a
Li Wan <lwan3@student.unimelb.edu.au>
2023-05-04 13:47:09 +1000

    add new tests for open clip models

--- tests/s2_inference/test_encoding.py
+++ tests/s2_inference/test_encoding.py
@@ -20,16 +20,13 @@ _load_model = functools.partial(og_load_model, calling_func = "unit_test")
 class TestEncoding(unittest.TestCase):
 
     def setUp(self) -> None:
-
         pass
 
     def tearDown(self) -> None:
         clear_loaded_models()
 
     def test_vectorize(self):
-        names = ["fp16/ViT-B/32", "open_clip/convnext_base_w/laion2b_s13b_b82k",
-                 "open_clip/convnext_base_w_320/laion_aesthetic_s13b_b82k_augreg",
-                 "onnx16/open_clip/ViT-B-32/laion400m_e32", 'onnx32/open_clip/ViT-B-32-quickgelu/laion400m_e32',
+        names = ["fp16/ViT-B/32", "onnx16/open_clip/ViT-B-32/laion400m_e32", 'onnx32/open_clip/ViT-B-32-quickgelu/laion400m_e32',
                  "all-MiniLM-L6-v1", "all_datasets_v4_MiniLM-L6", "hf/all-MiniLM-L6-v1", "hf/all_datasets_v4_MiniLM-L6",
                  "onnx/all-MiniLM-L6-v1", "onnx/all_datasets_v4_MiniLM-L6"]
 
@@ -56,9 +53,7 @@ class TestEncoding(unittest.TestCase):
             clear_loaded_models()
 
     def test_cpu_encode_type(self):
-        names = ["fp16/ViT-B/32", "open_clip/convnext_base_w/laion2b_s13b_b82k",
-                 "open_clip/convnext_base_w_320/laion_aesthetic_s13b_b82k_augreg",
-                 "onnx16/open_clip/ViT-B-32/laion400m_e32", 'onnx32/open_clip/ViT-B-32-quickgelu/laion400m_e32',
+        names = ["fp16/ViT-B/32", "onnx16/open_clip/ViT-B-32/laion400m_e32", 'onnx32/open_clip/ViT-B-32-quickgelu/laion400m_e32',
                  "all-MiniLM-L6-v1", "all_datasets_v4_MiniLM-L6", "hf/all-MiniLM-L6-v1", "hf/all_datasets_v4_MiniLM-L6",
                  "onnx/all-MiniLM-L6-v1", "onnx/all_datasets_v4_MiniLM-L6"]
 
@@ -80,8 +75,7 @@ class TestEncoding(unittest.TestCase):
 
     def test_load_clip_text_model(self):
         names = ["fp16/ViT-B/32", "onnx16/open_clip/ViT-B-32/laion400m_e32", 'onnx32/open_clip/ViT-B-32-quickgelu/laion400m_e32',
-                  'RN50', "ViT-B/16", "open_clip/convnext_base_w/laion2b_s13b_b82k",
-                 "open_clip/convnext_base_w_320/laion_aesthetic_s13b_b82k_augreg",]
+                  'RN50', "ViT-B/16"]
 
         device = 'cpu'
         eps = 1e-9
@@ -163,10 +157,7 @@ class TestEncoding(unittest.TestCase):
 
 
     def test_model_outputs(self):
-        names = ["fp16/ViT-B/32", "open_clip/convnext_base_w/laion2b_s13b_b82k",
-                 "open_clip/convnext_base_w_320/laion_aesthetic_s13b_b82k_augreg",
-                 "onnx16/open_clip/ViT-B-32/laion400m_e32", 'onnx32/open_clip/ViT-B-32-quickgelu/laion400m_e32',
-                 'open_clip/ViT-B-32/laion400m_e32', "all-MiniLM-L6-v1",
+        names = ["fp16/ViT-B/32", "onnx16/open_clip/ViT-B-32/laion400m_e32", 'onnx32/open_clip/ViT-B-32-quickgelu/laion400m_e32',"all-MiniLM-L6-v1",
                  "all_datasets_v4_MiniLM-L6", "hf/all-MiniLM-L6-v1",
                  "hf/all_datasets_v4_MiniLM-L6", "onnx/all-MiniLM-L6-v1", "onnx/all_datasets_v4_MiniLM-L6"]
 
@@ -189,7 +180,7 @@ class TestEncoding(unittest.TestCase):
 
     def test_model_normalization(self):
         names = ["fp16/ViT-B/32", "onnx16/open_clip/ViT-B-32/laion400m_e32", 'onnx32/open_clip/ViT-B-32-quickgelu/laion400m_e32',
-                 'open_clip/ViT-B-32/laion400m_e32', 'RN50', "ViT-B/16", "all-MiniLM-L6-v1",
+                 'RN50', "ViT-B/16", "all-MiniLM-L6-v1",
                  "all_datasets_v4_MiniLM-L6", "hf/all-MiniLM-L6-v1", "hf/all_datasets_v4_MiniLM-L6",
                  "onnx/all-MiniLM-L6-v1", "onnx/all_datasets_v4_MiniLM-L6"]
 
@@ -219,7 +210,7 @@ class TestEncoding(unittest.TestCase):
     def test_model_un_normalization(self):
         # note: sbert native seems to provide normalized embeddings even with = False, needs more investigation
         # , 
-        names = ['open_clip/ViT-B-32/laion400m_e32', 'RN50', "ViT-B/16", "hf/all-MiniLM-L6-v1",
+        names = [ 'RN50', "ViT-B/16", "hf/all-MiniLM-L6-v1",
                  "hf/all_datasets_v4_MiniLM-L6", "onnx/all-MiniLM-L6-v1", "onnx/all_datasets_v4_MiniLM-L6"]
 
         names_e5 = ["hf/e5-small", "hf/e5-base", "hf/e5-small-unsupervised", "hf/e5-base-unsupervised"]
@@ -244,53 +235,6 @@ class TestEncoding(unittest.TestCase):
 
             clear_loaded_models()
 
-
-    def test_open_clip_vectorize(self):
-        names = ["open_clip/convnext_base_w/laion2b_s13b_b82k",
-                 "open_clip/convnext_base_w_320/laion_aesthetic_s13b_b82k_augreg",
-                 'open_clip/ViT-B-32/laion400m_e32', 'open_clip/RN50/openai']
-
-        sentences = ['hello', 'this is a test sentence. so is this.', ['hello', 'this is a test sentence. so is this.']]
-        device = 'cpu'
-        eps = 1e-9
-
-        for name in names:
-            model_properties = get_model_properties_from_registry(name)
-            model = _load_model(model_properties['name'], model_properties=model_properties, device=device)
-
-            for sentence in sentences:
-                output_v = vectorise(name, sentence, model_properties, device, normalize_embeddings=True)
-
-                assert _check_output_type(output_v)
-
-                output_m = model.encode(sentence, normalize=True)
-
-                assert abs(torch.FloatTensor(output_m) - torch.FloatTensor(output_v)).sum() < eps
-
-            clear_loaded_models()
-
-
-    def test_open_clip_embedding_size(self):
-        names = ["open_clip/convnext_base_w/laion2b_s13b_b82k",
-                 "open_clip/convnext_base_w_320/laion_aesthetic_s13b_b82k_augreg",'open_clip/ViT-B-32/laion400m_e32', 'open_clip/RN50/openai']
-
-        device = "cpu"
-
-        sentences = ['hello', 'this is a test sentence. so is this.', ['hello', 'this is a test sentence. so is this.']]
-
-        for name in names:
-            open_clip_properties = _get_open_clip_properties()
-
-            for sentence in sentences:
-                output_v = vectorise(name, sentence, get_model_properties_from_registry(name), device, normalize_embeddings=True)
-                registered_dimension = open_clip_properties[name]["dimensions"]
-                output_dimension = len(output_v[0])
-
-                assert registered_dimension == output_dimension
-
-            clear_loaded_models()
-
-
     def test_onnx_clip_vectorise(self):
         names = ["onnx16/open_clip/ViT-B-32/laion400m_e32", 'onnx32/open_clip/ViT-B-32-quickgelu/laion400m_e32']
 
@@ -313,3 +257,178 @@ class TestEncoding(unittest.TestCase):
                 assert abs(torch.FloatTensor(output_m) - torch.FloatTensor(output_v)).sum() < eps
 
             clear_loaded_models()
+
+
+class TestOpenClipModelEncoding(unittest.TestCase):
+    '''
+    This test is for open clip models as they are heavily used in production.
+    '''
+
+    def setUp(self) -> None:
+        self.open_clip_test_model = ['open_clip/RN50/yfcc15m', 'open_clip/ViT-B-32/laion2b_s34b_b79k',
+                                     'open_clip/ViT-B-16/laion2b_s34b_b88k', 'open_clip/convnext_base/laion400m_s13b_b51k',
+                                     'open_clip/convnext_base_w/laion_aesthetic_s13b_b82k',
+                                     'open_clip/coca_ViT-B-32/mscoco_finetuned_laion2b_s13b_b90k']
+        pass
+
+    def tearDown(self) -> None:
+        clear_loaded_models()
+
+    def test_open_clip_vectorize(self):
+        names = self.open_clip_test_model
+
+        sentences = ['hello', 'this is a test sentence. so is this.', ['hello', 'this is a test sentence. so is this.']]
+        device = 'cpu'
+        eps = 1e-9
+
+        for name in names:
+            model_properties = get_model_properties_from_registry(name)
+            model = _load_model(model_properties['name'], model_properties=model_properties, device=device)
+
+            for sentence in sentences:
+                output_v = vectorise(name, sentence, model_properties, device, normalize_embeddings=True)
+
+                assert _check_output_type(output_v)
+
+                output_m = model.encode(sentence, normalize=True)
+
+                assert abs(torch.FloatTensor(output_m) - torch.FloatTensor(output_v)).sum() < eps
+
+            clear_loaded_models()
+
+    def test_load_clip_text_model(self):
+        names = self.open_clip_test_model
+
+        device = 'cpu'
+        eps = 1e-9
+        texts = ['hello', 'big', 'asasasasaaaaaaaaaaaa', '', 'a word. another one!?. #$#.']
+
+        for name in names:
+
+            model =  _load_model(name, model_properties=get_model_properties_from_registry(name), device=device)
+
+            for text in texts:
+                assert abs(model.encode(text) - model.encode([text])).sum() < eps
+                assert abs(model.encode_text(text) - model.encode([text])).sum() < eps
+                assert abs(model.encode(text) - model.encode_text([text])).sum() < eps
+
+            clear_loaded_models()
+
+    def test_load_clip_image_model(self):
+        names = self.open_clip_test_model
+
+        device = 'cpu'
+        eps = 1e-9
+        images = ["https://raw.githubusercontent.com/marqo-ai/marqo/mainline/examples/ImageSearchGuide/data/image0.jpg",
+                  "https://raw.githubusercontent.com/marqo-ai/marqo/mainline/examples/ImageSearchGuide/data/image1.jpg",
+                  "https://raw.githubusercontent.com/marqo-ai/marqo/mainline/examples/ImageSearchGuide/data/image2.jpg"]
+
+        for name in names:
+
+            model =  _load_model(name, model_properties=get_model_properties_from_registry(name), device=device)
+
+            for image in images:
+                assert abs(model.encode(image) - model.encode([image])).sum() < eps
+                assert abs(model.encode_text(image) - model.encode([image])).sum() < eps
+                assert abs(model.encode(image) - model.encode_text([image])).sum() < eps
+
+            clear_loaded_models()
+
+    def test_cpu_encode_type(self):
+        names = self.open_clip_test_model
+
+        names_e5 = ["hf/e5-small", "hf/e5-base", "hf/e5-small-unsupervised",     "hf/e5-base-unsupervised"]
+        names += names_e5
+
+        sentences = ['hello', 'this is a test sentence. so is this.', ['hello', 'this is a test sentence. so is this.']]
+        device = 'cpu'
+
+        for name in names:
+            model_properties = get_model_properties_from_registry(name)
+            model = _load_model(model_properties['name'], model_properties=model_properties, device=device, )
+
+            for sentence in sentences:
+                output_v = _convert_tensor_to_numpy(model.encode(sentence, normalize=True))
+                assert isinstance(output_v, np.ndarray)
+
+            clear_loaded_models()
+
+    def test_open_clip_embedding_size(self):
+        names = self.open_clip_test_model
+
+        device = "cpu"
+
+        sentences = ['hello', 'this is a test sentence. so is this.', ['hello', 'this is a test sentence. so is this.']]
+
+        for name in names:
+            open_clip_properties = _get_open_clip_properties()
+
+            for sentence in sentences:
+                output_v = vectorise(name, sentence, get_model_properties_from_registry(name), device, normalize_embeddings=True)
+                registered_dimension = open_clip_properties[name]["dimensions"]
+                output_dimension = len(output_v[0])
+
+                assert registered_dimension == output_dimension
+
+            clear_loaded_models()
+
+    def test_model_outputs(self):
+        names = self.open_clip_test_model
+
+        sentences = ['hello', 'this is a test sentence. so is this.', ['hello', 'this is a test sentence. so is this.']]
+        device = 'cpu'
+
+        for name in names:
+            model_properties = get_model_properties_from_registry(name)
+            model = _load_model(model_properties['name'], model_properties=model_properties, device=device)
+
+            for sentence in sentences:
+                output = model.encode(sentence)
+                assert _check_output_type(_convert_vectorized_output(output))
+
+            clear_loaded_models()
+
+    def test_model_normalization(self):
+        names = self.open_clip_test_model
+
+        sentences = ['hello', 'this is a test sentence. so is this.', ['hello', 'this is a test sentence. so is this.']]
+        device = 'cpu'
+        eps = 1e-6
+
+        for name in names:
+            model_properties = get_model_properties_from_registry(name)
+            model = _load_model(model_properties['name'], model_properties=model_properties, device=device)
+
+            for sentence in sentences:
+                output = model.encode(sentence, normalize=True)
+                output = _convert_vectorized_output(output)
+                max_output_norm = max(torch.linalg.norm(FloatTensor(output), dim=1))
+                min_output_norm = min(torch.linalg.norm(FloatTensor(output), dim=1))
+
+                assert abs(max_output_norm - 1) < eps, f"{name}, {sentence}"
+                assert abs(min_output_norm - 1) < eps, f"{name}, {sentence}"
+
+            clear_loaded_models()
+
+    def test_model_un_normalization(self):
+        names = self.open_clip_test_model
+
+        sentences = ['hello', 'this is a test sentence. so is this.', ['hello', 'this is a test sentence. so is this.']]
+        device = 'cpu'
+        eps = 1e-3
+
+        for name in names:
+            model_properties = get_model_properties_from_registry(name)
+            model = _load_model(model_properties['name'], model_properties=model_properties, device=device)
+
+            for sentence in sentences:
+                output = model.encode(sentence, normalize=False)
+                output = _convert_vectorized_output(output)
+                max_output_norm = max(torch.linalg.norm(FloatTensor(output), dim=1))
+                min_output_norm = min(torch.linalg.norm(FloatTensor(output), dim=1))
+
+                assert abs(max_output_norm - 1) > eps, f"{name}, {sentence}"
+                assert abs(min_output_norm - 1) > eps, f"{name}, {sentence}"
+
+            clear_loaded_models()
+
