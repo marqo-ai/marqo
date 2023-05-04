702ef7e5ab3cf9cadbc88e5353000dbc0b4c44d2
Li Wan <lwan3@student.unimelb.edu.au>
2023-05-04 15:08:35 +1000

    comment out 'open_clip/convnext_xxlarge/laion2b_s34b_b82k_augreg_soup'

--- tests/s2_inference/test_large_model_encoding.py
+++ tests/s2_inference/test_large_model_encoding.py
@@ -20,8 +20,10 @@ _load_model = functools.partial(og_load_model, calling_func = "unit_test")
 class TestLargeModelEncoding(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.large_clip_models = [ "open_clip/ViT-L-14/laion400m_e32", 'open_clip/coca_ViT-L-14/mscoco_finetuned_laion2b_s13b_b90k',
-                                   'open_clip/convnext_xxlarge/laion2b_s34b_b82k_augreg_soup', 'open_clip/convnext_large_d_320/laion2b_s29b_b131k_ft_soup'
+        self.large_clip_models = [ "open_clip/ViT-L-14/laion400m_e32",
+                                   'open_clip/coca_ViT-L-14/mscoco_finetuned_laion2b_s13b_b90k',
+                                   #'open_clip/convnext_xxlarge/laion2b_s34b_b82k_augreg_soup',  this model is not currently available in open_clip
+                                   'open_clip/convnext_large_d_320/laion2b_s29b_b131k_ft_soup'
                                    'open_clip/convnext_large_d/laion2b_s26b_b102k_augreg']
 
         self.multilingual_models = ["multilingual-clip/XLM-Roberta-Large-Vit-L-14"]
