f5ff4e4f05b8ebc5b3fac100af99c3b4fd75349d
Li Wan <lwan3@student.unimelb.edu.au>
2023-05-04 13:52:57 +1000

    add new tests for open clip models and large open clip models

--- tests/s2_inference/test_large_model_encoding.py
+++ tests/s2_inference/test_large_model_encoding.py
@@ -20,7 +20,9 @@ _load_model = functools.partial(og_load_model, calling_func = "unit_test")
 class TestLargeModelEncoding(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.large_clip_models = ["open_clip/ViT-L-14/openai", "open_clip/ViT-L-14/laion400m_e32"]
+        self.large_clip_models = [ "open_clip/ViT-L-14/laion400m_e32", 'open_clip/coca_ViT-L-14/mscoco_finetuned_laion2b_s13b_b90k',
+                                   'open_clip/convnext_xxlarge/laion2b_s34b_b82k_augreg_soup', 'open_clip/convnext_large_d_320/laion2b_s29b_b131k_ft_soup'
+                                   'open_clip/convnext_large_d/laion2b_s26b_b102k_augreg']
 
         self.multilingual_models = ["multilingual-clip/XLM-Roberta-Large-Vit-L-14"]
 
