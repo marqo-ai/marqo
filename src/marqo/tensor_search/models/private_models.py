41f30ef1f7d3ff7cdbe676761d5d1990e708cd53
Pandu Oliver Kerr <pandu@s2search.io>
2023-05-10 21:04:57 +1000

    fixed private model and test

--- src/marqo/tensor_search/models/private_models.py
+++ src/marqo/tensor_search/models/private_models.py
@@ -2,38 +2,35 @@
 
 For example models stored on custom Huggingface repos or on private s3 buckets
 """
-
-from typing import Optional
-from pydantic import BaseModel
 from marqo.tensor_search.models.external_apis.hf import HfAuth, HfModelLocation
 from marqo.tensor_search.models.external_apis.s3 import S3Auth, S3Location
+from pydantic import BaseModel, validator
 from marqo.errors import InvalidArgError
-
+from typing import Optional
 
 class ModelAuth(BaseModel):
+    """TODO: insert links to docs in error message"""
     class Config:
         allow_mutation = False
 
     s3: Optional[S3Auth] = None
     hf: Optional[HfAuth] = None
 
-    def __post_init__(self):
-        self._ensure_exactly_one_auth_method()
-
-    def _ensure_exactly_one_auth_method(self):
-        """TODO: insert links to docs in error message"""
-        auth_objects = [self.s3, self.hf]
-
-        auth_objects_presence = [x is not None for x in auth_objects]
-
-        if not any(auth_objects_presence):
+    def __init__(self, **data):
+        super().__init__(**data)
+        if self.s3 is None and self.hf is None:
             raise InvalidArgError(
                 "Missing authentication object. An authentic object, for example `s3` or  "
                 "`hf`, must be provided. ")
-        if sum(auth_objects_presence) > 1:
+
+    @validator('s3', 'hf', pre=True, always=True)
+    def _ensure_exactly_one_auth_method(cls, v, values, field):
+        other_field = 's3' if field.name == 'hf' else 'hf'
+        if other_field in values and values[other_field] is not None and v is not None:
             raise InvalidArgError(
                 "More than one model authentication was provided. "
                 "Only one model authentication object is allowed")
+        return v
 
 
 class ModelLocation(BaseModel):
@@ -45,20 +42,19 @@ class ModelLocation(BaseModel):
     hf: Optional[HfModelLocation] = None
     auth_required: bool = False
 
-    def __post_init__(self):
-        self._ensure_exactly_one_location()
-
-    def _ensure_exactly_one_location(self):
+    @validator('s3', 'hf', pre=True, always=True)
+    def _ensure_exactly_one_location(cls, v, values, field):
         """TODO: insert links to docs in error message"""
-        loc_objects = [self.s3, self.hf]
-
-        loc_objects_presence = [x is not None for x in loc_objects]
-
-        if not any(loc_objects_presence):
-            raise InvalidArgError(
-                "Missing model location object. A location object, for example `s3` or  "
-                "`hf`, must be provided. ")
-        if sum(loc_objects_presence) > 1:
+        other_field = 's3' if field.name == 'hf' else 'hf'
+        if other_field in values and values[other_field] is not None and v is not None:
             raise InvalidArgError(
                 "More than one model location object was provided. "
                 "Only one model authentication object is allowed")
+        return v
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        if self.s3 is None and self.hf is None:
+            raise InvalidArgError(
+                "Missing model location object. A location object, for example `s3` or  "
+                "`hf`, must be provided. ")
