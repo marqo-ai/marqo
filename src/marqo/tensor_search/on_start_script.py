72fb33d8b4f6db4e5f77d2090eb26765e3c4a6e9
Li Wan <lwan3@student.unimelb.edu.au>
2023-06-05 14:48:20 +1000

    add logger for start up

--- src/marqo/tensor_search/on_start_script.py
+++ src/marqo/tensor_search/on_start_script.py
@@ -1,5 +1,6 @@
 import json
 import os
+import logging
 from marqo.tensor_search import enums
 from marqo.tensor_search.tensor_search_logging import get_logger
 import time
@@ -12,32 +13,59 @@ from marqo._httprequests import HttpRequests
 from marqo import errors
 from marqo.tensor_search.throttling.redis_throttle import throttle
 from marqo.connections import redis_driver
+from functools import wraps
+
+logger = get_logger(__name__)
+
+# Configure the logger
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 def on_start(marqo_os_url: str):
-        
+    logger.info("Starting the on start script")
     to_run_on_start = (
-                        PopulateCache(marqo_os_url),
-                        DownloadStartText(),
-                        CUDAAvailable(), 
-                        ModelsForCacheing(), 
-                        InitializeRedis("localhost", 6379),    # TODO, have these variable
-                        DownloadFinishText(),
-                        MarqoWelcome(),
-                        MarqoPhrase(),
-                        )
+        PopulateCache(marqo_os_url),
+        DownloadStartText(),
+        CUDAAvailable(),
+        ModelsForCacheing(),
+        InitializeRedis("localhost", 6379),  # TODO, have these variable
+        DownloadFinishText(),
+        MarqoWelcome(),
+        MarqoPhrase(),
+    )
 
     for thing_to_start in to_run_on_start:
         thing_to_start.run()
 
 
+def log_time(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        logger.info(f"{func.__name__} started")
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        logger.info(f"{func.__name__} finished")
+        elapsed_time = end_time - start_time
+        logger.info(f"{func.__name__} finished in {elapsed_time} seconds")
+        return result
+
+    return wrapper
+
+
 class PopulateCache:
     """Populates the cache on start"""
 
+    @log_time
     def __init__(self, marqo_os_url: str):
         self.marqo_os_url = marqo_os_url
         pass
 
+    @log_time
     def run(self):
         c = config.Config(api_utils.upconstruct_authorized_url(
             opensearch_url=self.marqo_os_url
@@ -46,7 +74,7 @@ class PopulateCache:
             index_meta_cache.populate_cache(c)
         except errors.BackendCommunicationError as e:
             raise errors.BackendCommunicationError(
-                message="Can't connect to Marqo-os backend. \n"  
+                message="Can't connect to Marqo-os backend. \n"
                         "    Possible causes: \n"
                         "        - If this is an arm64 machine, ensure you are using an external Marqo-os instance \n"
                         "        - If you are using an external Marqo-os instance, check if it is running: "
@@ -67,15 +95,16 @@ class PopulateCache:
 
 
 class CUDAAvailable:
-
     """checks the status of cuda
     """
     logger = get_logger('CUDA device summary')
 
+    @log_time
     def __init__(self):
-        
+
         pass
 
+    @log_time
     def run(self):
         import torch
 
@@ -83,23 +112,25 @@ class CUDAAvailable:
             if id < 0:
                 return ['cpu']
             return [torch.cuda.get_device_name(id)]
-        
+
         device_count = 0 if not torch.cuda.is_available() else torch.cuda.device_count()
 
         # use -1 for cpu
         device_ids = [-1]
         device_ids += list(range(device_count))
-        
+
         device_names = []
         for device_id in device_ids:
-            device_names.append( {'id':device_id, 'name':id_to_device(device_id)})
+            device_names.append({'id': device_id, 'name': id_to_device(device_id)})
         self.logger.info(f"found devices {device_names}")
+
 
 class ModelsForCacheing:
     """warms the in-memory model cache by preloading good defaults
     """
     logger = get_logger('ModelsForStartup')
 
+    @log_time
     def __init__(self):
         import torch
         warmed_models = utils.read_env_vars_and_defaults(EnvVars.MARQO_MODELS_TO_PRELOAD)
@@ -122,15 +153,16 @@ class ModelsForCacheing:
 
         self.logger.info(f"pre-loading {self.models} onto devices={self.default_devices}")
 
+    @log_time
     def run(self):
         from marqo.s2_inference.s2_inference import vectorise
-       
+
         test_string = 'this is a test string'
         N = 10
         messages = []
         for model in self.models:
             for device in self.default_devices:
-                
+
                 # warm it up
                 _ = vectorise(model, test_string, device=device)
 
@@ -140,7 +172,7 @@ class ModelsForCacheing:
                     _ = vectorise(model, test_string, device=device)
                     t1 = time.time()
                     t += (t1 - t0)
-                message = f"{(t)/float((N))} for {model} and {device}"
+                message = f"{(t) / float((N))} for {model} and {device}"
                 messages.append(message)
                 self.logger.info(f"{model} {device} run succesfully!")
 
@@ -150,11 +182,12 @@ class ModelsForCacheing:
 
 
 class InitializeRedis:
-
+    @log_time
     def __init__(self, host: str, port: int):
         self.host = host
         self.port = port
 
+    @log_time
     def run(self):
         # Can be turned off with MARQO_ENABLE_THROTTLING = 'FALSE'
         if utils.read_env_vars_and_defaults(EnvVars.MARQO_ENABLE_THROTTLING) == "TRUE":
@@ -162,9 +195,8 @@ class InitializeRedis:
 
 
 class DownloadStartText:
-
+    @log_time
     def run(self):
-
         print('\n')
         print("###########################################################")
         print("###########################################################")
@@ -175,7 +207,7 @@ class DownloadStartText:
 
 
 class DownloadFinishText:
-
+    @log_time
     def run(self):
         print('\n')
         print("###########################################################")
@@ -187,25 +219,23 @@ class DownloadFinishText:
 
 
 class MarqoPhrase:
-
+    @log_time
     def run(self):
-
         message = r"""
      _____                                                   _        __              _                                     
     |_   _|__ _ __  ___  ___  _ __   ___  ___  __ _ _ __ ___| |__    / _| ___  _ __  | |__  _   _ _ __ ___   __ _ _ __  ___ 
       | |/ _ \ '_ \/ __|/ _ \| '__| / __|/ _ \/ _` | '__/ __| '_ \  | |_ / _ \| '__| | '_ \| | | | '_ ` _ \ / _` | '_ \/ __|
       | |  __/ | | \__ \ (_) | |    \__ \  __/ (_| | | | (__| | | | |  _| (_) | |    | | | | |_| | | | | | | (_| | | | \__ \
       |_|\___|_| |_|___/\___/|_|    |___/\___|\__,_|_|  \___|_| |_| |_|  \___/|_|    |_| |_|\__,_|_| |_| |_|\__,_|_| |_|___/
-                                                                                                                                                                                                                                                     
+
         """
 
         print(message)
 
 
 class MarqoWelcome:
-
+    @log_time
     def run(self):
-
         message = r"""   
      __    __    ___  _        __   ___   ___ ___    ___      ______   ___       ___ ___   ____  ____   ___    ___   __ 
     |  |__|  |  /  _]| |      /  ] /   \ |   |   |  /  _]    |      | /   \     |   |   | /    ||    \ /   \  /   \ |  |
@@ -214,6 +244,6 @@ class MarqoWelcome:
     |  `  '  ||   [_ |     /   \_ |     ||   |   ||   [_       |  |  |     |    |   |   ||  _  ||    \|     ||     | __ 
      \      / |     ||     \     ||     ||   |   ||     |      |  |  |     |    |   |   ||  |  ||  .  \     ||     ||  |
       \_/\_/  |_____||_____|\____| \___/ |___|___||_____|      |__|   \___/     |___|___||__|__||__|\_|\__,_| \___/ |__|
-                                                                                                                        
+
         """
         print(message)
