069efd0e14e32099be692a2c3859d75f789f3251
Li Wan <lwan3@student.unimelb.edu.au>
2023-06-05 15:35:44 +1000

    change logger

--- src/marqo/s2_inference/logger.py
+++ src/marqo/s2_inference/logger.py
@@ -1,12 +1,13 @@
 import logging
 
 def get_logger(name):
-    logging.basicConfig()
-
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
-    formatter = logging.Formatter(
-        "{asctime} {threadName:>11} {levelname} {message}", style='{')
+    handler = logging.StreamHandler()  # Creates a new handler
+    formatter = logging.Formatter("{asctime} {threadName:>11} {levelname} {message}", style='{')
+    handler.setFormatter(formatter)  # Applies the formatter to the handler
+
+    logger.addHandler(handler)  # Adds the handler to the logger
 
     return logger
