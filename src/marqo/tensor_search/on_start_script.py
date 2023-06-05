c2278ba393652a284d7f6482584588cb0ccc9b8f
Li Wan <lwan3@student.unimelb.edu.au>
2023-06-05 15:08:09 +1000

    change logger

--- src/marqo/tensor_search/on_start_script.py
+++ src/marqo/tensor_search/on_start_script.py
@@ -15,8 +15,8 @@ from marqo.tensor_search.throttling.redis_throttle import throttle
 from marqo.connections import redis_driver
 from functools import wraps
 
-logger = get_logger(__name__)
-
+logging.basicConfig()
+logger = logging.getLogger(__name__)
 # Configure the logger
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
