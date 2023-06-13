862ac85d3b753070e71ea555ec7be63b788ff38f
Li Wan <lwan3@student.unimelb.edu.au>
2023-06-13 16:29:11 +1000

    update error handling in s2_inference

--- src/marqo/s2_inference/s2_inference.py
+++ src/marqo/s2_inference/s2_inference.py
@@ -5,7 +5,7 @@ import numpy as np
 from marqo.errors import ModelCacheManagementError
 from marqo.s2_inference.errors import (
     VectoriseError, InvalidModelPropertiesError, ModelLoadError,
-    UnknownModelError, ModelNotInCacheError, ModelDownloadError)
+    UnknownModelError, ModelNotInCacheError, ModelDownloadError, S2InferenceError)
 from PIL import UnidentifiedImageError
 from marqo.s2_inference.model_registry import load_model_properties
 from marqo.s2_inference.configs import get_default_device, get_default_normalization, get_default_seq_length
@@ -164,11 +164,13 @@ def _update_available_models(model_cache_key: str, model_name: str, validated_mo
 
                 if isinstance(e, ModelDownloadError):
                     raise e
-                raise ModelLoadError(
-                    f"Unable to load model={model_name} on device={device} with normalization={normalize_embeddings}. "
-                    f"If you are trying to load a custom model, "
-                    f"please check that model_properties={validated_model_properties} is correct "
-                    f"and Marqo has access to the weights file.")
+                elif isinstance(e, S2InferenceError):
+                    raise ModelLoadError(f"Unable to load model={model_name} on device={device} with normalization={normalize_embeddings}. \n"
+                                         f"The error message is {str(e)} \n")
+                # for all other errors, which will cause an internal error in tensor_search
+                else:
+                    raise e
+
     else:
         most_recently_used_time = datetime.datetime.now()
         logger.debug(f'renewed {model_name} on device {device} with new most recently time={most_recently_used_time}.')
