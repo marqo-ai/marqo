57765eae2a4b051332fea50d14e2ad7609d96c6e
Pandu Oliver Kerr <pandu@s2search.io>
2023-07-14 09:25:34 +1000

    testing default shards=2, replicas=0

--- src/marqo/tensor_search/configs.py
+++ src/marqo/tensor_search/configs.py
@@ -21,8 +21,8 @@ def get_default_index_settings():
             },
             NsFields.ann_parameters: get_default_ann_parameters()
         },
-        NsFields.number_of_shards: 1,
-        NsFields.number_of_replicas : 0,
+        NsFields.number_of_shards: 2,
+        NsFields.number_of_replicas: 0,
     }
 
 def get_default_ann_parameters():
