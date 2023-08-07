8c83c43d0dd18a5142f960736cbc2e6e61ea84f5
Li Wan <lwan3@student.unimelb.edu.au>
2023-08-07 16:46:28 +1000

    Add field name also into params

--- src/marqo/tensor_search/models/score_modifiers_object.py
+++ src/marqo/tensor_search/models/score_modifiers_object.py
@@ -30,27 +30,31 @@ class ScoreModifierOperator(BaseModel):
             raise InvalidArgError("_id is not allowed as a field_name")
         return v
 
-    def to_painless_script_and_params(self, i: int, operation: Literal["multiply_score_by", "add_to_score"]) -> Tuple[str, Dict]:
-        """Convert a ScoreModifierOperator based on if it's an multiply_score_by', 'add_to_score' operation."""
+    def to_painless_script_and_params(self, field_index: int, operation: Literal["multiply_score_by", "add_to_score"]) \
+            -> Tuple[str, Dict, Dict]:
+        """Convert a ScoreModifierOperator based on if it's an multiply_score_by', 'add_to_score' operation.
+
+           Returns: A tuple of (the painless script, the weights in the params, the field name in the params)
+        """
         if operation == "multiply_score_by":
             return f"""
-            if (doc.containsKey('__chunks.{self.field_name}')) {{
-                if (doc['__chunks.{self.field_name}'].size() > 0 &&
-                    (doc['__chunks.{self.field_name}'].value instanceof java.lang.Number)) {{
-                    _score = _score * doc['__chunks.{self.field_name}'].value * params.multiplier_weight_{i};
+            if (doc.containsKey(params.multiplier_field_{field_index})) {{
+                if (doc[params.multiplier_field_{field_index}].size() > 0 &&
+                    (doc[params.multiplier_field_{field_index}].value instanceof java.lang.Number)) {{
+                    _score = _score * doc[params.multiplier_field_{field_index}].value * params.multiplier_weight_{field_index};
                 }}
             }}
-            """, {f"multiplier_weight_{i}": self.weight}
+            """, {f"multiplier_weight_{field_index}": self.weight}, {f"multiplier_field_{field_index}": f"__chunks.{self.field_name}"}
 
         elif operation == "add_to_score":
             return f""" 
-            if (doc.containsKey('__chunks.{self.field_name}')) {{     
-                if (doc['__chunks.{self.field_name}'].size() > 0 &&
-                    (doc['__chunks.{self.field_name}'].value instanceof java.lang.Number)) {{
-                    additive = additive + doc['__chunks.{self.field_name}'].value * params.add_weight_{i};
+            if (doc.containsKey(params.add_field_{field_index})) {{     
+                if (doc[params.add_field_{field_index}].size() > 0 &&
+                    (doc[params.add_field_{field_index}].value instanceof java.lang.Number)) {{
+                    additive = additive + doc[params.add_field_{field_index}].value * params.add_weight_{field_index};
                 }}
             }}
-            """, {f"add_weight_{i}": self.weight}
+            """, {f"add_weight_{field_index}": self.weight}, {f"add_field_{field_index}": f"__chunks.{self.field_name}"}
 
         else:
             raise ValueError(
@@ -88,25 +92,29 @@ class ScoreModifier(BaseModel):
         return v
 
     def to_script_score(self) -> dict:
-        """Convert this ScoreModifier to a painless script to modify the score."""
+        """Convert this ScoreModifier to a dictionary to modify the score.
+        returns:
+            A dictionary that can be used as a script_score in an Elasticsearch query.
+                source: the painless script
+                params: the field name and fields used in the painless script
+        """
         if self.multiply_score_by:
-            mult, mult_params = map(list, zip(*([x.to_painless_script_and_params(i, "multiply_score_by") for i, x in
+            mult, mult_weight_params, mult_field_params = map(list, zip(*([x.to_painless_script_and_params(field_index, "multiply_score_by") for field_index, x in
                                                  enumerate(self.multiply_score_by)])))
         else:
-            mult, mult_params = [], []
+            mult, mult_weight_params, mult_field_params = [], [], []
 
         if self.add_to_score:
-            add, add_params = map(list, zip(*([x.to_painless_script_and_params(i, "add_to_score") for i, x
+            add, add_weight_params, add_field_params = map(list, zip(*([x.to_painless_script_and_params(field_index, "add_to_score") for field_index, x
                                                in enumerate(self.add_to_score)])))
         else:
-            add, add_params = [], []
+            add, add_weight_params, add_field_params = [], [], []
 
         source_script = "\n".join(
             ["double additive = 0;"] + \
             mult + add + ["return Math.max(0.0, (_score + additive));"]
         )
 
-        params = {k: v for d in mult_params + add_params for k, v in d.items()}
+        params = {k: v for d in mult_weight_params + mult_field_params + add_weight_params + add_field_params for k, v in d.items()}
         script_score = {"source": f"""{source_script}""", "params": params}
-
         return script_score
