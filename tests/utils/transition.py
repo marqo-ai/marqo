dd85fc5539d562e3ac1cc093ea5a6be1ae07ee39
Joshua <joshua@marqo.ai>
2023-09-11 21:47:32 +1000

    Add max doc count limit to add_documents endpoint (#592)

--- tests/utils/transition.py
+++ tests/utils/transition.py
@@ -3,8 +3,11 @@ Our test suite is quite brittle. This helps the unit test suite navigate
 refactoring transitions in Marqo
 """
 from marqo.tensor_search.tensor_search import add_documents
+from marqo.tensor_search.configs import default_env_vars
 from marqo.tensor_search.models.add_docs_objects import AddDocsParams
+from marqo.tensor_search.enums import EnvVars
 from marqo.config import Config
+from copy import deepcopy
 
 def add_docs_caller(config: Config, **kwargs):
     """This represents the call signature of add_documents at commit
@@ -19,3 +22,19 @@ def add_docs_caller(config: Config, **kwargs):
         kwargs["device"] = "cpu"
     
     return add_documents(config=config, add_docs_params=AddDocsParams(**kwargs))
+
+def add_docs_batched(config: Config, batch_size: int = default_env_vars()[EnvVars.MARQO_MAX_ADD_DOCS_COUNT], **kwargs):
+    """
+    Helper function to batch large add_documents calls in testing
+    Default batch size is the default max add docs count env var
+
+    Must be called with raw kwargs (without AddDocsParams), as validation for this class will reject docs with large doc count.
+    """
+    docs = kwargs["docs"]
+    kwargs_without_docs = deepcopy(kwargs)
+    del kwargs_without_docs["docs"]
+
+    for i in range(0, len(docs), batch_size):
+        add_documents(
+            config=config, add_docs_params=AddDocsParams(docs=docs[i:i+batch_size], **kwargs_without_docs)
+        )
