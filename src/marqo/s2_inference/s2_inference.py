697574e0cd192d8020c7ac729e8a61f1e7f23197
danyilq <danyil@s2search.io>
2023-10-04 00:08:15 +0200

    replace model_size with memory_size

--- src/marqo/s2_inference/s2_inference.py
+++ src/marqo/s2_inference/s2_inference.py
@@ -303,8 +303,8 @@ def get_model_size(model_name: str, model_properties: dict) -> (int, float):
     Return the model size for given model
     Note that the priorities are size_in_properties -> model_name -> model_type -> default size
     '''
-    if "model_size" in model_properties:
-        return model_properties["model_size"]
+    if "memory_size" in model_properties:
+        return model_properties["memory_size"]
 
     name_info = (model_name + model_properties.get("name", "")).lower().replace("/", "-")
     for name, size in constants.MODEL_NAME_SIZE_MAPPING.items():
