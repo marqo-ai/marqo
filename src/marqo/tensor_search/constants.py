ae8f9881593f127045c2a749db2ff09c3fcd7d30
Joshua Kim <joshua@marqo.ai>
2023-10-05 00:21:44 +0800

    change allowed content field type constant to json schema form

--- src/marqo/tensor_search/constants.py
+++ src/marqo/tensor_search/constants.py
@@ -17,7 +17,8 @@ NON_TENSORISABLE_FIELD_TYPES = [int, float, bool, list]
 
 ALLOWED_MULTIMODAL_FIELD_TYPES = [str]
 
-ALLOWED_CUSTOM_VECTOR_CONTENT_TYPES = [str]
+# Must be written in JSON schema type form: https://json-schema.org/understanding-json-schema/reference/type
+ALLOWED_CUSTOM_VECTOR_CONTENT_TYPES = ["string"]
 
 LUCENE_SPECIAL_CHARS = {
     '/', '*', '^', '\\', '!', '[', '||', '?',
