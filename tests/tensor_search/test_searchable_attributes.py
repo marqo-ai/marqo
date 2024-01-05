e58c2c19c753981d97215a0c8f5e8824cb5d06c2
Li Wan <lwan3@student.unimelb.edu.au>
2024-01-05 11:16:40 +1100

    Add extra tests to ensure searchable attribuets work work with filter string

--- tests/tensor_search/test_searchable_attributes.py
+++ tests/tensor_search/test_searchable_attributes.py
@@ -23,68 +23,16 @@ class TestSearchStructured(MarqoTestCase):
                              features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
                 FieldRequest(name="text_field_3", type=FieldType.Text,
                              features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
-                FieldRequest(name="text_field_4", type=FieldType.Text,
-                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
-                FieldRequest(name="text_field_5", type=FieldType.Text,
-                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
-                FieldRequest(name="text_field_6", type=FieldType.Text,
-                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
-                FieldRequest(name="text_field_7", type=FieldType.Text,
-                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
-                FieldRequest(name="text_field_8", type=FieldType.Text,
-                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
-                FieldRequest(name="int_field_1", type=FieldType.Int,
-                             features=[FieldFeature.Filter]),
-                FieldRequest(name="float_field_1", type=FieldType.Float,
-                             features=[FieldFeature.Filter]),
-                FieldRequest(name="bool_field_1", type=FieldType.Bool,
-                             features=[FieldFeature.Filter]),
-                FieldRequest(name="list_field_1", type=FieldType.ArrayText,
-                             features=[FieldFeature.Filter]),
             ],
 
-            tensor_fields=["text_field_1", "text_field_2", "text_field_3",
-                           "text_field_4", "text_field_5", "text_field_6"]
-        )
-
-        default_image_index = cls.structured_marqo_index_request(
-            model=Model(name='open_clip/ViT-B-32/laion400m_e31'),
-            fields=[
-                FieldRequest(name="text_field_1", type=FieldType.Text,
-                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
-                FieldRequest(name="text_field_2", type=FieldType.Text,
-                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
-                FieldRequest(name="text_field_3", type=FieldType.Text,
-                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
-                FieldRequest(name="image_field_1", type=FieldType.ImagePointer),
-                FieldRequest(name="image_field_2", type=FieldType.ImagePointer),
-                FieldRequest(name="list_field_1", type=FieldType.ArrayText,
-                             features=[FieldFeature.Filter]),
-            ],
-            tensor_fields=["text_field_1", "text_field_2", "text_field_3", "image_field_1", "image_field_2"]
-        )
-
-        image_index_with_random_model = cls.structured_marqo_index_request(
-            model=Model(name='random/small'),
-            fields=[
-                FieldRequest(name="text_field_1", type=FieldType.Text,
-                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
-                FieldRequest(name="text_field_2", type=FieldType.Text,
-                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
-                FieldRequest(name="image_field_1", type=FieldType.ImagePointer)
-            ],
-            tensor_fields=["text_field_1", "text_field_2", "image_field_1"]
+            tensor_fields=["text_field_1", "text_field_2", "text_field_3"]
         )
 
         cls.indexes = cls.create_indexes([
             default_text_index,
-            default_image_index,
-            image_index_with_random_model
         ])
 
         cls.default_text_index = default_text_index.name
-        cls.default_image_index = default_image_index.name
-        cls.image_index_with_random_model = image_index_with_random_model.name
 
     def setUp(self) -> None:
         self.clear_indexes(self.indexes)
@@ -133,6 +81,45 @@ class TestSearchStructured(MarqoTestCase):
                 )
                 self.assertEqual(expected_id, res["hits"][0]["_id"])
 
+    def test_searchable_attributes_works_with_filter(self):
+        tensor_search.add_documents(
+            config=self.config,
+            add_docs_params=AddDocsParams(
+            index_name=self.default_text_index,
+            docs=[
+                {"text_field_1": "exact match field",
+                 "text_field_2": "baaadd efgh ",
+                 "text_field_3": "some field efgh",
+                 "_id": "1"},
+                {"text_field_1": "shouldn't really match ",
+                 "text_field_2": "exact match field",
+                 "text_field_3": "Random text here efgh",
+                 "_id": "2"},
+                {"text_field_1": "shouldn't really match ",
+                 "text_field_2": "shouldn't really match",
+                 "text_field_3": "exact match field",
+                 "_id": "3"},
+            ],
+            )
+        )
+
+        test_cases = [
+            (["text_field_1", "text_field_2"], "exact match field", "text_field_3:(some field efgh)", "1"),
+            (["text_field_1", "text_field_2"], "exact match field", "text_field_3:(Random text here efgh)", "2"),
+            (["text_field_2", "text_field_3"], "exact match field", "text_field_2:(shouldn't really match)", "3"),
+        ]
+        for search_method in ["TENSOR", "LEXICAL"]:
+            for searchable_attributes, query, filter_string, expected_id in test_cases:
+                with self.subTest(f"{search_method}-{searchable_attributes}-{filter_string}"):
+                    res = tensor_search.search(
+                        config=self.config,
+                        index_name=self.default_text_index,
+                        text=query,
+                        searchable_attributes=searchable_attributes,
+                        filter=filter_string
+                    )
+                    self.assertEqual(expected_id, res["hits"][0]["_id"])
+
     def test_searchable_attributes_empty_list(self):
         tensor_search.add_documents(
             config=self.config,
@@ -165,6 +152,43 @@ class TestSearchStructured(MarqoTestCase):
                 )
                 self.assertEqual(0, len(res["hits"]))
 
+    def test_searchable_attributes_empty_list_with_filter(self):
+        tensor_search.add_documents(
+            config=self.config,
+            add_docs_params=AddDocsParams(
+            index_name=self.default_text_index,
+            docs=[
+                {"text_field_1": "exact match field",
+                 "text_field_2": "baaadd efgh ",
+                 "text_field_3": "some field efgh ",
+                 "_id": "1"},
+                {"text_field_1": "shouldn't really match ",
+                 "text_field_2": "exact match field",
+                 "text_field_3": "Random text here efgh ",
+                 "_id": "2"},
+                {"text_field_1": "shouldn't really match ",
+                 "text_field_2": "shouldn't really match",
+                 "text_field_3": "exact match field",
+                 "_id": "3"},
+            ],
+            )
+        )
+
+        filter_string_cases = ["text_field_1:(exact match field)", "text_field_2:(exact match field)",
+                               "text_field_3:(exact match field)"]
+        for search_method in ["TENSOR", "LEXICAL"]:
+            for filter_string in filter_string_cases:
+                with self.subTest(f"{search_method}-{filter_string}"):
+                    res = tensor_search.search(
+                        config=self.config,
+                        index_name=self.default_text_index,
+                        text="exact match field",
+                        searchable_attributes=[],
+                        search_method=search_method,
+                        filter=filter_string
+                    )
+                    self.assertEqual(0, len(res["hits"]))
+
     def test_searchable_attributes_None(self):
         tensor_search.add_documents(
             config=self.config,
@@ -196,6 +220,3 @@ class TestSearchStructured(MarqoTestCase):
                     search_method=search_method
                 )
                 self.assertEqual(3, len(res["hits"]))
-
-
-
