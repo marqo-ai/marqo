9688d5069afda973f0e2170cbab9fe9b18742319
Li Wan <lwan3@student.unimelb.edu.au>
2024-01-16 12:24:38 +1100

    Add environment variable MARQO_CUDA_PATH to override the path of cuda for different platforms

--- run_marqo.sh
+++ run_marqo.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #source /opt/bash-utils/logger.sh
 export PYTHONPATH="${PYTHONPATH}:/app/src/"
-export CUDA_HOME=/usr/local/cuda/
+if [ -z "${MARQO_CUDA_PATH}" ]; then
+    export CUDA_HOME=/usr/local/cuda/
+else
+    export CUDA_HOME=${MARQO_CUDA_PATH}
+fi
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64
 export PATH=${CUDA_HOME}/bin:${PATH}
 
