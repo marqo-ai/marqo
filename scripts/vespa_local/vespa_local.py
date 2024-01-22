26f0c0e4d7ad5832709dc524c7bfff0580f24719
Joshua <joshua@marqo.ai>
2024-01-22 13:48:02 +0800

    increase memory usage to 16gb, delete zip file, change image to amd

--- scripts/vespa_local/vespa_local.py
+++ scripts/vespa_local/vespa_local.py
@@ -14,7 +14,7 @@ def start(args):
               "--name vespa "
               "--hostname vespa-container "
               "--publish 8080:8080 --publish 19071:19071 "
-              "--memory=\"8g\" "
+              "--memory=\"16g\" "
               "vespaengine/vespa")
 
 
