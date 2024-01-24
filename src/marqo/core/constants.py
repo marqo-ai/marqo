39ac74a93cfd8b4092d41f63ba58d808167dbedb
Farshid Zavareh <farshid@marqo.ai>
2024-01-25 09:48:16 +1100

    Remove unused variable

--- src/marqo/core/constants.py
+++ src/marqo/core/constants.py
@@ -4,4 +4,3 @@ MARQO_DOC_HIGHLIGHTS = '_highlights'  # doc-level so must not clash with index f
 MARQO_DOC_CHUNKS = 'chunks'
 MARQO_DOC_EMBEDDINGS = 'embeddings'
 MARQO_DOC_ID = '_id'
-MARQO_VESPA_QUERY_PROFILE = 'marqo_query_profile'
