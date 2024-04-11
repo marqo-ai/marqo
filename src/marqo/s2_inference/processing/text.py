4a3c900ebcb4435f3a58a749339c49283ace03b9
Raynor Chavez <raynorkirksonchavez@gmail.com>
2024-04-11 08:39:36 +0400

    prefix_text_chunks fxn in text.py s2_inference processing

--- src/marqo/s2_inference/processing/text.py
+++ src/marqo/s2_inference/processing/text.py
@@ -152,3 +152,14 @@ def split_text(text: str, split_by: str = 'sentence', split_length: int = 2, spl
 
     return text_splits
 
+def prefix_text_chunks(text_splits: List[str], text_chunk_prefix: str) -> List[str]:
+    """
+    Args:
+        text_splits (List[str]): Chunk list without prefixes
+        text_chunk_prefix (str): Prefix to add before each text chunk
+    Returns:
+        List[str]: Text splits with prefix prepended to each
+    """
+    if not text_chunk_prefix:
+        return text_splits
+    return [text_chunk_prefix + text for text in text_splits]
