a006ccece5e1f63315741c6d54202068b3d669bb
Li Wan <lwan3@student.unimelb.edu.au>
2024-04-16 14:09:43 +1000

    Add native

--- run_marqo.sh
+++ run_marqo.sh
@@ -145,7 +145,7 @@ MARQO_LOG_LEVEL=`echo "$MARQO_LOG_LEVEL" | tr '[:upper:]' '[:lower:]'`
 
 # Start the tensor search web app in the background
 cd /app/src/marqo/tensor_search || exit
-memray run --live-remote --live-port 8888 -m uvicorn api:app --host 0.0.0.0 --port 8882 --timeout-keep-alive 75 --log-level $MARQO_LOG_LEVEL &
+memray run --live-remote --native --live-port 8888 -m uvicorn api:app --host 0.0.0.0 --port 8882 --timeout-keep-alive 75 --log-level $MARQO_LOG_LEVEL &
 export api_pid=$!
 wait "$api_pid"
 
