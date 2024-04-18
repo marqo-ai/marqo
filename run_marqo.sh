a1783aebabc5f22edcd21a8766b326ea2791d6c9
Li Wan <lwan3@student.unimelb.edu.au>
2024-04-18 16:30:04 +1000

    Add --aggregate to memray run

--- run_marqo.sh
+++ run_marqo.sh
@@ -145,7 +145,7 @@ MARQO_LOG_LEVEL=`echo "$MARQO_LOG_LEVEL" | tr '[:upper:]' '[:lower:]'`
 
 # Start the tensor search web app in the background
 cd /app/src/marqo/tensor_search || exit
-memray run --out /opt/var/memray/240memray.bin --trace-python-allocators --follow-fork -m uvicorn api:app --host 0.0.0.0 --port 8882 --timeout-keep-alive 75 --log-level $MARQO_LOG_LEVEL &
+memray run --out /opt/var/memray/240memray.bin --aggregate --trace-python-allocators --follow-fork -m uvicorn api:app --host 0.0.0.0 --port 8882 --timeout-keep-alive 75 --log-level $MARQO_LOG_LEVEL &
 export api_pid=$!
 wait "$api_pid"
 
