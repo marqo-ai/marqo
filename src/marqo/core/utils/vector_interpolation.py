b5523951e3c42f0794cbe452a80ea799c4185d68
Farshid Zavareh <farshid@marqo.ai>
2024-04-26 15:24:57 +1000

    Fix SLERP implementation and add tests

--- src/marqo/core/utils/vector_interpolation.py
+++ src/marqo/core/utils/vector_interpolation.py
@@ -4,8 +4,6 @@ from enum import Enum
 from typing import List
 
 import numpy as np
-from scipy.spatial.transform import Rotation as R
-from scipy.spatial.transform import Slerp
 
 from marqo.core.models.interpolation_method import InterpolationMethod
 from marqo.exceptions import InternalError
@@ -13,7 +11,7 @@ from marqo.exceptions import InternalError
 
 class VectorInterpolation(abc.ABC):
     @abc.abstractmethod
-    def interpolate(self, vectors: List[List[float]], weights: List[float] = None) -> List[float]:
+    def interpolate(self, vectors: List[List[float]], weights: List[float], prenormalized: bool = False) -> List[float]:
         """
         Interpolates a list of vectors using the given weights.
 
@@ -21,6 +19,7 @@ class VectorInterpolation(abc.ABC):
             vectors: A list of vectors to interpolate.
             weights: A list of weights to use for the interpolation. If None, the interpolation will be done using
             euqual weights for all vectors.
+            prenormalized: If True, the vectors are assumed to be normalized
 
         Returns:
             The interpolated vector.
@@ -40,7 +39,7 @@ def from_interpolation_method(method: InterpolationMethod):
 
 
 class Lerp(VectorInterpolation):
-    def interpolate(self, vectors: List[List[float]], weights: List[float] = None) -> List[float]:
+    def interpolate(self, vectors: List[List[float]], weights: List[float], prenormalized: bool = False) -> List[float]:
         if len(vectors) < 1:
             raise ValueError('Cannot interpolate an empty list of vectors')
 
@@ -55,7 +54,8 @@ class Lerp(VectorInterpolation):
 
 
 class Nlerp(VectorInterpolation):
-    def interpolate(self, vectors: List[List[float]], weights: List[float] = None) -> List[float]:
+    def interpolate(self, vectors: List[List[float]], weights: List[float],
+                    prenormalized: bool = False) -> List[float]:
         lerp_result = Lerp().interpolate(vectors, weights)
         length = math.sqrt(sum(x ** 2 for x in lerp_result))
         return [x / length for x in lerp_result]
@@ -68,30 +68,50 @@ class Slerp(VectorInterpolation):
 
     def __init__(self, method: Method = Method.Hierarchical):
         self.method = method
-        self._slerp_impl = self._slerp
 
-    def _slerp_scipy(self, v1: List[float], v2: List[float], t: float) -> List[float]:
-        # Convert vectors to quaternions (assuming vectors are 3D)
-        q1 = np.array([0] + v1)
-        q2 = np.array([0] + v2)
+    def interpolate(self, vectors: List[List[float]], weights: List[float], prenormalized: bool = False) -> List[float]:
+        if len(vectors) < 1:
+            raise ValueError('Cannot interpolate an empty list of vectors')
 
-        # Create a Slerp object and interpolate
-        slerp = Slerp([0, 1], R.from_quat([q1, q2]))
-        interpolated_quaternion = slerp([t]).as_quat()[0]
+        if self.method == self.Method.Sequential:
+            return self._interpolate_sequential(vectors, weights)
+        elif self.method == self.Method.Hierarchical:
+            return self._interpolate_hierarchical(vectors, weights)
+        else:
+            raise InternalError(f'Unknown interpolation method: {self.method}')
 
-        # Return the vector part of the interpolated quaternion
-        return interpolated_quaternion[1:].tolist()
+    def _slerp(self, v0: List[float], v1: List[float], t: float, prenormalized: bool = False) -> List[float]:
+        v0, v1 = np.array(v0), np.array(v1)
 
-    def _slerp(self, v0: List[float], v1: List[float], t: float) -> List[float]:
-        dot = sum(a * b for a, b in zip(v0, v1))
-        theta = math.acos(dot) * t
-        relative_vec = [b - a * dot for a, b in zip(v0, v1)]
-        return [math.cos(theta) * a + math.sin(theta) * b for a, b in zip(v0, relative_vec)]
+        dot = np.dot(v0, v1)
+
+        if not prenormalized:
+            norm_v0 = np.linalg.norm(v0)
+            norm_v1 = np.linalg.norm(v1)
+            dot = dot / (norm_v0 * norm_v1)
+
+        # Ensure the dot product is within the range [-1, 1]
+        dot = np.clip(dot, -1.0, 1.0)
+
+        theta = math.acos(dot)
+
+        sin_theta = math.sin(theta)
+        if sin_theta == 0:
+            # Co-linear vectors, return linear interpolation
+            return ((1 - t) * v0 + t * v1).tolist()
+
+        slerp_v0 = math.sin((1 - t) * theta) / sin_theta * v0
+        slerp_v1 = math.sin(t * theta) / sin_theta * v1
+
+        result = slerp_v0 + slerp_v1
+        return result.tolist()
 
     def _interpolate_sequential(self, vectors: List[List[float]], weights: List[float]) -> List[float]:
+        weights_copy = weights.copy()
         result = vectors[0]
         for i in range(1, len(vectors)):
-            result = self._slerp_impl(result, vectors[i], weights[i])
+            result = self._slerp(result, vectors[i], weights_copy[i] / (weights_copy[i - 1] + weights_copy[i]))
+            weights_copy[i] = (weights_copy[i - 1] + weights_copy[i]) / 2
         return result
 
     def _interpolate_hierarchical(self, vectors: List[List[float]], weights: List[float]) -> List[float]:
@@ -106,21 +126,10 @@ class Slerp(VectorInterpolation):
                     continue
 
                 result.append(
-                    self._slerp_impl(vectors[i], vectors[i + 1], weights[i + 1] / (weights[i] + weights[i + 1]))
+                    self._slerp(vectors[i], vectors[i + 1], weights[i + 1] / (weights[i] + weights[i + 1]))
                 )
                 new_weights.append((weights[i] + weights[i + 1]) / 2)
             vectors = result
             weights = new_weights
 
         return vectors[0]
-
-    def interpolate(self, vectors: List[List[float]], weights: List[float] = None) -> List[float]:
-        if len(vectors) < 1:
-            raise ValueError('Cannot interpolate an empty list of vectors')
-
-        if self.method == self.Method.Sequential:
-            return self._interpolate_sequential(vectors, weights)
-        elif self.method == self.Method.Hierarchical:
-            return self._interpolate_hierarchical(vectors, weights)
-        else:
-            raise InternalError(f'Unknown interpolation method: {self.method}')
--- tests/core/utils/test_vector_interpolation.py
+++ tests/core/utils/test_vector_interpolation.py
@@ -85,29 +85,246 @@ class TestSlerp(unittest.TestCase):
         ]
         self.weights = [0.5, 0.5]  # Equal weighting for a simple midpoint test
 
-    def test_sequential_interpolation_simple(self):
+    def test_interpolate_sequential_success(self):
+        cases = [
+            (
+                [
+                    [math.sqrt(0.5), math.sqrt(0.5), 0]
+                ],
+                [1],
+                [math.sqrt(0.5), math.sqrt(0.5), 0],
+                'Single vector'
+            ),
+            (
+                [
+                    [math.sqrt(0.5), math.sqrt(0.5), 0]
+                ],
+                [2],
+                [math.sqrt(0.5), math.sqrt(0.5), 0],
+                'Single vector, weight 2'
+            ),
+            (
+                [
+                    [math.sqrt(0.5), math.sqrt(0.5), 0]
+                ] * 2,
+                [1] * 2,
+                [math.sqrt(0.5), math.sqrt(0.5), 0],
+                'Same vector * 2'
+            ),
+            (
+                [
+                    [math.sqrt(0.5), math.sqrt(0.5), 0]
+                ] * 2,
+                [2] * 2,
+                [math.sqrt(0.5), math.sqrt(0.5), 0],
+                'Same vector * 2, weight 2'
+            ),
+            (
+                [
+                    [math.sqrt(0.5), math.sqrt(0.5), 0]
+                ] * 5,
+                [1] * 5,
+                [math.sqrt(0.5), math.sqrt(0.5), 0],
+                'Same vector * 5'
+            ),
+            (
+                [
+                    [1, 0, 0],
+                    [0, 1, 0]
+                ],
+                [0.5, 0.5],
+                [math.sqrt(2) / 2, math.sqrt(2) / 2, 0],
+                '2 vectors, 45 degrees rotation around z-axis'
+            ),
+            (
+                [
+                    [0.24079554, -0.50855556, -0.69860205, 0.4419773],
+                    [0.60970949, 0.4784225, 0.61885735, -0.12799152],
+                    [0.76591685, -0.59245083, 0.08972328, 0.23307321]
+                ],
+                [1, 2, 0.5],
+                [0.9582755764466467, -0.03248832000519242, 0.1691362691503027, 0.22813450030113333],
+                '3 vectors'
+            ),
+            (
+                [
+                    [0.24079554, -0.50855556, -0.69860205, 0.4419773],
+                    [0.60970949, 0.4784225, 0.61885735, -0.12799152],
+                    [0.76591685, -0.59245083, 0.08972328, 0.23307321],
+                    [0.53470714, 0.7637857, 0.36148952, -0.0067081]
+                ],
+                [1, -0.5, 2, 1.5],
+                [0.8742225032825055, 0.13177697554091694, 0.46012597200027167, 0.0815715999267052],
+                '4 vectors'
+            ),
+        ]
+
         slerp = Slerp(Slerp.Method.Sequential)
-        result = slerp.interpolate(self.vectors, self.weights)
-        expected = [math.cos(math.pi / 4), math.sin(math.pi / 4), 0]  # 45 degrees rotation around z-axis
-        np.testing.assert_array_almost_equal(result, expected, decimal=5)
 
-    def test_hierarchical_interpolation_simple(self):
+        for vectors, weights, expected, msg in cases:
+            for prenormalized in [True, False]:
+                with self.subTest(case=msg, prenormalized=prenormalized):
+                    result = slerp.interpolate(vectors, weights, prenormalized=prenormalized)
+                    np.testing.assert_array_almost_equal(result, expected, decimal=5)
+
+    def test_interpolate_hierarchical_success(self):
+        cases = [
+            (
+                [
+                    [math.sqrt(0.5), math.sqrt(0.5), 0]
+                ],
+                [1],
+                [math.sqrt(0.5), math.sqrt(0.5), 0],
+                'Single vector'
+            ),
+            (
+                [
+                    [math.sqrt(0.5), math.sqrt(0.5), 0]
+                ],
+                [2],
+                [math.sqrt(0.5), math.sqrt(0.5), 0],
+                'Single vector, weight 2'
+            ),
+            (
+                [
+                    [math.sqrt(0.5), math.sqrt(0.5), 0]
+                ] * 2,
+                [1] * 2,
+                [math.sqrt(0.5), math.sqrt(0.5), 0],
+                'Same vector * 2'
+            ),
+            (
+                [
+                    [math.sqrt(0.5), math.sqrt(0.5), 0]
+                ] * 2,
+                [2] * 2,
+                [math.sqrt(0.5), math.sqrt(0.5), 0],
+                'Same vector * 2, weight 2'
+            ),
+            (
+                [
+                    [math.sqrt(0.5), math.sqrt(0.5), 0]
+                ] * 5,
+                [1] * 5,
+                [math.sqrt(0.5), math.sqrt(0.5), 0],
+                'Same vector * 5'
+            ),
+            (
+                [
+                    [1, 0, 0],
+                    [0, 1, 0]
+                ],
+                [0.5, 0.5],
+                [math.sqrt(2) / 2, math.sqrt(2) / 2, 0],
+                '2 vectors, 45 degrees rotation around z-axis'
+            ),
+            (
+                [
+                    [0.24079554, -0.50855556, -0.69860205, 0.4419773],
+                    [0.60970949, 0.4784225, 0.61885735, -0.12799152],
+                    [0.76591685, -0.59245083, 0.08972328, 0.23307321]
+                ],
+                [1, 2, 0.5],
+                [0.9582755764466467, -0.03248832000519242, 0.1691362691503027, 0.22813450030113333],
+                '3 vectors, odd number'
+            ),
+            (
+                [
+                    [0.24079554, -0.50855556, -0.69860205, 0.4419773],
+                    [0.60970949, 0.4784225, 0.61885735, -0.12799152],
+                    [0.76591685, -0.59245083, 0.08972328, 0.23307321],
+                    [0.53470714, 0.7637857, 0.36148952, -0.0067081]
+                ],
+                [1, -0.5, 2, 1.5],
+                [0.8307157370201422, 0.0889292113769262, 0.5487942602028577, 0.028771684936241104],
+                '4 vectors, even power of 2'
+            ),
+            (
+                [
+                    [0.24079554, -0.50855556, -0.69860205, 0.4419773],
+                    [0.60970949, 0.4784225, 0.61885735, -0.12799152],
+                    [0.76591685, -0.59245083, 0.08972328, 0.23307321],
+                    [0.53470714, 0.7637857, 0.36148952, -0.0067081],
+                    [0.06271936, 0.67834342, -0.56283931, -0.46811152],
+                    [0.30926992, -0.25873565, -0.40838477, -0.81891994]
+                ],
+                [1, -0.5, 2, 1.5, 0.25, 0.9],
+                [0.8759481090365622, 0.05428666526306154, 0.19410932061327218, -0.4382800871883284],
+                '6 vectors, even not power of 2'
+            ),
+        ]
+
         slerp = Slerp(Slerp.Method.Hierarchical)
-        result = slerp.interpolate(self.vectors, self.weights)
-        expected = [math.cos(math.pi / 4), math.sin(math.pi / 4), 0]  # Same expectation for simple case
-        np.testing.assert_array_almost_equal(result, expected, decimal=5)
 
-    def test_single_vector(self):
-        slerp = Slerp()
-        result = slerp.interpolate([self.vectors[0]])
-        np.testing.assert_array_almost_equal(result, self.vectors[0])
+        for vectors, weights, expected, msg in cases:
+            for prenormalized in [True, False]:
+                with self.subTest(case=msg, prenormalized=prenormalized):
+                    result = slerp.interpolate(vectors, weights, prenormalized=prenormalized)
+                    np.testing.assert_array_almost_equal(result, expected, decimal=5)
 
-    def test_empty_vectors(self):
-        slerp = Slerp()
-        with self.assertRaises(ValueError):
-            slerp.interpolate([])
+    def test_interpolate_nonUnitNorm_success(self):
+        vectors = [
+            [0.24079554, -0.50855556, -0.69860205, 0.4419773],
+            [0.60970949, 0.4784225, 0.61885735, -0.12799152],
+            [0.76591685, -0.59245083, 0.08972328, 0.23307321],
+            [0.53470714, 1, 0.36148952, -2.0067081, 0.36148952]
+        ]
+        weights = [1, -0.5, 2, 1.5]
+        expected = []
 
-    def test_interpolation_method_mismatch(self):
+    def test_interpolate_colinearVectors_success(self):
+        pass
+
+    def test_interpolate_zeroVector_failure(self):
+        pass
+
+    def test_interpolate_emptyVectors_failure(self):
+        for method in [Slerp.Method.Sequential, Slerp.Method.Hierarchical]:
+            with self.subTest(method=method):
+                slerp = Slerp(method)
+                with self.assertRaises(ValueError):
+                    slerp.interpolate([])
+
+    def test_interpolate_differentVectorLengths_failure(self):
+        vectors = [
+            [1, 0, 0],
+            [0, 1],  # length 2
+            [0, 0, 1]
+        ]
+        weights = [1, 1, 1]
+        for method in [Slerp.Method.Sequential, Slerp.Method.Hierarchical]:
+            with self.subTest(method=method):
+                slerp = Slerp(method)
+                with self.assertRaises(ValueError):
+                    slerp.interpolate(vectors, [1] * 3)
+
+    def test_interpolate_wrongWeightsLength_failure(self):
+        vectors = [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1]
+        ]
+        weights = [1] * 4
+        for method in [Slerp.Method.Sequential, Slerp.Method.Hierarchical]:
+            with self.subTest(method=method):
+                slerp = Slerp(method)
+                with self.assertRaises(ValueError):
+                    slerp.interpolate(vectors, weights)
+
+    def test_interpolate_missingWeights_failure(self):
+        vectors = [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1]
+        ]
+        weights = [1] * 4
+        for method in [Slerp.Method.Sequential, Slerp.Method.Hierarchical]:
+            with self.subTest(method=method):
+                slerp = Slerp(method)
+                with self.assertRaises(ValueError):
+                    slerp.interpolate(vectors, None)
+
+    def test_wrongInterpolationMethod_failure(self):
         slerp = Slerp("non_existing_method")
         with self.assertRaises(InternalError):  # Changed to AttributeError
             slerp.interpolate(self.vectors, self.weights)
