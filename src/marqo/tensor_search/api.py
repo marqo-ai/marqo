6da067b7578a6a7a89aec76cb237f9bd88f7ba27
Ali K <ali@s2search.io>
2024-05-01 15:02:44 +1000

    return values

--- src/marqo/tensor_search/api.py
+++ src/marqo/tensor_search/api.py
@@ -197,14 +197,23 @@ def memory():
 @utils.enable_ops_api()
 def schema_validation(index_name: str, settings_object: dict):
     (valid, error) = IndexManagement.validate_index_settings(index_name, settings_object)
-    return JSONResponse(
-        content={
-            "validated": valid,
-            "validationError": error,
-            "index": index_name
-        },
-        status_code=200 if valid else 400
-    )
+    if valid:
+        return JSONResponse(
+            content={
+                "validated": valid,
+                "index": index_name
+            },
+            status_code=200
+        )
+    else:
+        return JSONResponse(
+            content={
+                "validated": valid,
+                "validationError": error,
+                "index": index_name
+            },
+            status_code=400
+        )
 
 
 @app.post("/indexes/{index_name}")
