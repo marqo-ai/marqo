38c725f9c1bd2c17aa0670b4b9eca81f75d20e1c
Li Wan <lwan3@student.unimelb.edu.au>
2024-06-07 16:20:52 +1000

    Add MARQO_WORKERS environment variable

--- run_marqo.sh
+++ run_marqo.sh
@@ -143,10 +143,11 @@ fi
 # set the default value to info and convert to lower case
 export MARQO_LOG_LEVEL=${MARQO_LOG_LEVEL:-info}
 MARQO_LOG_LEVEL=`echo "$MARQO_LOG_LEVEL" | tr '[:upper:]' '[:lower:]'`
+export MARQO_WORKERS=${MARQO_WORKERS:-1}
 
 # Start the tensor search web app in the background
 cd /app/src/marqo/tensor_search || exit
-uvicorn api:app --host 0.0.0.0 --port 8882 --timeout-keep-alive 75 --log-level $MARQO_LOG_LEVEL &
+uvicorn api:app --host 0.0.0.0 --port 8882 --workers $MARQO_WORKERS --timeout-keep-alive 75 --log-level $MARQO_LOG_LEVEL &
 export api_pid=$!
 wait "$api_pid"
 
