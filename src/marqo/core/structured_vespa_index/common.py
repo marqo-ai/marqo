209f672ae25ef1a4845638dff6babf7e2e77aa3b
Joshua <joshua@marqo.ai>
2024-06-27 15:31:53 +0800

    add retry to jdk install

--- src/marqo/core/structured_vespa_index/common.py
+++ src/marqo/core/structured_vespa_index/common.py
@@ -15,10 +15,6 @@ RANK_PROFILE_EMBEDDING_SIMILARITY_MODIFIERS = 'embedding_similarity_modifiers'
 
 # For hybrid search
 RANK_PROFILE_HYBRID_CUSTOM_SEARCHER = 'hybrid_custom_searcher'
-RANK_PROFILE_HYBRID_RRF = 'hybrid_rrf'
-RANK_PROFILE_HYBRID_RRF_MODIFIERS = 'hybrid_rrf_modifiers'
-RANK_PROFILE_HYBRID_NORMALIZE_LINEAR = 'hybrid_normalize_linear'
-RANK_PROFILE_HYBRID_NORMALIZE_LINEAR_MODIFIERS = 'hybrid_normalize_linear_modifiers'
 
 # Note field names are also used as query inputs, so make sure these reserved names have a marqo__ prefix
 QUERY_INPUT_EMBEDDING = 'marqo__query_embedding'
