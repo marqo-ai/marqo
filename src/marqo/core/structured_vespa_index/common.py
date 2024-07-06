1b7beec3495bbca1386d84d2621ecdb397ad3fe1
Farshid Zavareh <farshid@marqo.ai>
2024-07-06 21:27:58 +1000

    Update rank profiles

--- src/marqo/core/structured_vespa_index/common.py
+++ src/marqo/core/structured_vespa_index/common.py
@@ -18,19 +18,22 @@ RANK_PROFILE_BASE = 'base_rank_profile'
 RANK_PROFILE_HYBRID_CUSTOM_SEARCHER = 'hybrid_custom_searcher'
 RANK_PROFILE_HYBRID_BM25_THEN_EMBEDDING_SIMILARITY = 'hybrid_bm25_then_embedding_similarity'
 RANK_PROFILE_HYBRID_EMBEDDING_SIMILARITY_THEN_BM25 = 'hybrid_embedding_similarity_then_bm25'
-RANK_FEATURE_PREFIX_LEXICAL = 'marqo__lexical_'
-RANK_FEATURE_PREFIX_TENSOR = 'marqo__tensor_'
 RESULT_FIELD_RAW_TENSOR_SCORE = 'marqo__raw_tensor_score'
 RESULT_FIELD_RAW_LEXICAL_SCORE = 'marqo__raw_lexical_score'
 
 # Note field names are also used as query inputs, so make sure these reserved names have a marqo__ prefix
 QUERY_INPUT_EMBEDDING = 'marqo__query_embedding'
-QUERY_INPUT_SCORE_MODIFIERS_MULT_WEIGHTS = 'marqo__mult_weights'
-QUERY_INPUT_SCORE_MODIFIERS_ADD_WEIGHTS = 'marqo__add_weights'
+QUERY_INPUT_SCORE_MODIFIERS_MULT_WEIGHTS_2_9 = 'marqo__mult_weights'
+QUERY_INPUT_SCORE_MODIFIERS_ADD_WEIGHTS_2_9 = 'marqo__add_weights'
+QUERY_INPUT_SCORE_MODIFIERS_MULT_WEIGHTS_LEXICAL = 'marqo__mult_weights_lexical'
+QUERY_INPUT_SCORE_MODIFIERS_ADD_WEIGHTS_LEXICAL = 'marqo__add_weights_lexical'
+QUERY_INPUT_SCORE_MODIFIERS_MULT_WEIGHTS_TENSOR = 'marqo__mult_weights_tensor'
+QUERY_INPUT_SCORE_MODIFIERS_ADD_WEIGHTS_TENSOR = 'marqo__add_weights_tensor'
 # Used for hybrid search (split in custom searcher)
 QUERY_INPUT_FIELDS_TO_RANK_LEXICAL = "marqo__fields_to_rank_lexical"
 QUERY_INPUT_FIELDS_TO_RANK_TENSOR = "marqo__fields_to_rank_tensor"
 QUERY_INPUT_BM25_AGGREGATOR = 'marqo__bm25_aggregator'
+
 
 SUMMARY_ALL_NON_VECTOR = 'all-non-vector-summary'
 SUMMARY_ALL_VECTOR = 'all-vector-summary'
