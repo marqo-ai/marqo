c4402af88957cff65d204fb3a9e3b3b92d94ff61
Farshid Zavareh <farshid@marqo.ai>
2024-07-07 19:25:55 +1000

    Fix Java formatting

--- scripts/vespa_local/src/main/java/ai/marqo/search/HybridSearcher.java
+++ scripts/vespa_local/src/main/java/ai/marqo/search/HybridSearcher.java
@@ -325,15 +325,26 @@ public class HybridSearcher extends Searcher {
         // YQL uses RETRIEVAL method
         String yqlNew = query.properties().getString("marqo__yql." + retrievalMethod, "");
         // Rank Profile uses RETRIEVAL + RANKING method
-        String rankProfileNew = query.properties().getString("marqo__ranking." + retrievalMethod + "." + rankingMethod, "");
+        String rankProfileNew =
+                query.properties()
+                        .getString("marqo__ranking." + retrievalMethod + "." + rankingMethod, "");
         String rankProfileNewScoreModifiers =
                 query.properties()
-                        .getString("marqo__ranking." + retrievalMethod + "." + rankingMethod + "ScoreModifiers", "");
+                        .getString(
+                                "marqo__ranking."
+                                        + retrievalMethod
+                                        + "."
+                                        + rankingMethod
+                                        + "ScoreModifiers",
+                                "");
 
         // Log fetched properties
         logIfVerbose(String.format("YQL %s found: %s", retrievalMethod, yqlNew), verbose);
         logIfVerbose(
-                String.format("Rank Profile %s.%s found: %s", retrievalMethod, rankingMethod, rankProfileNew), verbose);
+                String.format(
+                        "Rank Profile %s.%s found: %s",
+                        retrievalMethod, rankingMethod, rankProfileNew),
+                verbose);
         logIfVerbose(
                 String.format(
                         "Rank Profile %s.%s score modifiers found: %s",
@@ -356,7 +367,8 @@ public class HybridSearcher extends Searcher {
                                 cell, queryNew, retrievalMethod, rankingMethod, verbose));
 
         // Set rank profile (using RANKING method)
-        // TODO: Use rankProfileNewScoreModifiers also if retrievalMethod="lexical" + rankingMethod="tensor", and either are present.
+        // TODO: Use rankProfileNewScoreModifiers also if retrievalMethod="lexical" +
+        // rankingMethod="tensor", and either are present.
         if (query.properties()
                 .getBoolean("marqo__hybrid." + rankingMethod + "ScoreModifiersPresent")) {
             // With Score Modifiers (using RANKING method)
