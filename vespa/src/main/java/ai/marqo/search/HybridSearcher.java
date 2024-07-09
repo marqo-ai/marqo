dc301fe591e4db6ce9cd914e5feaa70ea3f34e39
yihanzhao <yihan@marqo.ai>
2024-07-09 10:56:50 +1000

    WIP refactored the main method

--- vespa/src/main/java/ai/marqo/search/HybridSearcher.java
+++ vespa/src/main/java/ai/marqo/search/HybridSearcher.java
@@ -9,14 +9,7 @@ import com.yahoo.search.result.Hit;
 import com.yahoo.search.result.HitGroup;
 import com.yahoo.search.searchchain.AsyncExecution;
 import com.yahoo.search.searchchain.Execution;
-import com.yahoo.tensor.Tensor;
-import com.yahoo.tensor.Tensor.Cell;
-import com.yahoo.tensor.TensorAddress;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -35,55 +28,19 @@ public class HybridSearcher extends Searcher {
 
     Logger logger = LoggerFactory.getLogger(HybridSearcher.class);
 
-    private static String MATCH_FEATURES_FIELD = "matchfeatures";
-    private static String QUERY_INPUT_SCORE_MODIFIERS_MULT_WEIGHTS = "marqo__mult_weights";
-    private static String QUERY_INPUT_SCORE_MODIFIERS_ADD_WEIGHTS = "marqo__add_weights";
-    private static String QUERY_INPUT_FIELDS_TO_RANK = "marqo__fields_to_rank";
-    private static String MARQO_SEARCH_METHOD_LEXICAL = "lexical";
-    private static String MARQO_SEARCH_METHOD_TENSOR = "tensor";
-    private List<String> STANDARD_SEARCH_TYPES = new ArrayList<>();
-
     @Override
     public Result search(Query query, Execution execution) {
-        // All query parameters starting with 'marqo__' are custom for Marqo hybrid search.
-
-        // Retrieval methods: disjunction, tensor, lexical
-        // Ranking methods: rrf, normalize_linear, tensor, lexical
-        STANDARD_SEARCH_TYPES.add(MARQO_SEARCH_METHOD_LEXICAL);
-        STANDARD_SEARCH_TYPES.add(MARQO_SEARCH_METHOD_TENSOR);
-
         boolean verbose = query.properties().getBoolean("marqo__hybrid.verbose", false);
-
         logIfVerbose("Starting Hybrid Search script.", verbose);
 
-        String retrievalMethod = query.properties().getString("marqo__hybrid.retrievalMethod", "");
-        String rankingMethod = query.properties().getString("marqo__hybrid.rankingMethod", "");
+        MarqoQuery marqoQuery = MarqoQuery.from(query);
 
-        Integer rrf_k = query.properties().getInteger("marqo__hybrid.rrf_k", 60);
-        Double alpha = query.properties().getDouble("marqo__hybrid.alpha", 0.5);
-        Integer timeout = query.properties().getInteger("timeout", 1000);
-
-        // Log fetched variables
-        logIfVerbose(String.format("Retrieval method found: %s", retrievalMethod), verbose);
-        logIfVerbose(String.format("Ranking method found: %s", rankingMethod), verbose);
-        logIfVerbose(String.format("alpha found: %.2f", alpha), verbose);
-        logIfVerbose(String.format("RRF k found: %d", rrf_k), verbose);
-        logIfVerbose(String.format("Timeout int found: %d", timeout), verbose);
-
-        logIfVerbose(String.format("Base Query is: "), verbose);
-        logIfVerbose(query.toDetailString(), verbose);
-
-        if (retrievalMethod.equals("disjunction")) {
+        if (marqoQuery.getRetrievalMethod() == RetrievalMethod.disjunction) {
             Result resultLexical, resultTensor;
             Query queryLexical =
-                    createSubQuery(
-                            query,
-                            MARQO_SEARCH_METHOD_LEXICAL,
-                            MARQO_SEARCH_METHOD_LEXICAL,
-                            verbose);
+                    marqoQuery.createSubQuery(RetrievalMethod.lexical, RankingMethod.lexical);
             Query queryTensor =
-                    createSubQuery(
-                            query, MARQO_SEARCH_METHOD_TENSOR, MARQO_SEARCH_METHOD_TENSOR, verbose);
+                    marqoQuery.createSubQuery(RetrievalMethod.tensor, RankingMethod.tensor);
 
             // Execute both searches async
             AsyncExecution asyncExecutionLexical = new AsyncExecution(execution);
@@ -91,53 +48,36 @@ public class HybridSearcher extends Searcher {
             AsyncExecution asyncExecutionTensor = new AsyncExecution(execution);
             Future<Result> futureTensor = asyncExecutionTensor.search(queryTensor);
             try {
-                resultLexical = futureLexical.get(timeout, TimeUnit.MILLISECONDS);
-                resultTensor = futureTensor.get(timeout, TimeUnit.MILLISECONDS);
+                resultLexical =
+                        futureLexical.get(marqoQuery.getTimeoutMillis(), TimeUnit.MILLISECONDS);
+                resultTensor =
+                        futureTensor.get(marqoQuery.getTimeoutMillis(), TimeUnit.MILLISECONDS);
             } catch (TimeoutException | InterruptedException | ExecutionException e) {
-                throw new RuntimeException(
-                        String.format(
-                                        "Hybrid search disjunction timeout error. Current timeout:"
-                                                + " %d. ",
-                                        timeout)
-                                + e.toString());
+                logger.warn(
+                        "Hybrid search disjunction timeout error. Current timeout:{}",
+                        marqoQuery.getTimeoutMillis(),
+                        e);
+                throw new RuntimeException(e);
             }
-
-            logIfVerbose(
-                    "LEXICAL RESULTS: "
-                            + resultLexical.toString()
-                            + " || TENSOR RESULTS: "
-                            + resultTensor.toString(),
-                    verbose);
 
             // Execute fusion ranking on 2 results.
-            if (rankingMethod.equals("rrf")) {
-                HitGroup fusedHitList =
-                        rrf(resultTensor.hits(), resultLexical.hits(), rrf_k, alpha, verbose);
-                logIfVerbose("RRF Fused Hit Group", verbose);
-                logHitGroup(fusedHitList, verbose);
-                Result tempResult = new Result(query, fusedHitList);
-                return tempResult;
-            } else {
-                throw new RuntimeException(
-                        "For retrievalMethod='disjunction', rankingMethod must be 'rrf'.");
-            }
+            HitGroup fusedHitList =
+                    rrf(
+                            resultTensor.hits(),
+                            resultLexical.hits(),
+                            marqoQuery.getRrfK(),
+                            marqoQuery.getAlpha(),
+                            verbose);
+            query.trace(new NamedHitGroup("RRF Fused", fusedHitList), 1);
+            return new Result(query, fusedHitList);
 
-        } else if (STANDARD_SEARCH_TYPES.contains(retrievalMethod)) {
-            if (STANDARD_SEARCH_TYPES.contains(rankingMethod)) {
-                Query combinedQuery =
-                        createSubQuery(query, retrievalMethod, rankingMethod, verbose);
-                Result result = execution.search(combinedQuery);
-                logIfVerbose("Results: ", verbose);
-                logHitGroup(result.hits(), verbose);
-                return execution.search(combinedQuery);
-            } else {
-                throw new RuntimeException(
-                        "If retrievalMethod is 'lexical' or 'tensor', rankingMethod can only be"
-                                + " 'lexical', or 'tensor'.");
-            }
         } else {
-            throw new RuntimeException(
-                    "retrievalMethod can only be 'disjunction', 'lexical', or 'tensor'.");
+            Query combinedQuery = marqoQuery.createSubQuery();
+            Result result = execution.search(combinedQuery);
+            query.trace(
+                    new NamedHitGroup(marqoQuery.getRetrievalMethod() + " Results:", result.hits()),
+                    1);
+            return result;
         }
     }
 
@@ -278,126 +218,6 @@ public class HybridSearcher extends Searcher {
     }
 
     /**
-     * Extracts mapped Tensor Address from cell then adds it as key to rank features, with cell value as the value.
-     * @param cell
-     * @param query
-     * @param verbose
-     */
-    void addFieldToRankFeatures(
-            Cell cell, Query query, String retrievalMethod, String rankingMethod, boolean verbose) {
-        TensorAddress cellKey = cell.getKey();
-        String queryInputString;
-        int dimensions = cellKey.size();
-        for (int i = 0; i < dimensions; i++) {
-            queryInputString = addQueryWrapper(cellKey.label(i));
-            query.getRanking().getFeatures().put(queryInputString, cell.getValue());
-            logIfVerbose(
-                    String.format(
-                            "Setting Rank Feature %s to %s", queryInputString, cell.getValue()),
-                    verbose);
-        }
-    }
-
-    /**
-     * Creates custom sub-query from the original query.
-     * Clone original query, Update the following:
-     * 'yql' (based on RETRIEVAL method)
-     * 'ranking.profile'    (based on RANKING method)
-     * 'ranking.features'
-     *      fields to search  (based on ??? method)
-     *      score modifiers (based on RANKING method)
-     * @param query
-     * @param retrievalMethod
-     * @param rankingMethod
-     * @param verbose
-     */
-    Query createSubQuery(
-            Query query, String retrievalMethod, String rankingMethod, boolean verbose) {
-        logIfVerbose(
-                String.format(
-                        "Creating subquery with retrieval: %s, ranking: %s",
-                        retrievalMethod, rankingMethod),
-                verbose);
-
-        // Extract relevant properties
-        // YQL uses RETRIEVAL method
-        String yqlNew = query.properties().getString("marqo__yql." + retrievalMethod, "");
-        // Rank Profile uses RETRIEVAL + RANKING method
-        String rankProfileNew =
-                query.properties()
-                        .getString("marqo__ranking." + retrievalMethod + "." + rankingMethod, "");
-
-        // Log fetched properties
-        logIfVerbose(String.format("YQL %s found: %s", retrievalMethod, yqlNew), verbose);
-        logIfVerbose(
-                String.format(
-                        "Rank Profile %s.%s found: %s",
-                        retrievalMethod, rankingMethod, rankProfileNew),
-                verbose);
-
-        // Create New Subquery
-        Query queryNew = query.clone();
-        queryNew.properties().set("yql", yqlNew);
-
-        // Set fields to rank (extract using RANKING method)
-        String featureNameFieldsToRank =
-                addQueryWrapper(QUERY_INPUT_FIELDS_TO_RANK + "_" + rankingMethod);
-        logIfVerbose("Using fields to rank from " + featureNameFieldsToRank, verbose);
-        Tensor fieldsToRank = extractTensorRankFeature(query, featureNameFieldsToRank);
-        Iterator<Cell> cells = fieldsToRank.cellIterator();
-        cells.forEachRemaining(
-                (cell) ->
-                        addFieldToRankFeatures(
-                                cell, queryNew, retrievalMethod, rankingMethod, verbose));
-
-        // Set rank profile (using RANKING method)
-        queryNew.getRanking().setProfile(rankProfileNew);
-        // TODO: Remove, no need to re assign features
-        /*
-        if (query.properties()
-                .getBoolean("marqo__hybrid." + rankingMethod + "ScoreModifiersPresent")) {
-            // Extract lexical/tensor rank features and reassign to main rank features.
-            // Only do this for disjunction, lexical/lexical, or tensor/tensor, which use the
-            // original
-            // rank profiles.
-
-            if (rankingMethod.equals(retrievalMethod)) {
-                String featureNameScoreModifiersAddWeights =
-                        addQueryWrapper(
-                                QUERY_INPUT_SCORE_MODIFIERS_ADD_WEIGHTS + "_" + rankingMethod);
-                String featureNameScoreModifiersMultWeights =
-                        addQueryWrapper(
-                                QUERY_INPUT_SCORE_MODIFIERS_MULT_WEIGHTS + "_" + rankingMethod);
-                Tensor add_weights =
-                        extractTensorRankFeature(query, featureNameScoreModifiersAddWeights);
-                Tensor mult_weights =
-                        extractTensorRankFeature(query, featureNameScoreModifiersMultWeights);
-                queryNew.getRanking()
-                        .getFeatures()
-                        .put(addQueryWrapper(QUERY_INPUT_SCORE_MODIFIERS_ADD_WEIGHTS), add_weights);
-                queryNew.getRanking()
-                        .getFeatures()
-                        .put(
-                                addQueryWrapper(QUERY_INPUT_SCORE_MODIFIERS_MULT_WEIGHTS),
-                                mult_weights);
-            }
-
-        }
-        */
-
-        // Log tensor query final state
-        logIfVerbose("FINAL QUERY: ", verbose);
-        logIfVerbose(queryNew.toDetailString(), verbose);
-        logIfVerbose(queryNew.getModel().getQueryString(), verbose);
-        logIfVerbose(queryNew.properties().getString("yql", ""), verbose);
-        logIfVerbose(queryNew.getRanking().getFeatures().toString(), verbose);
-        logIfVerbose(
-                String.format("Rank Profile: %s", queryNew.getRanking().getProfile()), verbose);
-
-        return queryNew;
-    }
-
-    /**
      * Print human-readable list of hits with relevances.
      * @param hits
      * @param verbose
@@ -427,31 +247,5 @@ public class HybridSearcher extends Searcher {
         if (verbose) {
             logger.info(str);
         }
-    }
-
-    /**
-     * Extract a tensor rank feature, throwing an error if it does not exist
-     * @param query
-     * @param featureName
-     */
-    Tensor extractTensorRankFeature(Query query, String featureName) {
-        Optional<Tensor> optionalTensor = query.getRanking().getFeatures().getTensor(featureName);
-        Tensor resultTensor;
-
-        if (optionalTensor.isPresent()) {
-            resultTensor = optionalTensor.get();
-        } else {
-            throw new RuntimeException("Rank Feature: " + featureName + " not found in query!");
-        }
-
-        return resultTensor;
-    }
-
-    /**
-     * Enclose string in query()
-     * @param str
-     */
-    String addQueryWrapper(String str) {
-        return "query(" + str + ")";
     }
 }
