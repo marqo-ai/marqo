77adedc3aa720be2bfaf24e025814397812a9f6c
Tom <tomhamer@live.com>
2024-07-23 13:25:21 +1000

    Update no_model_utils.py

--- src/marqo/s2_inference/no_model_utils.py
+++ src/marqo/s2_inference/no_model_utils.py
@@ -12,5 +12,8 @@ class NO_MODEL(Model):
 
     def encode(self, *args, **kwargs) -> None:
         raise VectoriseError(f"Cannot vectorise anything with '{ModelType.NO_MODEL}'. "
-                             f"This model is intended for adding documents and searching with custom vectors only. "
-                             f"If vectorisation is needed, please use a different model ")
+                             f"This model is intended for adding documents and searching with custom vectors only."
+                             f"If searching using this index, you can set the search_method to LEXICAL for lexical search, "
+                             f"or remove the field from tensor_fields when calling add_documents to make your content" 
+                             f"only available for lexical search or filtering."
+                             f"If vectorisation is needed, please re-create the index a different model.")
