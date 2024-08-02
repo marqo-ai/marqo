d21362dc6e7ce97c731930f92c56e1effa063598
Raynor Chavez <raynorkirksonchavez@gmail.com>
2024-08-02 12:11:00 +0800

    change machine from g4dn.xlarge to g4dn.2xlarge

--- tests/s2_inference/test_large_model_encoding.py
+++ tests/s2_inference/test_large_model_encoding.py
@@ -37,7 +37,14 @@ class TestLargeModelEncoding(unittest.TestCase):
 
     def setUp(self) -> None:
         self.large_clip_models = [
-            #"open_clip/ViT-SO400M-14-SigLIP-384/webli"
+            'open_clip/ViT-L-14/laion400m_e32',
+            'open_clip/coca_ViT-L-14/mscoco_finetuned_laion2b_s13b_b90k',
+            'open_clip/convnext_xxlarge/laion2b_s34b_b82k_augreg_soup',
+            'open_clip/convnext_large_d_320/laion2b_s29b_b131k_ft_soup',
+            'open_clip/convnext_large_d/laion2b_s26b_b102k_augreg',
+            'open_clip/xlm-roberta-base-ViT-B-32/laion5b_s13b_b90k',
+            'open_clip/ViT-H-14-378-quickgelu/dfn5b',
+            'open_clip/ViT-SO400M-14-SigLIP-384/webli'
         ]
 
         self.multilingual_models = [
@@ -69,7 +76,7 @@ class TestLargeModelEncoding(unittest.TestCase):
         sentences = ['hello', 'this is a test sentence. so is this.', ['hello', 'this is a test sentence. so is this.']]
         device = "cuda"
         eps = 1e-9
-        with patch.dict(os.environ, {"MARQO_MAX_CUDA_MODEL_MEMORY": "6"}):
+        with patch.dict(os.environ, {"MARQO_MAX_CUDA_MODEL_MEMORY": "10"}):
             def run():
                 for name in names:
                     model_properties = get_model_properties_from_registry(name)
