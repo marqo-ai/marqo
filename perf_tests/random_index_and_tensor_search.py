090227b006a888cb887fcff20df50167f40aaf49
yihanzhao <yihan@marqo.ai>
2024-08-28 11:29:58 +1000

    Improve the test suite Fix the start docker container script

--- perf_tests/random_index_and_tensor_search.py
+++ perf_tests/random_index_and_tensor_search.py
@@ -1,47 +1,55 @@
 from __future__ import annotations
 
+import random
+
 from locust import events, task, between, run_single_user
+from wonderwords import RandomSentence, RandomWord
 import marqo
 
 from common.marqo_locust_http_user import MarqoLocustHttpUser
 
+# TODO make this configurable
 INDEX_NAME = "locust-test"
 
 
 class IndexingUser(MarqoLocustHttpUser):
     fixed_count = 1
     wait_time = between(1, 2)
+    s = RandomSentence()
 
     @task
     def add_docs(self):
-        self.client.index(INDEX_NAME).add_documents(documents=[
-            {'title': 'hello'},
-            {'title': 'hello, world'},
-            {'title': 'hello, my friend'},
-            {'title': 'hello, doggy'},
-            {'title': 'hello, kitty'},
-        ], tensor_fields=['title'])
+        # Generate random documents batch (5-10 docs) with random length description of 1-5 sentences
+        random_docs = [{
+            'title': self.s.sentence(),
+            'description': ' '.join([self.s.sentence() for j in range(random.randint(1, 5))])
+        } for i in range(random.randint(5, 10))]
+
+        self.client.index(INDEX_NAME).add_documents(documents=random_docs, tensor_fields=['title', 'description'])
 
 
 class SearchUser(MarqoLocustHttpUser):
     wait_time = between(1, 2)
+    w = RandomWord()
 
     @task
     def search(self):
-        self.client.index(INDEX_NAME).search('hello')
+        # Random search query to retrieve first 20 results
+        self.client.index(INDEX_NAME).search(
+            q=' '.join(self.w.random_words(amount=random.randint(1, 5))),
+            search_method='TENSOR',
+            limit=20,
+            show_highlights=False,
+            offset=0,
+        )
 
 
 @events.test_start.add_listener
 def on_test_start(environment, **kwargs):
+    # TODO make the marqo client configurable, only create index for local tests
     marqo_client = marqo.Client(url="http://localhost:8882")
+    # TODO make the model configurable as well
     marqo_client.create_index(INDEX_NAME, model='hf/e5-base-v2')
-    marqo_client.index(INDEX_NAME).add_documents(documents=[
-        {'title': 'hello'},
-        {'title': 'hello, world'},
-        {'title': 'hello, my friend'},
-        {'title': 'hello, doggy'},
-        {'title': 'hello, kitty'},
-    ], tensor_fields=['title'])
 
 
 @events.test_stop.add_listener
@@ -60,4 +68,4 @@ def on_test_stop(environment, **kwargs):
 
 
 if __name__ == "__main__":
-    run_single_user(IndexingUser)
+    run_single_user(SearchUser)
