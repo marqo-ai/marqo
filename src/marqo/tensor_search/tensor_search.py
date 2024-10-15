645e5be9e3483af3b068cbca58222ad9a3676787
Li Wan <lwan3@student.unimelb.edu.au>
2024-10-15 16:43:20 +1100

    Add logger

--- src/marqo/tensor_search/tensor_search.py
+++ src/marqo/tensor_search/tensor_search.py
@@ -435,6 +435,18 @@ def _add_documents_unstructured(config: Config, add_docs_params: AddDocsParams, 
                             # ADD DOCS TIMER-LOGGER (4)
                             start_time = timer()
                             with RequestMetricsStore.for_request().time(f"add_documents.create_vectors"):
+                                logger.debug(
+                                    f"The parameters for vectorisation are: "
+                                    f"model_name={marqo_index.model.name}, ",
+                                    f"model_properties={marqo_index.model.get_properties()}, ",
+                                    f"content={content_chunks}, ",
+                                    f"device={add_docs_params.device}, ",
+                                    f"normalize_embeddings={normalize_embeddings}, ",
+                                    f"infer={marqo_index.treat_urls_and_pointers_as_images}, ",
+                                    f"model_auth={add_docs_params.model_auth}, ",
+                                    f"modality={modality}"
+                                )
+
                                 vector_chunks = s2_inference.vectorise(
                                     model_name=marqo_index.model.name,
                                     model_properties=marqo_index.model.get_properties(), content=content_chunks,
