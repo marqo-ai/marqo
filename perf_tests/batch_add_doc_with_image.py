e48ee8e4250be23f15241cda265877b25b4de8cb
yihanzhao <yihan@marqo.ai>
2024-10-16 13:25:09 +1100

    batch to my_multi_modal_field

--- perf_tests/batch_add_doc_with_image.py
+++ perf_tests/batch_add_doc_with_image.py
@@ -42,9 +42,9 @@ class AddDocToUnstructuredIndexUser(MarqoLocustHttpUser):
                     "image": "https://raw.githubusercontent.com/marqo-ai/marqo/mainline/examples/ImageSearchGuide/data/image4.jpg"
                 },
             ] for i in range(int(32 / 4))] for doc in docs],
-            tensor_fields=["image"],
-            # mappings={"my_multi_modal_field": {"type": "multimodal_combination",
-            #                                    "weights": {"title": 0.5, "image": 0.8}}}
+            tensor_fields=["my_multi_modal_field"],
+            mappings={"my_multi_modal_field": {"type": "multimodal_combination",
+                                               "weights": {"title": 0.5, "image": 0.8}}}
         )
 
 
