bf81b8483f243d5b8e5e0d06218ac7ca0f0168a0
Raynor Chavez <raynorkirksonchavez@gmail.com>
2024-10-21 14:43:07 +1100

    change " encoding

--- src/marqo/core/utils/special_characters_encoding.py
+++ src/marqo/core/utils/special_characters_encoding.py
@@ -1,7 +1,7 @@
 import re
 
 def custom_encode(s: str) -> str:
-    return re.sub(r'(")|\\u0022', lambda m: r'\u0022' if m.group(1) else r'\\u0022', s)
+    return re.sub(r'(")', lambda m: r'\u0022', s)
 
 def decode_key(encoded_key: str) -> str:
     return re.sub(r'\\u0022', '"', encoded_key)
