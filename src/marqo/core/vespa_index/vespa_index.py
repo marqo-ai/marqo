131f1b7d2f9dd2453cdcc499788198d50dce78f5
Raynor Chavez <raynorkirksonchavez@gmail.com>
2024-10-23 16:58:36 +1100

    remove score modifier if " is in score modifier

--- src/marqo/core/vespa_index/vespa_index.py
+++ src/marqo/core/vespa_index/vespa_index.py
@@ -7,7 +7,6 @@ from marqo.core.models.marqo_index import StructuredMarqoIndex, UnstructuredMarq
 from marqo.core.models.score_modifier import ScoreModifier, ScoreModifierType
 from marqo.core.models.marqo_index import *
 from marqo.exceptions import InternalError
-import re
 
 class VespaIndex(ABC):
     """
@@ -102,36 +101,6 @@ class VespaIndex(ABC):
         """
         Get the name of the id field in Vespa documents, inside the 'fields' dictionary."""
         pass
-    
-    def _custom_encode(self, s: str) -> str:
-        """
-        Encode double quotes in a string by replacing them with their Unicode escape sequence.
-
-        This function replaces all occurrences of double quotes (") in the input string
-        with the Unicode escape sequence '\u0022'.
-
-        Args:
-            s (str): The input string to be encoded.
-
-        Returns:
-            str: The encoded string with double quotes replaced by '\u0022'.
-        """
-        return re.sub(r'(")', lambda m: r'\u0022', s)
-
-    def _decode_key(self, encoded_key: str) -> str:
-        """
-        Decode a string by replacing Unicode escape sequences for double quotes with actual double quotes.
-
-        This function replaces all occurrences of the Unicode escape sequence '\u0022'
-        in the input string with double quotes (").
-
-        Args:
-            encoded_key (str): The input string to be decoded.
-
-        Returns:
-            str: The decoded string with '\u0022' replaced by double quotes.
-        """
-        return re.sub(r'\\u0022', '"', encoded_key)
 
     def _convert_score_modifiers_to_tensors(self, score_modifiers: List[ScoreModifier]) -> Dict[
         str, Dict[str, float]]:
@@ -142,10 +111,12 @@ class VespaIndex(ABC):
         mult_tensor = {}
         add_tensor = {}
         for modifier in score_modifiers:
+            if '"' in modifier.field:
+                continue # Skip this modifier if it contains double quotes. Vespa Errors out on this.
             if modifier.type == ScoreModifierType.Multiply:
-                mult_tensor[self._custom_encode(modifier.field)] = modifier.weight
+                mult_tensor[modifier.field] = modifier.weight
             elif modifier.type == ScoreModifierType.Add:
-                add_tensor[self._custom_encode(modifier.field)] = modifier.weight
+                add_tensor[modifier.field] = modifier.weight
             else:
                 raise InternalError(f'Unknown score modifier type {modifier.type}')
 
