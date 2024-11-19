481286cd7d0378daa7aae74e1ea0dffb9ff5dc60
Yihan Zhao <yihan@marqo.ai>
2024-11-19 18:10:00 +1100

    Skip convergence check when bootstrapping Marqo (#1036)

--- tests/core/index_management/test_index_management.py
+++ tests/core/index_management/test_index_management.py
@@ -42,7 +42,7 @@ class TestIndexManagement(MarqoTestCase):
                                                 zookeeper_client=self.zookeeper_client,
                                                 enable_index_operations=True,
                                                 deployment_timeout_seconds=30,
-                                                convergence_timeout_seconds=60)
+                                                convergence_timeout_seconds=120)
         # this resets the application package to a clean state
         self._test_dir = str(os.path.dirname(os.path.abspath(__file__)))
         self._deploy_initial_app_package()
@@ -243,6 +243,36 @@ class TestIndexManagement(MarqoTestCase):
                 os.path.join(latest_version, *file)
             )
 
+    @patch('marqo.vespa.vespa_client.VespaClient.check_for_application_convergence')
+    def test_bootstrap_and_rollback_should_skip_convergence_check(self, mock_check_convergence):
+        self.index_management.bootstrap_vespa()
+        mock_check_convergence.assert_not_called()
+
+        mock_check_convergence.reset_mock()
+
+        try:
+            self.index_management.rollback_vespa()
+        except ApplicationRollbackError:
+            pass
+        mock_check_convergence.assert_not_called()
+
+    @patch('marqo.vespa.vespa_client.VespaClient.check_for_application_convergence')
+    @patch('marqo.vespa.vespa_client.VespaClient.get_vespa_version')
+    def test_bootstrap_and_rollback_should_not_skip_convergence_check_for_older_vespa_version(self, mock_vespa_version,
+                                                                                              mock_check_convergence):
+        mock_vespa_version.return_value = '8.382.21'
+
+        self.index_management.bootstrap_vespa()
+        mock_check_convergence.assert_called_once()
+
+        mock_check_convergence.reset_mock()
+
+        try:
+            self.index_management.rollback_vespa()
+        except ApplicationRollbackError:
+            pass
+        mock_check_convergence.assert_called_once()
+
     def test_rollback_should_fail_when_target_version_is_current_version(self):
         self.index_management.bootstrap_vespa()
         with self.assertRaisesStrict(ApplicationRollbackError) as e:
@@ -304,45 +334,45 @@ class TestIndexManagement(MarqoTestCase):
             self.assertEqual("Aborting rollback. Reason: Vector store config has been changed since the last backup.",
                              str(e.exception))
 
-    def test_index_operation_methods_should_raise_error_if_index_operation_is_disabled(self):
-        # Create an index management instance with index operation disabled (by default)
-        self.index_management = IndexManagement(self.vespa_client, zookeeper_client=None)
+    def _index_operations(self, index_management: IndexManagement):
         index_request_1 = self.structured_marqo_index_request(
             fields=[FieldRequest(name='title', type=FieldType.Text)],
             tensor_fields=['title']
         )
         index_request_2 = self.unstructured_marqo_index_request()
 
-        with self.assertRaisesStrict(InternalError):
-            self.index_management.create_index(index_request_1)
+        return [
+            ('create single index', lambda: index_management.create_index(index_request_1)),
+            ('batch create indexes', lambda: index_management.batch_create_indexes([index_request_1, index_request_2])),
+            ('delete single index', lambda: index_management.delete_index_by_name(index_request_1.name)),
+            ('batch delete indexes', lambda: index_management.batch_delete_indexes_by_name([index_request_1.name, index_request_2.name])),
+        ]
 
-        with self.assertRaisesStrict(InternalError):
-            self.index_management.batch_create_indexes([index_request_1, index_request_2])
+    def test_index_operation_methods_should_raise_error_if_index_operation_is_disabled(self):
+        index_management_without_zookeeper = IndexManagement(self.vespa_client, zookeeper_client=None)
 
-        with self.assertRaisesStrict(InternalError):
-            self.index_management.delete_index_by_name(index_request_1.name)
-
-        with self.assertRaisesStrict(InternalError):
-            self.index_management.batch_delete_indexes_by_name([index_request_1.name, index_request_2.name])
+        for test_case, index_operation in self._index_operations(index_management_without_zookeeper):
+            with self.subTest(test_case):
+                with self.assertRaisesStrict(InternalError):
+                    index_operation()
 
     def test_index_operation_methods_should_raise_error_if_marqo_is_not_bootstrapped(self):
-        index_request_1 = self.structured_marqo_index_request(
-            fields=[FieldRequest(name='title', type=FieldType.Text)],
-            tensor_fields=['title']
-        )
-        index_request_2 = self.unstructured_marqo_index_request()
+        for test_case, index_operation in self._index_operations(self.index_management):
+            with self.subTest(test_case):
+                with self.assertRaisesStrict(ApplicationNotInitializedError):
+                    index_operation()
 
-        with self.assertRaisesStrict(ApplicationNotInitializedError):
-            self.index_management.create_index(index_request_1)
+    @patch('marqo.vespa.vespa_client.VespaClient.check_for_application_convergence')
+    def test_index_operation_methods_should_check_convergence(self, mock_check_convergence):
+        for test_case, index_operation in self._index_operations(self.index_management):
+            with self.subTest(test_case):
+                try:
+                    index_operation()
+                except ApplicationNotInitializedError:
+                    pass
 
-        with self.assertRaisesStrict(ApplicationNotInitializedError):
-            self.index_management.batch_create_indexes([index_request_1, index_request_2])
-
-        with self.assertRaisesStrict(ApplicationNotInitializedError):
-            self.index_management.delete_index_by_name(index_request_1.name)
-
-        with self.assertRaisesStrict(ApplicationNotInitializedError):
-            self.index_management.batch_delete_indexes_by_name([index_request_1.name, index_request_2.name])
+                mock_check_convergence.assert_called_once()
+                mock_check_convergence.reset_mock()
 
     def test_create_and_delete_index_should_succeed(self):
         # merge batch create and delete happy path to save some testing time
