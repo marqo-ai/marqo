ed1d9889280affb6a5d09a721b25c467bc34c7d3
Yihan Zhao <yihan@marqo.ai>
2024-12-10 12:20:36 +1100

    Fix a bug where attributes_to_retrieve does not work for non-string fields in unstructured indexes created with Marqo 2.13 [Patching 2.14] (#1064)

--- tests/tensor_search/integ_tests/test_get_documents_by_ids.py
+++ tests/tensor_search/integ_tests/test_get_documents_by_ids.py
@@ -1,24 +1,18 @@
 import functools
 import os
-import pprint
-import unittest
 import uuid
 from unittest import mock
+from unittest.mock import patch
 
-from marqo.api.exceptions import IndexNotFoundError
 from marqo.api.exceptions import (
-    InvalidDocumentIdError, InvalidArgError,
+    InvalidArgError,
     IllegalRequestedDocCount
 )
-from marqo.tensor_search import enums
-from marqo.tensor_search import tensor_search
-from marqo.core.models.add_docs_params import AddDocsParams
-from tests.marqo_test import MarqoTestCase
 from marqo.core.models.marqo_index import *
 from marqo.core.models.marqo_index_request import FieldRequest
-from unittest.mock import patch
-import os
-import pprint
+from marqo.tensor_search import enums
+from marqo.tensor_search import tensor_search
+from tests.marqo_test import MarqoTestCase
 from tests.utils.transition import *
 
 
@@ -33,15 +27,37 @@ class TestGetDocuments(MarqoTestCase):
             fields=[
                 FieldRequest(name='title1', type=FieldType.Text),
                 FieldRequest(name='desc2', type=FieldType.Text),
+                FieldRequest(name='int_field', type=FieldType.Int),
+                FieldRequest(name='int_array_field', type=FieldType.ArrayInt),
+                FieldRequest(name='int_map_field', type=FieldType.MapInt),
+                FieldRequest(name='float_field', type=FieldType.Float),
+                FieldRequest(name='float_array_field', type=FieldType.ArrayFloat),
+                FieldRequest(name='float_map_field', type=FieldType.MapFloat),
+                FieldRequest(name='long_field', type=FieldType.Long),
+                FieldRequest(name='long_array_field', type=FieldType.ArrayLong),
+                FieldRequest(name='long_map_field', type=FieldType.MapLong),
+                FieldRequest(name='double_field', type=FieldType.Double),
+                FieldRequest(name='double_array_field', type=FieldType.ArrayDouble),
+                FieldRequest(name='double_map_field', type=FieldType.MapDouble),
+                FieldRequest(name='string_array_field', type=FieldType.ArrayText),
+                FieldRequest(name='bool_field', type=FieldType.Bool),
+                FieldRequest(name='custom_vector_field', type=FieldType.CustomVector),
             ],
-            tensor_fields=["title1", "desc2"]
+            tensor_fields=["title1", "desc2", "custom_vector_field"]
         )
-        unstructured_text_index_with_random_model_request = cls.unstructured_marqo_index_request(model=Model(name='random'))
+        unstructured_text_index_with_random_model_request = cls.unstructured_marqo_index_request(
+            model=Model(name='random'),
+            marqo_version='2.12.0'
+        )
+        semi_structured_text_index_with_random_model_request = cls.unstructured_marqo_index_request(
+            model=Model(name='random'),
+        )
 
         # List of indexes to loop through per test. Test itself should extract index name.
         cls.indexes = cls.create_indexes([
             structured_text_index_with_random_model_request,
-            unstructured_text_index_with_random_model_request
+            unstructured_text_index_with_random_model_request,
+            semi_structured_text_index_with_random_model_request
         ])
 
     def setUp(self) -> None:
@@ -52,17 +68,67 @@ class TestGetDocuments(MarqoTestCase):
     def tearDown(self) -> None:
         self.device_patcher.stop()
 
-    def test_get_documents_by_ids(self):
+    def test_get_documents_by_ids_structured(self):
+        index = self.indexes[0]
+
+        docs = [
+            {"_id": "1", "title1": "content 1",
+             "int_field": 1, "int_array_field": [1, 2], "int_map_field": {"a": 1},
+             "float_field": 2.9, "float_array_field": [1.0, 2.0], "float_map_field": {"b": 2.9},
+             "long_field": 10, "long_array_field": [10, 20], "long_map_field": {"a": 10},
+             "double_field": 3.9, "double_array_field": [3.0, 5.0], "double_map_field": {"b": 5.9},
+             "bool_field": True, "string_array_field": ["a", "b", "c"]},
+            {"_id": "2", "title1": "content 2", "custom_vector_field": {"content": "a", "vector": [1.0] * 384}},
+            {"_id": "3", "title1": "content 3"}
+        ]
+
+        self.add_documents(
+            config=self.config,
+            add_docs_params=AddDocsParams(
+                index_name=index.name, docs=docs, device="cpu"
+            )
+        )
+        res = tensor_search.get_documents_by_ids(
+            config=self.config, index_name=index.name, document_ids=['1', '2', '3'],
+            show_vectors=True).dict(exclude_none=True, by_alias=True)
+
+        # Check that the documents are found and have the correct content
+        for i in range(3):
+            self.assertEqual(res['results'][i]['_found'], True)
+
+            for field_name, value in res['results'][i].items():
+                if field_name in [enums.TensorField.tensor_facets, "_found"]:
+                    # ignore meta fields
+                    continue
+                if field_name == "custom_vector_field":
+                    expected_value = docs[i]["custom_vector_field"]["content"]
+                else:
+                    expected_value = docs[i][field_name]
+
+                self.assertEqual(expected_value, value)
+
+            self.assertIn(enums.TensorField.tensor_facets, res['results'][i])
+            self.assertIn(enums.TensorField.embedding, res['results'][i][enums.TensorField.tensor_facets][0])
+
+    def test_get_documents_by_ids_unstructured(self):
         for index in self.indexes:
+            if index.type == IndexType.Structured:
+                continue
+
             with self.subTest(f"Index type: {index.type}. Index name: {index.name}"):
                 docs = [
-                    {"_id": "1", "title1": "content 1"}, {"_id": "2", "title1": "content 2"},
+                    {"_id": "1", "title1": "content 1", "int_field": 1, "int_map_field": {"a": 1}, "float_field": 2.9,
+                     "float_map_field": {"b": 2.9}, "bool_field": True, "string_array_field": ["a", "b", "c"]},
+                    {"_id": "2", "title1": "content 2", "custom_vector_field": {"content": "a", "vector": [1.0] * 384}},
                     {"_id": "3", "title1": "content 3"}
                 ]
                 self.add_documents(
                     config=self.config,
-                    add_docs_params=AddDocsParams(index_name=index.name, docs=docs, device="cpu",
-                    tensor_fields=["title1", "desc2"] if isinstance(index, UnstructuredMarqoIndex) else None)
+                    add_docs_params=AddDocsParams(
+                        index_name=index.name, docs=docs, device="cpu",
+                        tensor_fields=["title1", "desc2", "custom_vector_field"],
+                        mappings={"custom_vector_field": {"type": "custom_vector"}}
+                    )
                 )
                 res = tensor_search.get_documents_by_ids(
                     config=self.config, index_name=index.name, document_ids=['1', '2', '3'],
@@ -71,8 +137,22 @@ class TestGetDocuments(MarqoTestCase):
                 # Check that the documents are found and have the correct content
                 for i in range(3):
                     self.assertEqual(res['results'][i]['_found'], True)
-                    self.assertEqual(res['results'][i]['_id'], docs[i]['_id'])
-                    self.assertEqual(res['results'][i]['title1'], docs[i]['title1'])
+
+                    for field_name, value in res['results'][i].items():
+                        if field_name in [enums.TensorField.tensor_facets, "_found"]:
+                            # ignore meta fields
+                            continue
+                        if field_name == "custom_vector_field":
+                            expected_value = docs[i]["custom_vector_field"]["content"]
+                        elif '.' in field_name:
+                            # unstructured and semi-structured indexes have all map fields flattened
+                            map_field_name, key = field_name.split('.', 1)
+                            expected_value = docs[i][map_field_name][key]
+                        else:
+                            expected_value = docs[i][field_name]
+
+                        self.assertEqual(expected_value, value)
+
                     self.assertIn(enums.TensorField.tensor_facets, res['results'][i])
                     self.assertIn(enums.TensorField.embedding, res['results'][i][enums.TensorField.tensor_facets][0])
 
