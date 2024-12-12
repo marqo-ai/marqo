7d43ac539b6f57ba0112eed767a53fb1df0441a5
Aditya Bharadwaj <adityabharadwaj198@gmail.com>
2024-12-12 15:23:45 +0530

    addressing comments # 2

--- tests/s2_inference/test_model_registry.py
+++ tests/s2_inference/test_model_registry.py
@@ -5,58 +5,48 @@ from tests.marqo_test import MarqoTestCase
 
 
 class TestModelRegistry(unittest.TestCase):
+    """
+    Test case for verifying the configurations of different models in the model registry.
+    """
     def setUp(self):
         self.open_clip_properties = _get_open_clip_properties()
 
     def test_open_clip_model_configurations_match(self):
-        with self.subTest("Check if Marqo/ViT-B-32.openai & open_clip/ViT-B-32/openai configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/ViT-B-32.openai"],self.open_clip_properties["open_clip/ViT-B-32/openai"])
+        """
+        Test to verify that the configurations of Marqo (which are following Marqtune convention) and open_clip models match.
 
-        with self.subTest("Check if Marqo/ViT-B-32.laion400m_e31 & open_clip/ViT-B-32/laion400m_e31 configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/ViT-B-32.laion400m_e31"],self.open_clip_properties["open_clip/ViT-B-32/laion400m_e31"])
+        This test iterates over a list of model pairs and compares their configurations
+        to ensure they are identical.
 
+        The model pairs are defined as tuples of Marqo model names and their corresponding
+        open_clip model names.
 
-        with self.subTest("Check if Marqo/ViT-B-32.laion400m_e32 & open_clip/ViT-B-32/laion400m_e32 configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/ViT-B-32.laion400m_e32"],self.open_clip_properties["open_clip/ViT-B-32/laion400m_e32"])
+        Raises:
+            AssertionError: If the configurations of any model pair do not match.
+        """
+        model_pairs = [
+            ("Marqo/ViT-B-32.laion400m_e31", "open_clip/ViT-B-32/laion400m_e31"),
+            ("Marqo/ViT-B-32.laion400m_e32", "open_clip/ViT-B-32/laion400m_e32"),
+            ("Marqo/ViT-B-32.laion2b_e16", "open_clip/ViT-B-32/laion2b_e16"),
+            ("Marqo/ViT-B-32.laion2b_s34b_b79k", "open_clip/ViT-B-32/laion2b_s34b_b79k"),
+            ("Marqo/ViT-B-16.openai", "open_clip/ViT-B-16/openai"),
+            ("Marqo/ViT-B-16.laion400m_e31", "open_clip/ViT-B-16/laion400m_e31"),
+            ("Marqo/ViT-B-16.laion400m_e32", "open_clip/ViT-B-16.laion400m_e32"),
+            ("Marqo/ViT-B-16.laion2b_s34b_b88k", "open_clip/ViT-B-16/laion2b_s34b_b88k"),
+            ("Marqo/ViT-L-14.openai", "open_clip/ViT-L-14/openai"),
+            ("Marqo/ViT-L-14.laion400m_e31", "open_clip/ViT-L-14/laion400m_e31"),
+            ("Marqo/ViT-L-14.laion400m_e32", "open_clip/ViT-L-14/laion400m_e32"),
+            ("Marqo/ViT-L-14.laion2b_s32b_b82k", "open_clip/ViT-L-14/laion2b_s32b_b82k"),
+            ("Marqo/roberta-ViT-B-32.laion2b_s12b_b32k", "open_clip/roberta-ViT-B-32/laion2b_s12b_b32k"),
+            ("Marqo/xlm-roberta-base-ViT-B-32.laion5b_s13b_b90k",
+             "open_clip/xlm-roberta-base-ViT-B-32/laion5b_s13b_b90k"),
+            ("Marqo/xlm-roberta-large-ViT-H-14.frozen_laion5b_s13b_b90k",
+             "open_clip/xlm-roberta-large-ViT-H-14/frozen_laion5b_s13b_b90k")
+        ]
 
-        with self.subTest("Check if Marqo/ViT-B-32.laion2b_e16 & open_clip/ViT-B-32/laion2b_e16 configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/ViT-B-32.laion2b_e16"],self.open_clip_properties["open_clip/ViT-B-32/laion2b_e16"])
-
-        with self.subTest("Check if Marqo/ViT-B-32.laion2b_s34b_b79k & open_clip/ViT-B-32/laion2b_s34b_b79k configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/ViT-B-32.laion2b_s34b_b79k"],self.open_clip_properties["open_clip/ViT-B-32/laion2b_s34b_b79k"])
-
-        with self.subTest("Check if Marqo/ViT-B-16.openai & open_clip/ViT-B-16/openai configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/ViT-B-16.openai"],self.open_clip_properties["open_clip/ViT-B-16/openai"])
-
-        with self.subTest("Check if Marqo/ViT-B-16.laion400m_e31 & open_clip/ViT-B-16/laion400m_e31 configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/ViT-B-16.laion400m_e31"],self.open_clip_properties["open_clip/ViT-B-16/laion400m_e31"])
-
-        with self.subTest("Check if Marqo/ViT-B-16.laion400m_e32 & open_clip/ViT-B-16/laion400m_e32 configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/ViT-B-16.laion400m_e32"],self.open_clip_properties["open_clip/ViT-B-16/laion400m_e32"])
-
-        with self.subTest("Check if Marqo/ViT-B-16.laion2b_s34b_b88k & open_clip/ViT-B-16/laion2b_s34b_b88k configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/ViT-B-16.laion2b_s34b_b88k"],self.open_clip_properties["open_clip/ViT-B-16/laion2b_s34b_b88k"])
-
-        with self.subTest("Check if Marqo/ViT-L-14.openai & open_clip/ViT-L-14/openai configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/ViT-L-14.openai"],self.open_clip_properties["open_clip/ViT-L-14/openai"])
-
-        with self.subTest("Check if Marqo/ViT-L-14.laion400m_e31 & open_clip/ViT-L-14/laion400m_e31 configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/ViT-L-14.laion400m_e31"],self.open_clip_properties["open_clip/ViT-L-14/laion400m_e31"])
-
-        with self.subTest("Check if Marqo/ViT-L-14.laion400m_e32 & open_clip/ViT-L-14/laion400m_e32 configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/ViT-L-14.laion400m_e32"],self.open_clip_properties["open_clip/ViT-L-14/laion400m_e32"])
-
-        with self.subTest("Check if Marqo/ViT-L-14.laion2b_s32b_b82k & open_clip/ViT-L-14/laion2b_s32b_b82k configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/ViT-L-14.laion2b_s32b_b82k"],self.open_clip_properties["open_clip/ViT-L-14/laion2b_s32b_b82k"])
-
-        with self.subTest("Check if Marqo/roberta-ViT-B-32.laion2b_s12b_b32k & open_clip/roberta-ViT-B-32/laion2b_s12b_b32k configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/roberta-ViT-B-32.laion2b_s12b_b32k"],self.open_clip_properties["open_clip/roberta-ViT-B-32/laion2b_s12b_b32k"])
-
-        with self.subTest("Check if Marqo/xlm-roberta-base-ViT-B-32.laion5b_s13b_b90k & open_clip/xlm-roberta-base-ViT-B-32/laion5b_s13b_b90k configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/xlm-roberta-base-ViT-B-32.laion5b_s13b_b90k"],self.open_clip_properties["open_clip/xlm-roberta-base-ViT-B-32/laion5b_s13b_b90k"])
-
-        with self.subTest("Check if Marqo/xlm-roberta-large-ViT-H-14.frozen_laion5b_s13b_b90k & open_clip/xlm-roberta-large-ViT-H-14/frozen_laion5b_s13b_b90k configurations match"):
-            self.compare_models(self.open_clip_properties["Marqo/xlm-roberta-large-ViT-H-14.frozen_laion5b_s13b_b90k"],self.open_clip_properties["open_clip/xlm-roberta-large-ViT-H-14/frozen_laion5b_s13b_b90k"])
+        for marqo_model, open_clip_model in model_pairs:
+            with self.subTest(f"Check if {marqo_model} & {open_clip_model} configurations match"):
+                self.compare_models(self.open_clip_properties[marqo_model], self.open_clip_properties[open_clip_model])
 
     def compare_models(self, model_properties_with_name_in_marqtune_convention, model_properties):
         self.assertEqual(model_properties_with_name_in_marqtune_convention["dimensions"], model_properties["dimensions"], f"dimensions do not match for {model_properties_with_name_in_marqtune_convention['name']} and {model_properties['name']}")
