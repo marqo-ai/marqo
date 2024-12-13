fb8e2eb4e89bdbb2acc9248a0edc5a9e466b2945
Farshid Zavareh <farshid@marqo.ai>
2024-12-13 15:11:19 +1100

    Fix start vespa call path

--- tests/api-tests/v1/scripts/start_local_marqo.sh
+++ tests/api-tests/v1/scripts/start_local_marqo.sh
@@ -3,13 +3,13 @@
 # $1 : marqo_image_name - name of the image you want to test
 # $@ : env_vars - strings representing all args to pass docker call
 
+set -e
 
-python3 scripts/start_vespa.py
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+python3 "$SCRIPT_DIR/start_vespa.py"
 
 MARQO_DOCKER_IMAGE="$1"
-shift
-
-docker rm -f marqo 2>/dev/null || true
 
 # Explanation:
 # -d detaches docker from process (so subprocess does not wait for it)
