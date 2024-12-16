211085cd04c85bd7e1198927a109b8796d378774
Li Wan <li@marqo.ai>
2024-12-17 10:02:14 +1100

    Fix tests

--- src/marqo/core/inference/embedding_models/languagebind_model.py
+++ src/marqo/core/inference/embedding_models/languagebind_model.py
@@ -129,7 +129,7 @@ class LanguagebindModel(AbstractEmbeddingModel):
                 "audio": None
             }
             for modality in self.model_properties.supportedModalities:
-                if modality == Modality.LANGUAGE:
+                if modality == Modality.TEXT:
                     continue
                 model_location: ModalityLocation = getattr(self.model_properties.modelLocation, modality)
                 if model_location is None:
@@ -201,7 +201,7 @@ class LanguagebindModel(AbstractEmbeddingModel):
             raise MediaMismatchError(f"The provided modality is not supported by the model. This model support "
                                      f"the following modalities: {self.model_properties.supportedModalities}")
 
-        if modality == Modality.LANGUAGE:
+        if modality == Modality.TEXT:
             return self._encode_text(content, normalize)
         elif modality == Modality.IMAGE:
             return self._encode_image(content, normalize, media_download_headers)
