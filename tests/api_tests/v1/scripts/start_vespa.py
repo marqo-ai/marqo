2e0f883c53cb59f135d18f103f78d904e00223d5
Farshid Zavareh <farshid@marqo.ai>
2025-01-14 15:32:15 +1100

    Upgrade Vespa version

--- tests/api_tests/v1/scripts/start_vespa.py
+++ tests/api_tests/v1/scripts/start_vespa.py
@@ -21,7 +21,7 @@ import sys
 
 import requests
 
-VESPA_VERSION=os.getenv('VESPA_VERSION', '8.396.18')  # default version baked into marqo-base:30
+VESPA_VERSION=os.getenv('VESPA_VERSION', '8.431.32')  # default version baked into marqo-base:30
 
 
 def start_vespa() -> None:
