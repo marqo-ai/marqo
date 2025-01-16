9e6f112cd6f3301be563c50ee822dc848531db9e
yihanzhao <yihan@marqo.ai>
2025-01-16 16:09:24 +1100

    a quick fix

--- src/marqo/tensor_search/tensor_search.py
+++ src/marqo/tensor_search/tensor_search.py
@@ -1778,7 +1778,7 @@ def gather_documents_from_response(response: QueryResult, marqo_index: MarqoInde
     vespa_index = vespa_index_factory(marqo_index)
     hits = []
     for doc in response.hits:
-        marqo_doc = vespa_index.to_marqo_document(doc.dict(), return_highlights=highlights)
+        marqo_doc = vespa_index.to_marqo_document(dict(doc), return_highlights=highlights)
         marqo_doc['_score'] = doc.relevance
 
         if (marqo_index.type in [IndexType.Unstructured, IndexType.SemiStructured] and
