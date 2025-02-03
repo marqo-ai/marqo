de684c6456f3eb280bea72fe72ff02a8c1102a6a
yihanzhao <yihan@marqo.ai>
2025-02-03 20:52:42 +1100

    extract Vectoriser protocol

--- src/marqo/tensor_search/vectoriser.py
+++ src/marqo/tensor_search/vectoriser.py
@@ -2,6 +2,8 @@ from typing import List, Union, Protocol, Optional
 
 import httpx
 from orjson import orjson
+import numpy as np
+import os
 
 from marqo.s2_inference import s2_inference
 from marqo.s2_inference.multimodal_model_load import Modality
@@ -16,17 +18,37 @@ class InferenceError(Exception):
     pass
 
 
-# class Vectoriser(Protocol):
-#     def vectorise(self, model_name: str, content: Union[str, List[str]], normalize_embeddings: bool,
-#                   modality: Modality = Modality.TEXT, device: str = None, **kwargs) -> List[List[float]]:
-#         ...
+class Vectoriser(Protocol):
+    def vectorise(self, model_name: str, content: Union[str, List[str]],
+                  model_properties: dict = None, device: str = None, normalize_embeddings: bool = True,
+                  model_auth: ModelAuth = None, enable_cache: bool = False, modality: Modality = Modality.TEXT,
+                  media_download_headers: Optional[dict] = None, **kwargs) -> List[List[float]]:
+        ...
 
 
-# class LocalVectoriser(Vectoriser):
-#     def vectorise(self, model_name: str, content: Union[str, List[str]], normalize_embeddings: bool,
-#                   modality: Modality = Modality.TEXT, device: str = None, **kwargs) -> List[List[float]]:
-#
-#         return s2_inference.vectorise(model_name, content, normalize_embeddings, modality, device, **kwargs)
+class LocalVectoriser:
+    def vectorise(self, model_name: str, content: Union[str, List[str]],
+                  model_properties: dict = None, device: str = None, normalize_embeddings: bool = True,
+                  model_auth: ModelAuth = None, enable_cache: bool = False, modality: Modality = Modality.TEXT,
+                  media_download_headers: Optional[dict] = None, **kwargs) -> List[List[float]]:
+        return s2_inference.vectorise(model_name=model_name, content=content, model_properties=model_properties,
+                                      device=device, normalize_embeddings=normalize_embeddings, model_auth=model_auth,
+                                      enable_cache=enable_cache, modality=modality,
+                                      media_download_headers=media_download_headers, **kwargs)
+
+
+class StaticVectoriser:
+    def __init__(self, dimension: int):
+        self.dimension = dimension
+        random_array = np.random.rand(dimension)
+        normalized_array = random_array / np.linalg.norm(random_array)
+        self.static_vector = [normalized_array.tolist()]
+
+    def vectorise(self, model_name: str, content: Union[str, List[str]],
+                  model_properties: dict = None, device: str = None, normalize_embeddings: bool = True,
+                  model_auth: ModelAuth = None, enable_cache: bool = False, modality: Modality = Modality.TEXT,
+                  media_download_headers: Optional[dict] = None, **kwargs) -> List[List[float]]:
+        return self.static_vector
 
 
 class RemoteVectoriser:
@@ -66,11 +88,16 @@ class RemoteVectoriser:
         return orjson.loads(resp.text)
 
 
+bypassing_inference = os.environ.get("MARQO_BYPASS_INFERENCE", "FALSE") == 'TRUE'
 running_remote_inference = utils.read_env_vars_and_defaults(EnvVars.MARQO_REMOTE_INFERENCE) == 'TRUE'
-remote_inference_url = utils.read_env_vars_and_defaults(EnvVars.MARQO_REMOTE_INFERENCE_URL)
-if running_remote_inference:
-    inference_server = RemoteVectoriser(inf_url=remote_inference_url)
-# TODO compose Vectoriser Protocol
+
+if bypassing_inference:
+    vectoriser = StaticVectoriser(dimension=768)
+elif running_remote_inference:
+    remote_inference_url = utils.read_env_vars_and_defaults(EnvVars.MARQO_REMOTE_INFERENCE_URL)
+    vectoriser = RemoteVectoriser(inf_url=remote_inference_url)
+else:
+    vectoriser = LocalVectoriser()
 
 
 def vectorise(model_name: str, content: Union[str, List[str]],
@@ -78,14 +105,9 @@ def vectorise(model_name: str, content: Union[str, List[str]],
               device: str = None, normalize_embeddings: bool = True,
               model_auth: ModelAuth = None, enable_cache: bool = False, modality: Modality = Modality.TEXT,
               media_download_headers: Optional[dict] = None, **kwargs) -> List[List[float]]:
-    if running_remote_inference:
-        return inference_server.vectorise(model_name=model_name, content=content, model_properties=model_properties,
-                                      device=device, normalize_embeddings=normalize_embeddings, model_auth=model_auth,
-                                      enable_cache=enable_cache, modality=modality,
-                                      media_download_headers=media_download_headers, **kwargs)
-    else:
-        return s2_inference.vectorise(model_name=model_name, content=content, model_properties=model_properties,
-                                      device=device, normalize_embeddings=normalize_embeddings, model_auth=model_auth,
-                                      enable_cache=enable_cache, modality=modality,
-                                      media_download_headers=media_download_headers, **kwargs)
+
+    return vectoriser.vectorise(model_name=model_name, content=content, model_properties=model_properties,
+                                  device=device, normalize_embeddings=normalize_embeddings, model_auth=model_auth,
+                                  enable_cache=enable_cache, modality=modality,
+                                  media_download_headers=media_download_headers, **kwargs)
 
