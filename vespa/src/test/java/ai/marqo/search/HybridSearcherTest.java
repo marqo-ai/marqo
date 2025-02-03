5856b4a8af211a5f8d97fa7f3aa5f593b82a97f5
Joshua <joshua@marqo.ai>
2025-02-03 12:00:47 +0800

    format: spotless apply

--- vespa/src/test/java/ai/marqo/search/HybridSearcherTest.java
+++ vespa/src/test/java/ai/marqo/search/HybridSearcherTest.java
@@ -17,11 +17,9 @@ import com.yahoo.search.searchchain.*;
 import com.yahoo.tensor.Tensor;
 import com.yahoo.tensor.TensorAddress;
 import com.yahoo.tensor.TensorType;
-
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -393,10 +391,11 @@ class HybridSearcherTest {
                             new Query(),
                             ErrorMessage.createInternalServerError("Example lexical error"));
             Result resultTensor = new Result(new Query());
-            HitGroup combinedErrors = hybridSearcher.collectErrorsFromResults(
-                    resultLexical, resultTensor);
+            HitGroup combinedErrors =
+                    hybridSearcher.collectErrorsFromResults(resultLexical, resultTensor);
 
-            assertThat(combinedErrors.getError().getDetailedMessage()).contains("Example lexical error");
+            assertThat(combinedErrors.getError().getDetailedMessage())
+                    .contains("Example lexical error");
         }
 
         @Test
@@ -406,10 +405,11 @@ class HybridSearcherTest {
                     new Result(
                             new Query(),
                             ErrorMessage.createInternalServerError("Example tensor error"));
-            HitGroup combinedErrors = hybridSearcher.collectErrorsFromResults(
-                    resultLexical, resultTensor);
+            HitGroup combinedErrors =
+                    hybridSearcher.collectErrorsFromResults(resultLexical, resultTensor);
 
-            assertThat(combinedErrors.getError().getDetailedMessage()).contains("Example tensor error");
+            assertThat(combinedErrors.getError().getDetailedMessage())
+                    .contains("Example tensor error");
         }
 
         @Test
@@ -422,8 +422,8 @@ class HybridSearcherTest {
                     new Result(
                             new Query(),
                             ErrorMessage.createInternalServerError("Example tensor error"));
-            HitGroup combinedErrors = hybridSearcher.collectErrorsFromResults(
-                    resultLexical, resultTensor);
+            HitGroup combinedErrors =
+                    hybridSearcher.collectErrorsFromResults(resultLexical, resultTensor);
 
             Iterator<ErrorMessage> iterator = combinedErrors.getErrorHit().errors().iterator();
             assertThat(iterator.next().getDetailedMessage()).contains("Example tensor error");
@@ -434,7 +434,8 @@ class HybridSearcherTest {
         void shouldNotRaiseErrorIfNeitherResultHasError() {
             Result resultLexical = new Result(new Query());
             Result resultTensor = new Result(new Query());
-            HitGroup combinedErrors = hybridSearcher.collectErrorsFromResults(resultLexical, resultTensor);
+            HitGroup combinedErrors =
+                    hybridSearcher.collectErrorsFromResults(resultLexical, resultTensor);
             assertThat(combinedErrors.getError()).isNull();
         }
     }
