fef0bf9136d2adfaa2f298a7df31bae9dce8bcea
Aditya Bharadwaj <adityabharadwaj198@gmail.com>
2025-02-13 13:30:14 +1100

    Changing condition to not use hardcoded "marqo__field_types" and "tensor" values

--- src/marqo/vespa/vespa_client.py
+++ src/marqo/vespa/vespa_client.py
@@ -15,6 +15,8 @@ import httpx
 import marqo.logging
 import marqo.vespa.concurrency as conc
 from marqo.core.models import MarqoIndex
+from marqo.core.semi_structured_vespa_index.common import VESPA_DOC_FIELD_TYPE
+from marqo.core.semi_structured_vespa_index.marqo_field_types import MarqoFieldTypes
 from marqo.vespa.exceptions import (VespaStatusError, VespaError, InvalidVespaApplicationError,
                                     VespaTimeoutError, VespaNotConvergedError, VespaActivationConflictError)
 from marqo.vespa.models import VespaDocument, QueryResult, FeedBatchDocumentResponse, FeedBatchResponse, \
@@ -836,8 +838,8 @@ class VespaClient:
             data["condition"] = f'{schema}.{vespa_id_field}==\"{doc_id}\"'
             if types is not None: # Types will be none for structured index as we are not storing types at the time of Add docs.
                 for key, value in types.items():
-                    data["condition"] += (f' and (not {schema}.marqo__field_types{{\"{key}\"}} or {schema}.marqo__field_types{{\"{key}\"}}==\"{value}\")'
-                                          f' and (not ({schema}.marqo__field_types{{\"{key}\"}}=="tensor"))')
+                    data["condition"] += (f' and (not {schema}.{VESPA_DOC_FIELD_TYPE}{{\"{key}\"}} or {schema}.{VESPA_DOC_FIELD_TYPE}{{\"{key}\"}}==\"{value}\")'
+                                          f' and (not ({schema}.{VESPA_DOC_FIELD_TYPE}{{\"{key}\"}}=="{MarqoFieldTypes.TENSOR.value}"))')
             try:
                 resp = await async_client.put(end_point, json=data, timeout=timeout)
             except httpx.RequestError as e:
