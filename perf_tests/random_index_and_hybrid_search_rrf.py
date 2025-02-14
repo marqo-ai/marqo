855cac7478ccddb46f76fb82576ea81d73ffbff2
Joshua <joshua@marqo.ai>
2025-02-14 15:39:37 +0800

    fix s

--- perf_tests/random_index_and_hybrid_search_rrf.py
+++ perf_tests/random_index_and_hybrid_search_rrf.py
@@ -21,11 +21,11 @@ INDEX_NAME = os.getenv('MARQO_INDEX_NAME', 'locust-test')
 class AddDocToStructuredIndexUser(MarqoLocustHttpUser):
     fixed_count = 1
     wait_time = between(1, 2)
-    s = "this is a random sentence."
 
     @task
     def add_docs(self):
         # Generate random documents batch (5-10 docs) with random length description of 1-5 sentences
+        s = "this is a random sentence."
         random_docs = [{
             'title': s,
             'description': ' '.join([s for j in range(i)]),
