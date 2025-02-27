e3a3f54c1ca9dce7557851a2f0ccb0151345dfb0
Aditya Bharadwaj <adityabharadwaj198@gmail.com>
2025-02-27 16:11:05 +1100

    Adding a space and a colon in the error message

--- src/marqo/vespa/vespa_client.py
+++ src/marqo/vespa/vespa_client.py
@@ -601,7 +601,7 @@ class VespaClient:
         vespa_status_code_to_marqo_doc_error_map = {
             200: (200, None),
             404: (404, "Document does not exist in the index"),
-            412: (400, "Marqo vector store couldn't update the document. Please see" + update_documents_response() + " for more details"), # Update documents get 412 from Vespa for document not found as we use condition
+            412: (400, "Marqo vector store couldn't update the document. Please see: " + update_documents_response() + " for more details"), # Update documents get 412 from Vespa for document not found as we use condition
             429: (429, "Marqo vector store receives too many requests. Please try again later"),
             507: (400, "Marqo vector store is out of memory or disk space"),
         }
