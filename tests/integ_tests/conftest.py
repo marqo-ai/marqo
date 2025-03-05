2e7dcc3db5d1ad84a2dcc095b85fdb075a545fa7
danyilq <danyil@s2search.io>
2025-03-05 15:37:05 +1100

    resolve comments

--- tests/integ_tests/conftest.py
+++ tests/integ_tests/conftest.py
@@ -1,4 +1,5 @@
-import pytest
+CLASSES_SPLIT = "classes"
+TESTS_SPLIT = "tests"
 
 
 def pytest_addoption(parser):
@@ -7,7 +8,9 @@ def pytest_addoption(parser):
     parser.addoption("--split-n", action="store", type=int, default=1, help="Split tests into N parts")
     parser.addoption("--split-part", action="store", type=int, default=0, help="Run tests for part N (zero-based index)")
     # add option to split either by classes or tests
-    parser.addoption("--split-by", action="store", type=str, default="tests", help="Split tests by classes or tests")
+    parser.addoption(
+        "--split-by", action="store", type=str, default=TESTS_SPLIT, help=f"Split tests by {CLASSES_SPLIT} or {TESTS_SPLIT}"
+    )
 
 
 def pytest_configure(config):
@@ -42,21 +45,24 @@ def pytest_collection_modifyitems(config, items):
 
         filtered_items.append(item)
 
-    # Step 1.1: **Split by classes if split-by==classes**
-    if split_by == "classes":
-        class_to_tests = {}
+    if split_by == CLASSES_SPLIT:
+        classes_to_tests = {}
         for item in filtered_items:
             class_name = item.parent.name
-            if class_name not in class_to_tests:
-                class_to_tests[class_name] = []
-            class_to_tests[class_name].append(item)
-        sorted_classes = sorted(class_to_tests.keys())
+            if class_name not in classes_to_tests:
+                classes_to_tests[class_name] = []
+            classes_to_tests[class_name].append(item)
+        sorted_classes = sorted(classes_to_tests.keys())
 
-    # Step 2: **Determine size of a partition**
-    if split_by == "classes":
+        # Step 2: **Determine size of a partition**
         chunk_size = max(1, len(sorted_classes) // parts)
+        last_idx = len(sorted_classes)
+
     else:
+        # Step 2: **Determine size of a partition**
         chunk_size = max(1, len(filtered_items) // parts)
+        last_idx = len(filtered_items)
+
     print(f"Total tests: {len(filtered_items)}, Splitting into {parts} parts, each with {chunk_size} {split_by}")
 
     # Step 3: **Determine the range of tests/classes to keep in this partition**
@@ -65,10 +71,12 @@ def pytest_collection_modifyitems(config, items):
 
     if part + 1 == parts:
         # Include all remaining classes/tests in the last partition
-        end_idx = len(filtered_items) if not split_by == "classes" else len(sorted_classes)
+        end_idx = last_idx
     print(f"Running tests from index {start_idx} to {end_idx}")
 
 
     # Step 4: **Modify the list of collected tests**
-    items[:] = filtered_items[start_idx:end_idx] if not split_by == "classes" else \
-        [item for item in filtered_items if item.parent.name in set(sorted_classes[start_idx:end_idx])]
+    if split_by == CLASSES_SPLIT:
+        items[:] = [item for item in filtered_items if item.parent.name in set(sorted_classes[start_idx:end_idx])]
+    else:
+        items[:] = filtered_items[start_idx:end_idx]
