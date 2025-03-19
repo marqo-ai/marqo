263fb487467b2fa9ec90caff104b42a1af574220
Farshid Zavareh <farshid@marqo.ai>
2025-03-19 13:36:15 +1100

    Add number of workers to run_marqo.sh

--- run_marqo.sh
+++ run_marqo.sh
@@ -147,9 +147,13 @@ fi
 export MARQO_LOG_LEVEL=${MARQO_LOG_LEVEL:-info}
 MARQO_LOG_LEVEL=`echo "$MARQO_LOG_LEVEL" | tr '[:upper:]' '[:lower:]'`
 
+# set default number of workers to 1
+set -q MARQO_API_WORKERS; or set MARQO_API_WORKERS 1
+
 # Start the tensor search web app in the background
 cd /app/src/marqo/tensor_search || exit
-uvicorn api:app --host 0.0.0.0 --port 8882 --timeout-keep-alive 75 --log-level $MARQO_LOG_LEVEL &
+uvicorn api:app --host 0.0.0.0 --port 8882 --workers $MARQO_API_WORKERS --timeout-keep-alive 75 --log-level $MARQO_LOG_LEVEL &
+
 export api_pid=$!
 wait "$api_pid"
 
