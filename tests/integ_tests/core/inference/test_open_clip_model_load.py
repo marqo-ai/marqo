1f750dec66c74f1d43fca157341295bd7aeb59a0
Li Wan <li@marqo.ai>
2025-03-19 16:02:06 +1100

    Add back the removed fashionclip tests and add marks

--- tests/integ_tests/core/inference/test_open_clip_model_load.py
+++ tests/integ_tests/core/inference/test_open_clip_model_load.py
@@ -10,7 +10,7 @@ from marqo.s2_inference.model_registry import _get_open_clip_properties
 from marqo.tensor_search.models.external_apis.s3 import S3Auth
 from marqo.tensor_search.models.private_models import ModelAuth, ModelLocation
 
-@pytest.mark.unittest
+
 class TestOpenCLIPModelLoad(TestCase):
     """A test suite for loading OpenCLIP models.
 
