edd1862d56377bc57f59d85d6d95e76052295ca0
Li Wan <li@marqo.ai>
2025-03-21 14:46:09 +1100

    Fix tests for OpenCLIPEncode

--- tests/integ_tests/inference/inference_test_case.py
+++ tests/integ_tests/inference/inference_test_case.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
 from unittest.mock import patch, Mock
 
+from marqo.core.inference.api import *
+from marqo.inference.native_inference.load_model import MODEL_PROPERTIES, clear_loaded_models
+from marqo.inference.native_inference.local_inference import NativeInferenceLocal
 from marqo.tensor_search.telemetry import RequestMetricsStore
 
 
@@ -19,3 +22,62 @@ class InferenceTestCase(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.configure_request_metrics()
+
+    def get_model_properties_from_registry(self, model_name: str) -> dict:
+        return MODEL_PROPERTIES["models"][model_name]
+
+    def encode_content_helper(
+            self, content: list[str], model_name: str="", model_properties: Optional[dict] = None,
+            modality: Union[Modality, str] = Modality.TEXT, device: Optional[str] = "cpu",
+            normalize_embeddings: bool = True, media_download_headers: Optional[dict] = None
+    ) -> List[ndarray]:
+        """
+        A helper function to encode the content of a document.
+
+        Args:
+            content: A list of strings to be encoded, can be text or media URLs.
+            model_name: The name of the model to be used for encoding.
+            model_properties: A dictionary containing the properties of the model to be used for encoding. If this is
+                None, model_properties will be retrieved from the model registry and model_name must be a valid model
+                in the registry. If this is not None, model_name is ignored and we will use the model properties
+                to load the model.
+            modality: The modality of the content to be encoded. Default is TEXT.
+            device: The device to be used for encoding. Default is "cpu".
+            normalize_embeddings: Whether to normalize the embeddings. Default is True.
+            media_download_headers: The headers to be used for downloading media. Default is None.
+        Returns:
+            A list of numpy arrays, each representing the encoded content, with dimensions (Dim, ).
+        """
+        if not model_name and not model_properties:
+            raise ValueError("Either model_name or model_properties must be provided.")
+
+        if not model_properties:
+            model_properties = self.get_model_properties_from_registry(model_name)
+
+        if modality == Modality.TEXT:
+            preprocessing_config = TextPreprocessingConfig(should_chunk=False)
+        elif modality == Modality.IMAGE:
+            preprocessing_config = ImagePreprocessingConfig(
+                download_thread_count=1,
+                download_header=media_download_headers
+            )
+        else:
+            raise ValueError(f"Unsupported modality: {modality}")
+
+        inference_request = InferenceRequest(
+            modality=modality,
+            contents=content,
+            device=device,
+            model_config=ModelConfig(
+                model_name=model_name,
+                model_properties=model_properties,
+                normalize_embeddings=normalize_embeddings
+            ),
+            use_inference_cache=False,
+            return_individual_error=True,
+            preprocessing_config=preprocessing_config
+        )
+
+        results: InferenceResult = NativeInferenceLocal().vectorise(inference_request)
+        embeddings = [result[0][1] for result in results.result]
+        return embeddings
---
+++ tests/integ_tests/inference/native_inference/embedding_models/test_open_clip_model_encode.py
@@ -0,0 +1,163 @@
+import numpy as np
+from parameterized import parameterized_class
+
+from integ_tests.inference.inference_test_case import *
+from marqo.inference.media_download_and_preprocess.image_download import load_image_from_path
+from marqo.inference.native_inference.load_model import load_model
+from tests.integ_tests.marqo_test import TestImageUrls
+
+OPEN_CLIP_TEST_MODELS = [
+    'open_clip/RN50/yfcc15m',
+    'Marqo/ViT-B-32.laion2b_s34b_b79k',
+    'open_clip/ViT-B-32/laion2b_s34b_b79k',
+    'open_clip/ViT-B-32/laion400m_e31',
+    'open_clip/ViT-B-16/laion2b_s34b_b88k',
+    'Marqo/ViT-B-16.laion2b_s34b_b88k',
+    'open_clip/convnext_base/laion400m_s13b_b51k',
+    'open_clip/convnext_base_w/laion_aesthetic_s13b_b82k',
+    'open_clip/coca_ViT-B-32/mscoco_finetuned_laion2b_s13b_b90k',
+    'open_clip/EVA02-B-16/merged2b_s8b_b131k',
+    "open_clip/MobileCLIP-B/datacompdr_lt",
+    "open_clip/MobileCLIP-S1/datacompdr"
+]
+
+
+@parameterized_class([{"model_name": model_name} for model_name in OPEN_CLIP_TEST_MODELS])
+class TestOpenClipModelEncoding(InferenceTestCase):
+    '''
+    This test is for open clip models as they are heavily used in production.
+    '''
+
+    model_name: str # A class variable to store the model name that will be populated by the parameterized decorator
+    device = "cpu"
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        clear_loaded_models()
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        clear_loaded_models()
+
+    def setUp(self):
+        super().setUp()
+        self.model = load_model(
+            self.model_name,
+            model_properties=self.get_model_properties_from_registry(self.model_name),
+            model_auth=None,
+            device=self.device
+        )
+        self.eps = 1e-6
+
+    def test_open_clip_encode_text_normalized(self):
+        """
+        A test to ensure that the open clip model generates the same embeddings as the pipeline for text inputs when
+        normalize is set to True.
+        """
+        texts = ['hello', 'big', 'asasasasaaaaaaaaaaaa', '', 'a word. another one!?. #$#.']
+
+        tokenized_text = self.model.get_preprocessor().preprocess(texts, modality=Modality.TEXT)
+        raw_embeddings = self.model.encode(tokenized_text, modality=Modality.TEXT, normalize=True)
+        pipeline_embeddings = self.encode_content_helper(
+            content=texts,
+            model_name=self.model_name,
+            modality=Modality.TEXT,
+            device=self.device,
+            normalize_embeddings=True
+        )
+
+        for i, raw_embedding in enumerate(raw_embeddings):
+            pipeline_embedding = pipeline_embeddings[i]
+            self.assertEqual(raw_embedding.shape, pipeline_embedding.shape)
+            self.assertTrue((raw_embedding - pipeline_embedding < self.eps).all())
+            self.assertTrue(np.linalg.norm(raw_embedding) - 1 < self.eps)
+            self.assertTrue(np.linalg.norm(pipeline_embedding) -1 < self.eps)
+
+    def test_open_clip_encode_image_normalized(self):
+        """
+        A test to ensure that the open clip model generates the same embeddings as the pipeline for image inputs when
+        normalize is set to True.
+        """
+        image_urls = [
+            TestImageUrls.IMAGE0.value,
+            TestImageUrls.IMAGE1.value,
+            TestImageUrls.IMAGE2.value,
+        ]
+
+        images = [load_image_from_path(image, media_download_headers=dict()) for image in image_urls]
+
+        preprocessed_images = self.model.get_preprocessor().preprocess(images, modality=Modality.IMAGE)
+        raw_embeddings = self.model.encode(preprocessed_images, modality=Modality.IMAGE, normalize=True)
+
+        pipeline_embeddings = self.encode_content_helper(
+            content=image_urls,
+            model_name=self.model_name,
+            modality=Modality.IMAGE,
+            device=self.device,
+            normalize_embeddings=True
+        )
+        for i, raw_embedding in enumerate(raw_embeddings):
+            pipeline_embedding = pipeline_embeddings[i]
+            self.assertEqual(raw_embedding.shape, pipeline_embedding.shape)
+            self.assertTrue((raw_embedding - pipeline_embedding < self.eps).all())
+            self.assertTrue(np.linalg.norm(raw_embedding) -1 < self.eps, np.linalg.norm(raw_embedding))
+            self.assertTrue(np.linalg.norm(pipeline_embedding) -1 < self.eps)
+
+    def test_open_clip_encode_text_not_normalized(self):
+        """
+        A test to ensure that the open clip model generates the same embeddings as the pipeline for text inputs when
+        normalize is set to False.
+        """
+        texts = ['hello', 'big', 'asasasasaaaaaaaaaaaa', '', 'a word. another one!?. #$#.']
+
+        tokenized_text = self.model.get_preprocessor().preprocess(texts, modality=Modality.TEXT)
+        raw_embeddings = self.model.encode(tokenized_text, modality=Modality.TEXT, normalize=False)
+        pipeline_embeddings = self.encode_content_helper(
+            content=texts,
+            model_name=self.model_name,
+            modality=Modality.TEXT,
+            device=self.device,
+            normalize_embeddings=False
+        )
+
+        unnormlised_epsilon = 1e-3
+
+        for i, raw_embedding in enumerate(raw_embeddings):
+            pipeline_embedding = pipeline_embeddings[i]
+            self.assertEqual(raw_embedding.shape, pipeline_embedding.shape)
+            self.assertTrue((raw_embedding - pipeline_embedding < self.eps).all())
+            self.assertTrue(np.linalg.norm(raw_embedding) - 1 > unnormlised_epsilon)
+            self.assertTrue(np.linalg.norm(pipeline_embedding) -1 > unnormlised_epsilon)
+
+    def test_open_clip_encode_image_not_normalized(self):
+        """
+        A test to ensure that the open clip model generates the same embeddings as the pipeline for image inputs when
+        normalize is set to False.
+        """
+        image_urls = [
+            TestImageUrls.IMAGE0.value,
+            TestImageUrls.IMAGE1.value,
+            TestImageUrls.IMAGE2.value,
+        ]
+        unnormlised_epsilon = 1e-3
+
+        images = [load_image_from_path(image, media_download_headers=dict()) for image in image_urls]
+
+        preprocessed_images = self.model.get_preprocessor().preprocess(images, modality=Modality.IMAGE)
+        raw_embeddings = self.model.encode(preprocessed_images, modality=Modality.IMAGE, normalize=False)
+
+        pipeline_embeddings = self.encode_content_helper(
+            content=image_urls,
+            model_name=self.model_name,
+            modality=Modality.IMAGE,
+            device=self.device,
+            normalize_embeddings=False
+        )
+        for i, raw_embedding in enumerate(raw_embeddings):
+            pipeline_embedding = pipeline_embeddings[i]
+            self.assertEqual(raw_embedding.shape, pipeline_embedding.shape)
+            self.assertTrue((raw_embedding - pipeline_embedding < self.eps).all())
+            self.assertTrue(np.linalg.norm(raw_embedding) -1 > unnormlised_epsilon)
+            self.assertTrue(np.linalg.norm(pipeline_embedding) -1 > unnormlised_epsilon)
