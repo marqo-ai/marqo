201459444b3b4ed6e027fc94b1cc2fdd741fee94
yihanzhao <yihan@marqo.ai>
2025-03-22 21:28:48 +1100

    Change more sbert model to hf

--- tests/api_tests/v1/tests/api_tests/test_create_index.py
+++ tests/api_tests/v1/tests/api_tests/test_create_index.py
@@ -79,7 +79,7 @@ class TestCreateIndex(MarqoTestCase):
                                  model_properties={"name": "sentence-transformers/multi-qa-MiniLM-L6-cos-v1",
                                                    "dimensions": 384,
                                                    "tokens": 128,
-                                                   "type": "sbert"}
+                                                   "type": "hf"}
                                  )
         documents = [{"test": "test"}]
         self.client.index(self.index_name).add_documents(documents, tensor_fields=["test"])
@@ -96,7 +96,7 @@ class TestCreateIndex(MarqoTestCase):
         self.assertEqual({"name": "sentence-transformers/multi-qa-MiniLM-L6-cos-v1",
                           "dimensions": 384,
                           "tokens": 128,
-                          "type": "sbert"}, index_settings['modelProperties'])
+                          "type": "hf"}, index_settings['modelProperties'])
 
     def test_created_unstructured_image_index_with_preprocessing(self):
         self.client.create_index(index_name=self.index_name, type="unstructured",
@@ -317,7 +317,7 @@ class TestCreateIndex(MarqoTestCase):
                                  model_properties={"name": "sentence-transformers/multi-qa-MiniLM-L6-cos-v1",
                                                    "dimensions": 384,
                                                    "tokens": 128,
-                                                   "type": "sbert"},
+                                                   "type": "hf"},
                                  all_fields=[{"name": "test", "type": "text", "features": ["lexical_search"]}],
                                  tensor_fields=["test"])
         documents = [{"test": "test"}]
@@ -334,7 +334,7 @@ class TestCreateIndex(MarqoTestCase):
         self.assertEqual({"name": "sentence-transformers/multi-qa-MiniLM-L6-cos-v1",
                           "dimensions": 384,
                           "tokens": 128,
-                          "type": "sbert"}, index_settings['modelProperties'])
+                          "type": "hf"}, index_settings['modelProperties'])
 
     def test_create_structured_image_index_with_preprocessing(self):
         self.client.create_index(index_name=self.index_name,
