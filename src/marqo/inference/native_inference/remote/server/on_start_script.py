d63b00c0cdc09324bd83bccc378b2b7094a394b8
yihanzhao <yihan@marqo.ai>
2025-03-25 09:35:31 +1100

    add back banners

--- src/marqo/inference/native_inference/remote/server/on_start_script.py
+++ src/marqo/inference/native_inference/remote/server/on_start_script.py
@@ -33,18 +33,18 @@ logger = get_logger(__name__)
 
 def on_start(config: Config):
     to_run_on_start = (
-        # DownloadStartText(),
-        # CUDAAvailable(),
+        DownloadStartText(),
+        CUDAAvailable(),
         SetEnableVideoGPUAcceleration(),
         CheckNLTKTokenizers(),
         CacheModels(config),
         # CachePatchModels(),  # TODO patch model can be deprecated, we comment it out for now
-        # DownloadFinishText(),
+        DownloadFinishText(),
         PrintVersion(),
 
         # TODO do we still need banners? or a different banner?
-        # MarqoWelcome(),
-        # MarqoPhrase(),
+        MarqoWelcome(),
+        MarqoPhrase(),
     )
 
     for thing_to_start in to_run_on_start:
