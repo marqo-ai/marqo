867f5d1f4e02649d9296430155a4e7bbcc32fd33
Li Wan <li@marqo.ai>
2025-03-24 15:23:30 +1100

    Fix OpenCLIP and LargeOpenCLIP Encode Tests (#1160)

--- tests/unit_tests/marqo/inference/native_inference/test_chunk_download_preprocess_image_content.py
+++ tests/unit_tests/marqo/inference/native_inference/test_chunk_download_preprocess_image_content.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from marqo.inference.native_inference.embedding_models.abstract_preprocessor import AbstractPreprocessor
 from marqo.inference.type import *
-from tests.integ_tests.marqo_test import TestImageUrls
+from integ_tests.marqo_test import TestImageUrls
 from marqo.inference.native_inference.content_preprocessing import download_and_preprocess_image
 from unit_tests.marqo_test import MarqoTestCase
 
