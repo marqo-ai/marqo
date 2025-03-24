dcefa62c9f08ae7331f51ad6a07aa5f508793618
Li Wan <li@marqo.ai>
2025-03-24 14:08:59 +1100

    Fix tests

--- tests/integ_tests/inference/native_inference/embedding_models/test_large_model_open_clip_encode.py
+++ tests/integ_tests/inference/native_inference/embedding_models/test_large_model_open_clip_encode.py
@@ -13,19 +13,19 @@ from marqo.inference.native_inference.load_model import load_model, clear_loaded
 
 LARGE_OPEN_CLIP_TEST_MODELS = [
     'open_clip/ViT-L-14/laion400m_e32',
-    # 'Marqo/ViT-L-14.laion400m_e32',
-    # 'open_clip/coca_ViT-L-14/mscoco_finetuned_laion2b_s13b_b90k',
-    # 'open_clip/convnext_xxlarge/laion2b_s34b_b82k_augreg_soup',
-    # 'open_clip/convnext_large_d_320/laion2b_s29b_b131k_ft_soup',
-    # 'open_clip/convnext_large_d/laion2b_s26b_b102k_augreg',
-    # 'open_clip/xlm-roberta-base-ViT-B-32/laion5b_s13b_b90k',
-    # 'Marqo/xlm-roberta-base-ViT-B-32.laion5b_s13b_b90k',
-    # 'open_clip/ViT-H-14-378-quickgelu/dfn5b',
-    # 'open_clip/ViT-SO400M-14-SigLIP-384/webli',
-    # "visheratin/nllb-siglip-mrl-large",
-    # "visheratin/nllb-clip-large-siglip",
-    # "visheratin/nllb-siglip-mrl-base",
-    # "visheratin/nllb-clip-base-siglip"
+    'Marqo/ViT-L-14.laion400m_e32',
+    'open_clip/coca_ViT-L-14/mscoco_finetuned_laion2b_s13b_b90k',
+    'open_clip/convnext_xxlarge/laion2b_s34b_b82k_augreg_soup',
+    'open_clip/convnext_large_d_320/laion2b_s29b_b131k_ft_soup',
+    'open_clip/convnext_large_d/laion2b_s26b_b102k_augreg',
+    'open_clip/xlm-roberta-base-ViT-B-32/laion5b_s13b_b90k',
+    'Marqo/xlm-roberta-base-ViT-B-32.laion5b_s13b_b90k',
+    'open_clip/ViT-H-14-378-quickgelu/dfn5b',
+    'open_clip/ViT-SO400M-14-SigLIP-384/webli',
+    "visheratin/nllb-siglip-mrl-large",
+    "visheratin/nllb-clip-large-siglip",
+    "visheratin/nllb-siglip-mrl-base",
+    "visheratin/nllb-clip-base-siglip"
 ]
 
 @pytest.mark.largemodel
