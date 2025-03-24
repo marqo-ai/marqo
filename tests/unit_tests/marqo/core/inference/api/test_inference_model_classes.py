73d2fc44c240e33ee08480da4003dbcf64369be3
yihanzhao <yihan@marqo.ai>
2025-03-24 17:49:36 +1100

    fix unit test

--- tests/unit_tests/marqo/core/inference/api/test_inference_model_classes.py
+++ tests/unit_tests/marqo/core/inference/api/test_inference_model_classes.py
@@ -155,7 +155,7 @@ class TestInferenceRequest(unittest.TestCase):
                 model_config=self.model_config,
                 preprocessing_config=TextPreprocessingConfig()
             )
-        self.assertIn('Modality or preprocessing_config is missing.', str(context.exception))
+        self.assertIn('Modality or preprocessing_config is missing', str(context.exception))
 
     def test_invalid_inference_request_with_missing_preprocessing_config(self):
         with self.assertRaises(ValidationError) as context:
@@ -165,7 +165,7 @@ class TestInferenceRequest(unittest.TestCase):
                 model_config=self.model_config,
                 preprocessing_config=None
             )
-        self.assertIn('Modality or preprocessing_config is missing.', str(context.exception))
+        self.assertIn('Modality or preprocessing_config is missing', str(context.exception))
 
     def test_invalid_inference_request_for_non_matching_modality(self):
         for modality, preprocessing_config in [
