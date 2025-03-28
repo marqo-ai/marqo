dc59bc2b645466ea2684a58b7876b0080f8e1375
Li Wan <li@marqo.ai>
2025-03-28 15:54:20 +1100

    Add Stella model to tests and two sentence-transformers models (#1167)
    Add Stella models to tests and add two sentence-transformers models, sentence-transformers/all-MiniLM-L12-v2 and
    sentence-transformers/all-MiniLM-L6-v2, to the model registry.

--- tests/integ_tests/inference/native_inference/test_hugging_face_model_inference_pipeline.py
+++ tests/integ_tests/inference/native_inference/test_hugging_face_model_inference_pipeline.py
@@ -5,6 +5,7 @@ from marqo.core.inference.api import *
 from marqo.inference.native_inference.device_manager import DeviceManager
 from marqo.inference.native_inference.local_inference import NativeInferenceLocal
 from integ_tests.marqo_test import TestImageUrls
+import pytest
 
 
 class TestHuggingfaceModelInferencePipeline(InferenceTestCase):
@@ -93,3 +94,46 @@ class TestHuggingfaceModelInferencePipeline(InferenceTestCase):
         self.assertTrue(isinstance(results_1[0][1], np.ndarray))
         self.assertEqual((768, ), results_1[0][1].shape)
         self.assertEqual(TestImageUrls.IMAGE1.value, results_1[0][0])
+
+    @pytest.mark.largemodel
+    def test_stella_model_also_work(self):
+        """Stella models inherit from HuggingFaceModel, so it should work with the same pipeline.
+        This is a test to ensure that the pipeline can handle it."""
+
+        text_inference_request = InferenceRequest(
+            modality=Modality.IMAGE,
+            contents=[TestImageUrls.IMAGE1.value],
+            device="cpu",
+            model_config=ModelConfig(
+                model_name="Marqo/dunzhang-stella_en_400M_v5",
+                model_properties={
+                    "name": "Marqo/dunzhang-stella_en_400M_v5",
+                    "dimensions": 1024,
+                    "tokens": 512,
+                    "type": "hf_stella",
+                    "trustRemoteCode": True,
+                    "text_query_prefix": "Instruct: Given a web search query, "
+                                         "retrieve relevant passages that answer the query.\nQuery: "
+                },
+                normalize_embeddings=True
+            ),
+            preprocessing_config=ImagePreprocessingConfig(
+                download_header=dict(),
+                download_thread_count=1,
+            )
+        )
+
+        results = self.inference.vectorise(text_inference_request)
+        self.assertTrue(isinstance(results, InferenceResult))
+        self.assertTrue(isinstance(results.result, list))
+        self.assertTrue(len(results.result) == 1)
+
+        results_1: list[tuple[str, ndarray]] = results.result[0]
+        self.assertTrue(isinstance(results_1, list))
+        self.assertTrue(len(results_1) == 1)
+        self.assertTrue(isinstance(results_1[0], tuple))
+        self.assertTrue(isinstance(results_1[0][0], str))
+        self.assertTrue(isinstance(results_1[0][1], np.ndarray))
+        self.assertEqual((1024, ), results_1[0][1].shape)
+        self.assertEqual(TestImageUrls.IMAGE1.value, results_1[0][0])
+
