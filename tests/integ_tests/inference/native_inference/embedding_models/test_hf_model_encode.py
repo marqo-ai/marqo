2d33eba27589264eecf12527273047a9aa55afe1
Li Wan <li@marqo.ai>
2025-03-28 13:50:57 +1100

    Add stella model to tests

--- tests/integ_tests/inference/native_inference/embedding_models/test_hf_model_encode.py
+++ tests/integ_tests/inference/native_inference/embedding_models/test_hf_model_encode.py
@@ -25,7 +25,7 @@ HF_TEST_MODELS = [
     "hf/bge-small-en-v1.5",
     "hf/bge-base-en-v1.5",
     "hf/snowflake-arctic-embed-m",
-    "hf/snowflake-arctic-embed-m-v1.5"
+    "hf/snowflake-arctic-embed-m-v1.5",
 ]
 
 
