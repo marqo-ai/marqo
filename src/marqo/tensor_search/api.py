dfe9a5301a41bfd7a7e323f55e35f4476d450722
yihanzhao <yihan@marqo.ai>
2025-04-03 22:28:23 +0800

    fix more integration tests

--- src/marqo/tensor_search/api.py
+++ src/marqo/tensor_search/api.py
@@ -456,8 +456,8 @@ def add_or_replace_documents(
 @app.patch("/indexes/{index_name}/documents")
 @throttle(RequestType.PARTIAL_UPDATE)
 def update_documents(
-        body: UpdateDocumentsBodyParams,
         index_name: str,
+        body_dict: dict,
         marqo_config: config.Config = Depends(get_config)):
     """
     Update an array of documents in a given index. Please refer to
@@ -465,7 +465,7 @@ def update_documents(
     """
     # TODO this a temporary fix due to the mixed use of pydantic v1 and v2.
     #  UpdateDocumentsBodyParams can be injected after migrated to v2
-    # body = parse_obj_as(UpdateDocumentsBodyParams, body_dict)
+    body = parse_obj_as(UpdateDocumentsBodyParams, body_dict)
 
     res = marqo_config.document.partial_update_documents_by_index_name(
         index_name=index_name, partial_documents=body.documents)
