52a639b7ae796d323e5b11d456b4c627ceb33d59
Li Wan <li@marqo.ai>
2025-04-08 10:36:41 +1000

    Fix `version_uuid` backwards compatibility issue (#1174)
    Fix a bug that Marqo incorrectly sends the marqo__version_uuid field to Vespa, even if the index does not support this field. This field was added in 2.16.0. Only indexes created after 2.16.0 have this field. However, Marqo is sending this field to Vespa for indexes created before 2.16.0

--- src/marqo/core/document/document.py
+++ src/marqo/core/document/document.py
@@ -1,19 +1,17 @@
 from timeit import default_timer as timer
 from typing import Dict, List, Tuple, Optional
 
-import semver
-
 import marqo.api.exceptions as api_exceptions
 from marqo.core.constants import MARQO_DOC_ID
-from marqo.core.models.add_docs_params import AddDocsParams
 from marqo.core.exceptions import UnsupportedFeatureError, ParsingError, InternalError, MarqoDocumentParsingError
 from marqo.core.index_management.index_management import IndexManagement
+from marqo.core.models.add_docs_params import AddDocsParams
 from marqo.core.models.marqo_add_documents_response import MarqoAddDocumentsResponse, MarqoAddDocumentsItem
 from marqo.core.models.marqo_index import IndexType, SemiStructuredMarqoIndex, StructuredMarqoIndex, \
     UnstructuredMarqoIndex
 from marqo.core.models.marqo_update_documents_response import MarqoUpdateDocumentsResponse, MarqoUpdateDocumentsItem
-from marqo.core.semi_structured_vespa_index.common import SEMISTRUCTURED_INDEX_PARTIAL_UPDATE_SUPPORT_VERSION, \
-    VESPA_FIELD_ID, INT_FIELDS, FLOAT_FIELDS, VESPA_DOC_FIELD_TYPES, VESPA_DOC_VERSION_UUID
+from marqo.core.semi_structured_vespa_index.common import VESPA_FIELD_ID, INT_FIELDS, FLOAT_FIELDS, \
+    VESPA_DOC_FIELD_TYPES, VESPA_DOC_VERSION_UUID
 from marqo.core.semi_structured_vespa_index.semi_structured_add_document_handler import \
     SemiStructuredAddDocumentsHandler, SemiStructuredFieldCountConfig
 from marqo.core.structured_vespa_index.structured_add_document_handler import StructuredAddDocumentsHandler
@@ -26,7 +24,6 @@ from marqo.vespa.models import UpdateDocumentsBatchResponse, VespaDocument
 from marqo.vespa.models.delete_document_response import DeleteAllDocumentsResponse
 from marqo.vespa.models.feed_response import FeedBatchResponse
 from marqo.vespa.vespa_client import VespaClient
-from marqo.version import get_version
 
 logger = get_logger(__name__)
 
@@ -117,9 +114,12 @@ class Document:
         elif marqo_index.type is IndexType.Structured:
             pass
         elif marqo_index.type is IndexType.SemiStructured:
-            if marqo_index.parsed_marqo_version() < SEMISTRUCTURED_INDEX_PARTIAL_UPDATE_SUPPORT_VERSION: # Partial updates for semi-structured indexes are only supported for Marqo version >= 2.16.0
-                raise UnsupportedFeatureError("Partial document update is not supported for this index version. "
-                                          "Please upgrade the index version, or create a new index to use this feature.")
+            if not marqo_index.index_supports_partial_updates: # Partial updates for semi-structured indexes are only supported for Marqo version >= 2.16.0
+                raise UnsupportedFeatureError(
+                    f"The 'update_documents' endpoint for an unstructured index "
+                    f"is only supported for indexes created with Marqo version 2.16.0 or "
+                    f"later. This index was created with Marqo {marqo_index.version}."
+                )
         else:
             raise ValueError(f"Invalid index type: {marqo_index.type}")
 
