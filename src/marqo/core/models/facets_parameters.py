de7ab0ee0345c1150ff37552f1c02a2243c273c9
danyilq <danyil@s2search.io>
2025-04-09 03:49:19 +1000

    improve error messages

--- src/marqo/core/models/facets_parameters.py
+++ src/marqo/core/models/facets_parameters.py
@@ -29,9 +29,9 @@ class FieldFacetsConfiguration(StrictBaseModel):
     @validator('max_results')
     def validate_max_results(cls, v):
         if v is not None and v <= 0:
-            raise ValueError("max_results must be greater than 0")
+            raise ValueError("'maxResults' must be greater than 0")
         if v is not None and v > 10000:
-            raise ValueError("max_results must be less than or equal to 10000")
+            raise ValueError("'maxResults' must be less than or equal to 10000")
         return v
 
     @validator('ranges')
@@ -60,16 +60,16 @@ class FacetsParameters(StrictBaseModel):
     @validator('max_depth')
     def validate_max_depth(cls, v):
         if v is not None and v <= 0:
-            raise ValueError("max_depth must be greater than 0")
+            raise ValueError("'maxDepth' must be greater than 0")
 
         return v
 
     @validator('max_results')
     def validate_max_results(cls, v):
         if v is not None and v <= 0:
-            raise ValueError("max_results must be greater than 0")
+            raise ValueError("'maxResults' must be greater than 0")
         if v is not None and v > 10000:
-            raise ValueError("max_depth must be less than or equal to 10000")
+            raise ValueError("'maxResults' must be less than or equal to 10000")
         return v
 
 
