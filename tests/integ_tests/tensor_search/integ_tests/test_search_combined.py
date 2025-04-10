5a02fd94f3497b7afc0606f9293888e0c33bb0d4
Joshua <joshua@marqo.ai>
2025-04-10 16:53:21 +0800

    add fuzz testing for lexical

--- tests/integ_tests/tensor_search/integ_tests/test_search_combined.py
+++ tests/integ_tests/tensor_search/integ_tests/test_search_combined.py
@@ -23,6 +23,7 @@ from marqo.core.unstructured_vespa_index.unstructured_vespa_index import Unstruc
 from marqo.tensor_search import tensor_search
 from marqo.tensor_search.enums import SearchMethod
 from marqo.tensor_search.models.api_models import SearchQuery, CustomVectorQuery
+from hypothesis import given, settings, Verbosity, strategies as st
 
 
 class TestSearch(MarqoTestCase):
@@ -1130,7 +1131,6 @@ class TestSearch(MarqoTestCase):
             {"_id": "red_herring_1", "text_field_1": '12'},
             {"_id": "red_herring_4", "text_field_1": 'escaped'},
             {"_id": "red_herring_5", "text_field_1": 'backslash'},
-
         ]
         test_cases = [
             ('hello\\normal char', []),
@@ -1173,6 +1173,7 @@ class TestSearch(MarqoTestCase):
                         )
                         self.assertEqual(len(expected_ids), len(res['hits']))
                         self.assertEqual(set(expected_ids), {hit['_id'] for hit in res['hits']})
+
 
     def test_search_private_image_return_proper_error(self):
         """A test to ensure that InvalidArgumentError is raised when searching for a private image."""
@@ -1337,3 +1338,144 @@ class TestSearch(MarqoTestCase):
                     )
                     self.assertEqual(len(res["hits"]), 3)
 
+
+# Set up text strategy to prioritize " and \\
+# Strategy that produces one of your special characters
+special_char_strategy = st.sampled_from(['"', '\\'])
+
+# Strategy that produces a normal character.
+normal_char_strategy = st.characters(blacklist_categories=('Cc', 'Cs'))
+
+# Combine the two strategies with weighting.
+# 3 in 10 characters will be one of the special ones,
+# 7 in 10 characters will be a normal one.
+weighted_strategies = [special_char_strategy] * 3 + [normal_char_strategy] * 7
+biased_char_strategy = st.one_of(*weighted_strategies)
+
+# Now generate a string by building a list of characters and joining them.
+biased_text_strategy = st.lists(biased_char_strategy, min_size=0, max_size=100).map(''.join)
+
+
+class TestSearchFuzz(MarqoTestCase):
+    """
+    Combined fuzz tests for unstructured and structured search.
+    Ensures that no errors are raised when searching with random characters in queries or filter strings.
+    Documents are added to indexes in setup, so it doesn't affect time taken in fuzz tests.
+    """
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+
+        # UNSTRUCTURED indexes
+        unstructured_default_text_index = cls.unstructured_marqo_index_request(
+            model=Model(name='hf/all_datasets_v4_MiniLM-L6')
+        )
+
+        # STRUCTURED indexes
+        structured_default_text_index = cls.structured_marqo_index_request(
+            model=Model(name="hf/all_datasets_v4_MiniLM-L6"),
+            fields=[
+                FieldRequest(name="text_field_1", type=FieldType.Text,
+                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
+                FieldRequest(name="text_field_2", type=FieldType.Text,
+                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
+                FieldRequest(name="text_field_3", type=FieldType.Text,
+                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
+                FieldRequest(name="text_field_4", type=FieldType.Text,
+                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
+                FieldRequest(name="text_field_5", type=FieldType.Text,
+                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
+                FieldRequest(name="text_field_6", type=FieldType.Text,
+                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
+                FieldRequest(name="text_field_7", type=FieldType.Text,
+                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
+                FieldRequest(name="text_field_8", type=FieldType.Text,
+                             features=[FieldFeature.LexicalSearch, FieldFeature.Filter]),
+                FieldRequest(name="int_field_1", type=FieldType.Int,
+                             features=[FieldFeature.Filter]),
+                FieldRequest(name="float_field_1", type=FieldType.Float,
+                             features=[FieldFeature.Filter]),
+                FieldRequest(name="bool_field_1", type=FieldType.Bool,
+                             features=[FieldFeature.Filter]),
+                FieldRequest(name="bool_field_2", type=FieldType.Bool,
+                             features=[FieldFeature.Filter]),
+                FieldRequest(name="list_field_1", type=FieldType.ArrayText,
+                             features=[FieldFeature.Filter]),
+                FieldRequest(name="long_field_1", type=FieldType.Long, features=[FieldFeature.Filter]),
+                FieldRequest(name="double_field_1", type=FieldType.Double, features=[FieldFeature.Filter]),
+                FieldRequest(name="custom_vector_field_1", type=FieldType.CustomVector, features=[FieldFeature.Filter]),
+                FieldRequest(name="multimodal_field_1", type=FieldType.MultimodalCombination,
+                             dependent_fields={"text_field_7": 0.1, "text_field_8": 0.1})
+            ],
+
+            tensor_fields=["text_field_1", "text_field_2", "text_field_3",
+                           "text_field_4", "text_field_5", "text_field_6",
+                           "custom_vector_field_1", "multimodal_field_1"]
+        )
+
+        cls.indexes = cls.create_indexes([
+            unstructured_default_text_index,
+            structured_default_text_index,
+        ])
+
+        # Assign to objects so they can be used in tests
+        cls.unstructured_default_text_index = cls.indexes[0]
+        cls.structured_default_text_index = cls.indexes[1]
+
+        # Add dummy document to all indexes in set up:
+        for index in [cls.unstructured_default_text_index, cls.structured_default_text_index]:
+            cls.add_documents(
+                config=cls.config,
+                add_docs_params=AddDocsParams(
+                    index_name=index.name,
+                    docs=[{"_id": "1", "text_field_1": "dummy"}],
+                    tensor_fields=["text_field_1"] if isinstance(index, UnstructuredMarqoIndex) else None
+                )
+            )
+
+    def setUp(self) -> None:
+        super().setUp()
+        # Any tests that call add_documents, search, bulk_search need this env var
+        self.device_patcher = mock.patch.dict(os.environ, {
+            "MARQO_BEST_AVAILABLE_DEVICE": "cpu",
+            "MARQO_MAX_CPU_MODEL_MEMORY": "15"
+        })
+        self.device_patcher.start()
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        self.device_patcher.stop()
+
+    @given(query=biased_text_strategy)
+    def test_fuzz_lexical_search_all_characters(self, query: str):
+        """
+        Fuzz test lexical query parsing invariants
+        Testing random strings to ensure no error is raised in lexical search.
+        Ensure no error is raised.
+        """
+        for index in [self.unstructured_default_text_index, self.structured_default_text_index]:
+            with self.subTest(index=index.type):
+                res = tensor_search.search(
+                    text=query, config=self.config, index_name=index.name,
+                    search_method=SearchMethod.LEXICAL
+                )
+                self.assertIn("hits", res)
+
+    @unittest.skip(reason='temporarily skip. to come back and add coverage for equality, range, and IN filters')
+    @given(filter=biased_text_strategy)
+    def test_fuzz_filter_all_characters(self, filter: str):
+        """
+        Fuzz test filter string parsing invariants
+        Testing random strings to ensure no error is raised with filters.
+        Ensure no error is raised.
+        TODO: Add coverage for equality, range, and IN filters with these random characters.
+        Note that tokens with unescaped special characters will raise filter string parsing errors.
+        """
+        for index in [self.unstructured_default_text_index, self.structured_default_text_index]:
+            with self.subTest(index=index.type):
+                res = tensor_search.search(
+                    text='', config=self.config, index_name=index.name,
+                    filter=filter
+                )
+                self.assertIn("hits", res)
