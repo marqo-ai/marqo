00d746bdaf56f80869c8d47c08d5fd7bfa810d81
Li Wan <li@marqo.ai>
2025-04-11 14:23:55 +1000

    Catch releases/2.17 (#1181)

--- tests/integ_tests/inference/native_inference/embedding_models/test_hf_model_encode.py
+++ tests/integ_tests/inference/native_inference/embedding_models/test_hf_model_encode.py
@@ -109,7 +109,7 @@ class TestHFModelEncode(InferenceTestCase):
             pipeline_embedding = pipeline_embeddings[i]
             self.assertEqual(raw_embedding.shape, pipeline_embedding.shape)
             self.assertTrue((raw_embedding - pipeline_embedding < self.eps).all())
-            self.assertTrue(raw_embedding.shape[0], self.model.model_properties.dimensions)
+            self.assertEqual(raw_embedding.shape[0], self.model.model_properties.dimensions)
             self.validate_norm(raw_embedding, epsilon=self.eps, normalize=True)
             self.validate_norm(pipeline_embedding, epsilon=self.eps, normalize=True)
 
@@ -134,6 +134,6 @@ class TestHFModelEncode(InferenceTestCase):
             pipeline_embedding = pipeline_embeddings[i]
             self.assertEqual(raw_embedding.shape, pipeline_embedding.shape)
             self.assertTrue((raw_embedding - pipeline_embedding < self.eps).all())
-            self.assertTrue(raw_embedding.shape[0], self.model.model_properties.dimensions)
+            self.assertEqual(raw_embedding.shape[0], self.model.model_properties.dimensions)
             self.validate_norm(raw_embedding, epsilon=self.eps, normalize=False)
             self.validate_norm(pipeline_embedding, epsilon=self.eps, normalize=False)
