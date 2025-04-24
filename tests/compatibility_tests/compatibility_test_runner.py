65d2d0e4582b9b208d94abce281ecdab3f104033
Joshua <joshua@marqo.ai>
2025-04-24 13:16:52 +0800

    add combined dir

--- tests/compatibility_tests/compatibility_test_runner.py
+++ tests/compatibility_tests/compatibility_test_runner.py
@@ -72,7 +72,7 @@ def run_prepare_mode(version_to_test_against: str):
 
         # TODO: remove this
         # Manually remove test classes that are not in a predefined list.
-        test_classes_to_prepare = ['TestSearchWithGlobalScoreModifiers']
+        test_classes_to_prepare = ['TestSearch', 'TestSearchWithGlobalScoreModifiers', 'TestSearchWithScoreModifiers']
         if test_class.__name__ not in test_classes_to_prepare:
             logger.info(f"Skipping test class {test_class.__name__} as it is not in the predefined list")
             continue
