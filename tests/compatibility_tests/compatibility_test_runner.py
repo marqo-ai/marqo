2641e101bf0ef7aac80545d3ad39c9f8c74f8abb
Joshua <joshua@marqo.ai>
2025-04-28 17:49:47 +0800

    add delete embed recommend tests

--- tests/compatibility_tests/compatibility_test_runner.py
+++ tests/compatibility_tests/compatibility_test_runner.py
@@ -79,7 +79,8 @@ def run_prepare_mode(version_to_test_against: str):
             'TestCreateStructuredIndexv2_0', 'TestCreateStructuredIndexv2_2', 'TestCreateStructuredIndexv2_9',
             'TestCreateStructuredIndexv2_12',
             'TestAddDocumentsv2_2', 'TestAddDocumentsv2_9', 'TestAddDocumentsv2_12',
-            'TestAddDocumentsMultiModal', 'TestAddDocumentsWithCustomVector', 'TestDocumentAPIv2_0'
+            'TestAddDocumentsMultiModal', 'TestAddDocumentsWithCustomVector', 'TestDocumentAPIv2_0',
+            'TestDeleteDocuments', 'TestEmbed', 'TestRecommend'
         ]
         if test_class.__name__ not in test_classes_to_prepare:
             logger.info(f"Skipping test class {test_class.__name__} as it is not in the predefined list")
