0089dc081191258f8dca1a252e52c079ab2e3dae
Joshua <joshua@marqo.ai>
2025-04-29 00:06:04 +0800

    add add docs tests

--- tests/compatibility_tests/compatibility_test_runner.py
+++ tests/compatibility_tests/compatibility_test_runner.py
@@ -79,8 +79,8 @@ def run_prepare_mode(version_to_test_against: str):
             'TestCreateIndex', 'TestCreateIndexBringYourOwnModel', 'TestCreateIndexWithNoModel',
             'TestCreateStructuredIndexv2_0', 'TestCreateStructuredIndexv2_2', 'TestCreateStructuredIndexv2_9',
             'TestCreateStructuredIndexv2_12',
-            #'TestAddDocumentsv2_2', 'TestAddDocumentsv2_9', 'TestAddDocumentsv2_12',
-            #'TestAddDocumentsMultiModal', 'TestAddDocumentsWithCustomVector', 'TestDocumentAPIv2_0',
+            'TestAddDocumentsv2_2', 'TestAddDocumentsv2_9', 'TestAddDocumentsv2_12',
+            'TestAddDocumentsMultiModal', 'TestAddDocumentsWithCustomVector', 'TestDocumentAPIv2_0',
             #'TestDeleteDocuments', 'TestEmbed', 'TestRecommend',
             'TestUpdateDocuments', 'TestUpdateDocumentsUnstructured2_16'
         ]
