54d0f6a3576eaf97b648913af758e8c74fd03ac7
yihanzhao <yihan@marqo.ai>
2025-04-29 12:52:30 +1000

    Replace unsupported model `ViT-B/16` with `open_clip/ViT-B-16/openai`

--- tests/integ_tests/tensor_search/integ_tests/test_add_documents_structured.py
+++ tests/integ_tests/tensor_search/integ_tests/test_add_documents_structured.py
@@ -125,7 +125,7 @@ class TestAddDocumentsStructured(MarqoTestCase):
                 )
             ],
             tensor_fields=['image_field', 'image_field_2'],
-            model=Model(name='ViT-B/16')
+            model=Model(name='open_clip/ViT-B-16/openai')
         )
         index_request_img_chunking = cls.structured_marqo_index_request(
             fields=[
@@ -141,7 +141,7 @@ class TestAddDocumentsStructured(MarqoTestCase):
                 )
             ],
             tensor_fields=['image_field'],
-            model=Model(name='ViT-B/16'),
+            model=Model(name='open_clip/ViT-B-16/openai'),
             normalize_embeddings=True,
             image_preprocessing=ImagePreProcessing(patch_method=PatchMethod.Frcnn)
         )
