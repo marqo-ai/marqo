a219fac09b1e50ad8b860ffe93d6fae6092decab
Joshua <joshua@marqo.ai>
2025-05-12 14:30:21 +0800

    import pytest

--- tests/integ_tests/core/search/test_recommender.py
+++ tests/integ_tests/core/search/test_recommender.py
@@ -14,6 +14,7 @@ from marqo.tensor_search import tensor_search
 from marqo.core.models.add_docs_params import AddDocsParams
 from marqo.tensor_search.models.score_modifiers_object import ScoreModifierLists, ScoreModifierOperator
 from integ_tests.marqo_test import MarqoTestCase
+import pytest
 
 
 class TestRecommender(MarqoTestCase):
--- tests/integ_tests/vespa/test_update_documents_batch.py
+++ tests/integ_tests/vespa/test_update_documents_batch.py
@@ -7,6 +7,7 @@ from marqo.vespa.exceptions import VespaError
 from marqo.vespa.models import VespaDocument
 from marqo.vespa.vespa_client import VespaClient
 from integ_tests.marqo_test import AsyncMarqoTestCase
+import pytest
 
 
 class TestFeedDocumentAsync(AsyncMarqoTestCase):
