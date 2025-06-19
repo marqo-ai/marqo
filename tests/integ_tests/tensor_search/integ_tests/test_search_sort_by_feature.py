4edd84898ec852009effe5f7f5061a37495f1e8f
Li Wan <li@marqo.ai>
2025-06-19 11:22:48 +1000

    Fix tests

--- tests/integ_tests/tensor_search/integ_tests/test_search_sort_by_feature.py
+++ tests/integ_tests/tensor_search/integ_tests/test_search_sort_by_feature.py
@@ -164,7 +164,7 @@ class TestSearchSortByFeatureSort1Field(MarqoTestCase):
         for _ in range(10):
             # We run it several times to ensure that the results are consistent
             res = self._help_sort_function(sort_by=sort_by)
-            self.assertEqual(10,res["_sortCandidates"])
+            self.assertEqual(10, res["_sortCandidates"])
             hits = res["hits"]
             self.assertEqual(10, len(hits))
             ids = [hit["_id"] for hit in hits]
