bc57e99d3570fbe321988570e7cbe558cc9c0592
Joshua <joshua@marqo.ai>
2025-06-25 16:53:36 +0800

    Revert "set max connections to unlimited"
    This reverts commit e077cb3db8bead182316f06a8756a8ed9ff36037.

--- src/marqo/vespa/vespa_client.py
+++ src/marqo/vespa/vespa_client.py
@@ -82,7 +82,7 @@ class VespaClient:
         self.async_transport = httpx.AsyncHTTPTransport(
             limits=httpx.Limits(
                 max_keepalive_connections=async_pool_size,
-                max_connections=None),
+                max_connections=async_pool_size),
             http1=True,
             http2=False  # Using http2 is slightly slower
         )
