21acba9e1a8512c6f0e6163c4460b8b866ef3633
Li Wan <li@marqo.ai>
2025-07-03 12:53:25 +1000

    Fix vespa custom searcher typo

--- src/marqo/tensor_search/models/relevance_cutoff_model.py
+++ src/marqo/tensor_search/models/relevance_cutoff_model.py
@@ -26,11 +26,11 @@ class RelevanceCutoffModel(StrictBaseModel):
     Attributes:
         method (RelevanceCutoffMethod): The method to use for relevance cutoff.
         probe_depth (int): The number of documents to probe for relevance cutoff. Defaults to 1000. We use
-            a lexical search as a probe search. Check Vespa Customer Searcher for more details.
+            a lexical search as a probe search. Check Vespa Custom Searcher for more details.
         parameters (Union[RelativeMaxScoreParameters, MeanStdParameters]): The parameters for the relevance cutoff method.
             If the method is RelativeMaxScore, you must provide 'relativeScoreFactor' as a parameter.
             If the method is MeanStd, you must provide 'stdDevFactor' as a parameter.
-            Check Vespa Customer Searcher for more details.
+            Check Vespa Custom Searcher for more details.
     """
     method: RelevanceCutoffMethod
     probe_depth: int = Field(1000, ge=1, alias="probeDepth")
