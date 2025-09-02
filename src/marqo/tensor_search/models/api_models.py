d542bb0251c2048bcc1e67148058d1570e03eb7e
Li Wan <li@marqo.ai>
2025-09-02 20:20:17 +1000

    Update error message

--- src/marqo/tensor_search/models/api_models.py
+++ src/marqo/tensor_search/models/api_models.py
@@ -363,8 +363,8 @@ class SearchQuery(BaseMarqoModel):
         sort_by = values.get('sort_by')
         score_modifiers = values.get('scoreModifiers')
         if sort_by is not None and score_modifiers is not None:
-            raise ValueError("'sortBy' cannot be used with 'scoreModifiers' in hybrid search as they are working in "
-                             "the same rerank phase. "
+            raise ValueError("'sortBy' cannot be used with 'scoreModifiers' (global score modifiers) in hybrid search "
+                             "as they are working in the same rerank phase. "
                              "Please use sortBy only for sorting by fields, and scoreModifiers only for modifying scores")
         return values
 
