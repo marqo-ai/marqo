67d3b3f6b1185e4c3b4deda501ed988d0500bcd5
Li Wan <li@marqo.ai>
2025-10-28 17:10:45 +1100

    Responde to Yihan's comments

--- components/marqo/src/marqo/core/index_management/vespa_application_package.py
+++ components/marqo/src/marqo/core/index_management/vespa_application_package.py
@@ -748,8 +748,7 @@ class VespaApplicationPackage:
         if not self.has_index(index.name):
             raise IndexNotFoundError(f"Index {index.name} not found")
 
-        version = index.version + 1 if index.version is not None else 1
-        self._index_setting_store.save_index_setting(index.copy(update={'version': version}))
+        self._index_setting_store.save_index_setting(index)
         self._persist_index_settings()
         self._deploy()
 
