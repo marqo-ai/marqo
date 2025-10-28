82bf3521473885efb6277cb518f238085390b73e
Li Wan <li@marqo.ai>
2025-10-28 18:26:14 +1100

    Fix tests

--- components/marqo/tests/integ_tests/tensor_search/test_api.py
+++ components/marqo/tests/integ_tests/tensor_search/test_api.py
@@ -332,6 +332,9 @@ class TestApiErrors(MarqoTestCase):
         cls.unstructured_index = cls.indexes[0]
         cls.structured_index = cls.indexes[1]
 
+    def setUp(self):
+        self.client = TestClient(api.app)
+
     def test_index_not_found_error(self):
         index_name = self.random_index_name()
 
