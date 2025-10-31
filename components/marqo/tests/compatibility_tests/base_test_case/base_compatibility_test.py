125015068adcf196829225837a535c6b1b5acda3
Li Wan <li@marqo.ai>
2025-10-31 18:02:51 +1100

    Test backwards compatibility tests

--- components/marqo/tests/compatibility_tests/base_test_case/base_compatibility_test.py
+++ components/marqo/tests/compatibility_tests/base_test_case/base_compatibility_test.py
@@ -80,11 +80,11 @@ class BaseCompatibilityTestCase(MarqoTestCase, ABC):
     def _compare_search_results(self, expected_result, actual_result):
         """Compare two search results and assert if they match."""
         score_fields = ["_score", "_lexical_score", "_tensor_score"]
-        expected_hits = copy.deepcopy(expected_result["hits"])
         try:
+            expected_hits = copy.deepcopy(expected_result["hits"])
             actual_hits = copy.deepcopy(actual_result["hits"])
         except KeyError as e:
-            raise KeyError(f"Actual result is missing 'hits' key: {e}. Actual result: {actual_result}")
+            raise KeyError(f"The result is missing 'hits' key: {e}. Result: {actual_result}")
 
         for index, expected_hit in enumerate(expected_hits):
             actual_hit = actual_hits[index]
