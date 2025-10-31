e768eb18eca8c9e0b6f0b8709bd54184023a530a
Li Wan <li@marqo.ai>
2025-10-31 15:32:35 +1100

    Test backwards compatibility tests

--- components/marqo/tests/compatibility_tests/compatibility_test_runner.py
+++ components/marqo/tests/compatibility_tests/compatibility_test_runner.py
@@ -122,8 +122,6 @@ def run_prepare_mode(version_to_test_against: str):
             errors.append(f"Failed to run prepare mode on testcase: {test_class.__name__}, when test mode runs on this test case, it is expected to fail. Search the class name in the logs to find the exact error.")
         logger.info(f"##################################################################################################")
 
-        break
-
 
     if errors:
         raise RuntimeError(f"Some errors occurred while running prepare mode on test cases: {errors}")
