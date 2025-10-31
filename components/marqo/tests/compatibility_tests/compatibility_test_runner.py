a611fcc246a60de805ca878c816e6ce8e4e49b12
Li Wan <li@marqo.ai>
2025-10-31 12:01:16 +1100

    Add a break

--- components/marqo/tests/compatibility_tests/compatibility_test_runner.py
+++ components/marqo/tests/compatibility_tests/compatibility_test_runner.py
@@ -121,7 +121,6 @@ def run_prepare_mode(version_to_test_against: str):
             logger.error(f"Failed to run prepare mode on testcase: {test_class.__name__}, when test mode runs on this test case, it is expected to fail. The exception was {e}", exc_info=True)
             errors.append(f"Failed to run prepare mode on testcase: {test_class.__name__}, when test mode runs on this test case, it is expected to fail. Search the class name in the logs to find the exact error.")
         logger.info(f"##################################################################################################")
-        break # TODO remove this break after testing since we just do one test case for now
 
     if errors:
         raise RuntimeError(f"Some errors occurred while running prepare mode on test cases: {errors}")
