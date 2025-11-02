bbbef12b46059074d72b1821e975695fc0e52821
Li Wan <li@marqo.ai>
2025-11-03 10:45:18 +1100

    Fix compatibility tests

--- components/marqo/tests/compatibility_tests/compatibility_test_runner.py
+++ components/marqo/tests/compatibility_tests/compatibility_test_runner.py
@@ -124,6 +124,9 @@ def run_prepare_mode(version_to_test_against: str) -> list[BaseCompatibilityTest
             errors.append(f"Failed to run prepare mode on testcase: {test_class.__name__}, when test mode runs on this test case, it is expected to fail. Search the class name in the logs to find the exact error.")
         logger.info(f"##################################################################################################")
 
+        if len(collected_classes) > 2:
+            break
+
     return collected_classes
 
     if errors:
