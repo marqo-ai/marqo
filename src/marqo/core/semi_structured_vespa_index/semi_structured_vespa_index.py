ccd4c34c16ae9f77420105b56218533274f5155e
yihanzhao <yihan@marqo.ai>
2025-11-15 21:17:50 +1100

    renaming done

--- src/marqo/core/semi_structured_vespa_index/semi_structured_vespa_index.py
+++ src/marqo/core/semi_structured_vespa_index/semi_structured_vespa_index.py
@@ -144,8 +144,8 @@ class SemiStructuredVespaIndex(StructuredVespaIndex, UnstructuredVespaIndex):
         return {
             constants.QUERY_INPUT_RECENCY_SHOULD_CALCULATE_SCORE: 1,
             constants.QUERY_INPUT_RECENCY_SHOULD_APPLY_SCORE: 0 if recency_params.apply_in_ranking_phase == 'only-global' else 1,
-            constants.QUERY_INPUT_RECENCY_DECAY_FACTOR: (recency_params.scale * 24 * 60 * 60),
-            constants.QUERY_INPUT_RECENCY_MIN_FACTOR: recency_params.min_score,
+            constants.QUERY_INPUT_RECENCY_SCALE_SECONDS: (recency_params.scale * 24 * 60 * 60),
+            constants.QUERY_INPUT_RECENCY_MIN_SCORE: recency_params.min_score,
             constants.QUERY_INPUT_RECENCY_TIMESTAMP_KEY: {recency_params.recency_field: 1.0},
             constants.QUERY_INPUT_RECENCY_DECAY_FUNCTION_TYPE: decay_function_map[recency_params.decay_function]
         }
