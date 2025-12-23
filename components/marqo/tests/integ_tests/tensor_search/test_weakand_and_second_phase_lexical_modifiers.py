c3df0fa49528e209b44d53107ed59c472b70b958
Li Wan <li@marqo.ai>
2025-12-23 19:50:10 +1100

    Add integration tests

--- components/marqo/tests/integ_tests/tensor_search/test_weakand_and_second_phase_lexical_modifiers.py
+++ components/marqo/tests/integ_tests/tensor_search/test_weakand_and_second_phase_lexical_modifiers.py
@@ -1,5 +1,5 @@
 from marqo.core.models.add_docs_params import AddDocsParams
-from marqo.core.models.hybrid_parameters import RetrievalMethod, RankingMethod, HybridParameters
+from marqo.core.models.hybrid_parameters import RetrievalMethod, RankingMethod, HybridParameters, WeakAndParameters
 from marqo.core.models.marqo_index import *
 from marqo.core.models.marqo_index_request import FieldRequest
 from marqo.tensor_search import tensor_search
@@ -9,6 +9,7 @@ from marqo.tensor_search.models.score_modifiers_object import ScoreModifierOpera
 from tests.integ_tests.marqo_test import MarqoTestCase
 import pytest
 from marqo.core.exceptions import UnsupportedFeatureError
+from pydantic.v1 import ValidationError
 
 
 @pytest.mark.skip_for_multinode
@@ -92,7 +93,6 @@ class TestSecondPhaseLexicalModifiers(MarqoTestCase):
         # The relevant document should not be in the results as it has no score modifier field
         self.assertNotIn('relevant_0', first_phase_score_modifier_results_ids)
 
-
         second_phase_score_modifier_results = tensor_search.search(
             config=self.config,
             index_name=self.index_name,
@@ -157,7 +157,7 @@ class TestUnsupportedScenarioForSecondPhaseLexicalModifiers(MarqoTestCase):
         structured_default_text_index = cls.structured_marqo_index_request(
             model=Model(name='hf/all-MiniLM-L6-v2'),
             fields=[FieldRequest(name='title', type=FieldType.Text)],
-            tensor_fields = ['title']
+            tensor_fields=['title']
         )
         unstructured_index_with_collapse_field = cls.unstructured_marqo_index_request(
             model=Model(name='hf/all-MiniLM-L6-v2'),
@@ -174,7 +174,7 @@ class TestUnsupportedScenarioForSecondPhaseLexicalModifiers(MarqoTestCase):
 
     def test_structured_index_raises_error(self):
         with self.assertRaises(UnsupportedFeatureError) as cm:
-             tensor_search.search(
+            tensor_search.search(
                 config=self.config,
                 index_name=self.structured_marqo_index,
                 text="test",
@@ -192,7 +192,7 @@ class TestUnsupportedScenarioForSecondPhaseLexicalModifiers(MarqoTestCase):
 
     def test_unstructured_index_with_collapse_field_raises_error(self):
         with self.assertRaises(UnsupportedFeatureError) as cm:
-             tensor_search.search(
+            tensor_search.search(
                 config=self.config,
                 index_name=self.unstructured_index_with_collapse_field,
                 text="test",
@@ -207,3 +207,179 @@ class TestUnsupportedScenarioForSecondPhaseLexicalModifiers(MarqoTestCase):
                 offset=0
             )
         self.assertIn("collapse fields as the collapse operation disables second phase", str(cm.exception))
+
+
+class TestRerankDepthLexicalAndWeakAndParameters(MarqoTestCase):
+    """
+    A test class to verify rerankDepthLexical and weakAndParameters.
+
+    """
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        semi_structured_default_text_index = cls.unstructured_marqo_index_request(
+            model=Model(name='random/small')
+        )
+
+        cls.create_indexes([
+            semi_structured_default_text_index,
+        ])
+
+        cls.index_name = semi_structured_default_text_index.name
+
+    def test_invalid_retrieval_method_raises_error(self):
+        with self.assertRaises(ValidationError) as cm:
+            tensor_search.search(
+                config=self.config,
+                index_name=self.index_name,
+                text="test",
+                search_method=SearchMethod.HYBRID,
+                hybrid_parameters=HybridParameters(
+                    retrievalMethod=RetrievalMethod.Tensor,
+                    rankingMethod=RankingMethod.Lexical,
+                    rerankDepthLexical=10,
+                ),
+                result_count=10,
+                offset=0
+            )
+        self.assertIn(
+            "'rerankDepthLexical' can only be set when 'retrievalMethod' is "
+            "'lexical' or 'disjunction'", str(cm.exception)
+        )
+
+    def test_weakand_parameters_stopwordlimit(self):
+        """
+        Test that very common stop words can be excluded in the retrieval using weakAndParameters.stopwordLimit.
+        """
+        irrelevant_docs = [
+            {
+                '_id': f'{doc_id}',
+                'text': 'stop stop stop irrelevant',
+                "score_modifier_value": 100.0
+            }
+            for doc_id in range(10)
+        ]
+
+        relevant_docs = [
+            {
+                "_id": f'relevant_0',
+                "text": "relevant documents",
+            }
+        ]
+
+        self.add_documents(
+            config=self.config, add_docs_params=AddDocsParams(
+                index_name=self.index_name,
+                docs=irrelevant_docs + relevant_docs,
+                tensor_fields=['text'],
+            )
+        )
+
+        results_without_weakand_parameters = tensor_search.search(
+            config=self.config,
+            index_name=self.index_name,
+            text="stop stop stop relevant documents",
+            search_method=SearchMethod.HYBRID,
+            hybrid_parameters=HybridParameters(
+                retrievalMethod=RetrievalMethod.Lexical,
+                rankingMethod=RankingMethod.Lexical,
+                rerankDepthLexical=100,
+                scoreModifiersLexical=ScoreModifierLists(
+                    add_to_score=[
+                        ScoreModifierOperator(
+                            field_name="score_modifier_value",
+                            weight=1.0
+                        )
+                    ]
+                )
+            ),
+            offset=0,
+            result_count=10
+        )
+
+        results_without_weakand_parameters_ids = [
+            doc['_id'] for doc in results_without_weakand_parameters['hits']
+        ]
+        self.assertNotIn('relevant_0', results_without_weakand_parameters_ids)
+
+
+        results_with_stop_word_limit = tensor_search.search(
+            config=self.config,
+            index_name=self.index_name,
+            text="stop stop stop relevant documents",
+            search_method=SearchMethod.HYBRID,
+            hybrid_parameters=HybridParameters(
+                retrievalMethod=RetrievalMethod.Lexical,
+                rankingMethod=RankingMethod.Lexical,
+                rerankDepthLexical=10,
+                weakAndParameters=WeakAndParameters(
+                    stopwordLimit=0.2,
+                    allowDropAll=False,
+                    adjustTarget=0.8,
+                ),
+                scoreModifiersLexical=ScoreModifierLists(
+                    add_to_score=[
+                        ScoreModifierOperator(
+                            field_name="score_modifier_value",
+                            weight=1.0
+                        )
+                    ]
+                )
+            ),
+            offset=0,
+            result_count=10
+        )
+
+        results_with_stop_word_limit_ids = [
+            doc['_id'] for doc in results_with_stop_word_limit['hits']
+        ]
+        self.assertIn('relevant_0', results_with_stop_word_limit_ids)
+
+    def test_weakand_parameters_drop_all(self):
+        """
+        Test that when all terms are dropped due to stopwordLimit, no results are returned if allowDropAll is True.
+        """
+        irrelevant_docs = [
+            {
+                '_id': f'{doc_id}',
+                'text': 'stop stop stop irrelevant',
+                "score_modifier_value": 100.0
+            }
+            for doc_id in range(10)
+        ]
+
+        relevant_docs = [
+            {
+                "_id": f'relevant_0',
+                "text": "relevant documents",
+            }
+        ]
+
+        self.add_documents(
+            config=self.config, add_docs_params=AddDocsParams(
+                index_name=self.index_name,
+                docs=irrelevant_docs + relevant_docs,
+                tensor_fields=['text'],
+            )
+        )
+        results_with_stop_word_limit_and_drop_all = tensor_search.search(
+            config=self.config,
+            index_name=self.index_name,
+            text="stop stop stop",
+            search_method=SearchMethod.HYBRID,
+            hybrid_parameters=HybridParameters(
+                retrievalMethod=RetrievalMethod.Lexical,
+                rankingMethod=RankingMethod.Lexical,
+                rerankDepthLexical=10,
+                weakAndParameters=WeakAndParameters(
+                    stopwordLimit=0.2,
+                    allowDropAll=True,
+                    adjustTarget=0.8,
+                ),
+            ),
+            offset=0,
+            result_count=10
+        )
+
+        self.assertEqual([], results_with_stop_word_limit_and_drop_all['hits'])
