6fda50c7553ca9a0f8fa3c6923a53d8d3de97d29
Li Wan <li@marqo.ai>
2026-01-21 13:32:16 +1100

    perf test modification

--- src/marqo/core/semi_structured_vespa_index/semi_structured_vespa_index.py
+++ src/marqo/core/semi_structured_vespa_index/semi_structured_vespa_index.py
@@ -262,15 +262,15 @@ class SemiStructuredVespaIndex(StructuredVespaIndex, UnstructuredVespaIndex):
 
         if marqo_query.track_total_hits is not None:
             # 0 is byte representation of letter "t"
-            if marqo_query.collapse_field_name:
-                total_hit_query = f"all(group({self._TOTAL_HITS_GROUP_CONST}) each(group({marqo_query.collapse_field_name}) output(count())))"
+            if marqo_query.collapse.name:
+                total_hit_query = f"all(group({self._TOTAL_HITS_GROUP_CONST}) each(group({marqo_query.collapse.name}) output(count())))"
             else:
                 total_hit_query = f"all(group({self._TOTAL_HITS_GROUP_CONST}) each(output(count())))"
             facet_queries.append(facets_query_skeleton % (f'{base_yql}{filter_term}', total_hit_query))
 
         if marqo_query.facets is not None:
             facets_term = self._get_facets_term(marqo_query.facets,
-                                                collapse_field_name=marqo_query.collapse_field_name)
+                                                collapse_field_name=marqo_query.collapse.name)
 
             if facets_term is not None:
                 facet_queries.append(facets_query_skeleton % (f'{base_yql}{filter_term}', facets_term))
@@ -290,7 +290,7 @@ class SemiStructuredVespaIndex(StructuredVespaIndex, UnstructuredVespaIndex):
                     else:
                         new_filter_term = ''
                     new_facets_term = self._get_facets_term(marqo_query.facets, facet_parameters.exclude_terms,
-                                                            collapse_field_name=marqo_query.collapse_field_name)
+                                                            collapse_field_name=marqo_query.collapse.name)
 
                     query_yql = f'{base_yql}{new_filter_term}'
 
