b506c9c54bb751adeac0ace4b375237c2a143c03
Li Wan <li@marqo.ai>
2026-03-03 13:04:25 +0800

    Add model timm/ViT-B-16-SigLIP2-256 (#1373)

--- components/common/src/marqo_common/model_registry.py
+++ components/common/src/marqo_common/model_registry.py
@@ -179,6 +179,63 @@ _MODEL_REGISTRY: dict[str, dict] = {
             "output": [{"name": "output", "dims": [768], "dataType": "TYPE_FP32"}],
         },
     },
+    "timm/ViT-B-16-SigLIP2-256": {
+        "name": "hf-hub:timm/ViT-B-16-SigLIP2-256",
+        "dimensions": 768,
+        "type": "open_clip",
+        "tritonImageEncoderProperties": {
+            "maxBatchSize": 8,
+            "name": "timm-ViT-B-16-SigLIP2-256-image-encoder",
+            "sources": [
+                f"{_MARQO_DEFAULT_MODELS_S3_BUCKET_PLACE_HOLDER}/timm-ViT-B-16-SigLIP2-256/image-encoder/model.onnx"
+            ],
+            "input": [
+                {
+                    "name": "input",
+                    "dims": [
+                        3,
+                        256,
+                        256
+                    ],
+                    "dataType": "TYPE_FP32"
+                }
+            ],
+            "output": [
+                {
+                    "name": "output",
+                    "dims": [
+                        768
+                    ],
+                    "dataType": "TYPE_FP32"
+                }
+            ]
+        },
+        "tritonTextEncoderProperties": {
+            "maxBatchSize": 16,
+            "name": "timm-ViT-B-16-SigLIP2-256-text-encoder",
+            "sources": [
+                f"{_MARQO_DEFAULT_MODELS_S3_BUCKET_PLACE_HOLDER}/timm-ViT-B-16-SigLIP2-256/text-encoder/model.onnx"
+            ],
+            "input": [
+                {
+                    "name": "input",
+                    "dims": [
+                        64
+                    ],
+                    "dataType": "TYPE_INT32"
+                }
+            ],
+            "output": [
+                {
+                    "name": "output",
+                    "dims": [
+                        768
+                    ],
+                    "dataType": "TYPE_FP32"
+                }
+            ]
+        }
+    },
     "open_clip/ViT-B-32/laion2b_s34b_b79k": {
         "name": "hf-hub:laion/CLIP-ViT-B-32-laion2B-s34B-b79K",
         "dimensions": 512,
